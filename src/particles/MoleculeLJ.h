4cecc88e075fd5b2adefca60001a469f04d8245d
Steffen Seckler <seckler@in.tum.de>
2018-04-06 02:45:39 +0000

    documentation of MoleculeLj
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/autopas/trunk@6194 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/particles/MoleculeLJ.h
+++ src/particles/MoleculeLJ.h
@@ -12,23 +12,47 @@
 
 namespace autopas {
 
+/**
+ * lennard jones molecule class
+ */
 class MoleculeLJ : public Particle {
  public:
   MoleculeLJ() = default;
 
+  /**
+   * constructor of a lennard jones molecule
+   * @param r position of the molecule
+   * @param v velocity of the molecule
+   * @param id id of the molecule
+   */
   explicit MoleculeLJ(std::array<double, 3> r, std::array<double, 3> v,
                       unsigned long id)
       : Particle(r, v, id) {}
 
   virtual ~MoleculeLJ() = default;
-  ;
 
+  /**
+   * get epsilon (characteristic energy of the lj potential)
+   * @return epsilon
+   */
   static double getEpsilon() { return EPSILON; }
 
+  /**
+   * set epsilon (characteristic energy of the lj potential)
+   * @param epsilon
+   */
   static void setEpsilon(double epsilon) { EPSILON = epsilon; }
 
+  /**
+   * get sigma (characteristic length of the lj potential)
+   * @return sigma
+   */
   static double getSigma() { return SIGMA; }
 
+  /**
+   * set sigma (characteristic length of the lj potential)
+   * @param sigma
+   */
   static void setSigma(double sigma) { SIGMA = sigma; }
 
  private:
