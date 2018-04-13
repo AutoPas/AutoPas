9e092c6e5c27f4740ada925c7bf46f6a2a0c04f6
Steffen Seckler <steffen.seckler@tum.de>
2018-04-13 19:05:25 +0900

    adds small status message

--- cmake/modules/CodeCoverageAutopas.cmake
+++ cmake/modules/CodeCoverageAutopas.cmake
@@ -2,6 +2,7 @@ option(CodeCoverage "CodeCoverage" OFF)
 
 if (CMAKE_BUILD_TYPE MATCHES Debug)
     if (CodeCoverage MATCHES ON)
+        message(STATUS "ENABLING CODE COVERAGE TARGET")
         include(CodeCoverage)
         set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_COVERAGE} ${CMAKE_CXX_FLAGS_DEBUG}")
         setup_target_for_coverage(${PROJECT_NAME}_coverage runTests coverage)
