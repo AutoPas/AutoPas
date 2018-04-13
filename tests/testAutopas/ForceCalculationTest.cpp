6b7d87af9124924a1efbbf1f9a93839927d8a59e
FG-TUM <f.gratl@tum.de>
2018-04-13 17:36:29 +0200

    added test for lj soa

--- tests/testAutopas/ForceCalculationTest.cpp
+++ tests/testAutopas/ForceCalculationTest.cpp
@@ -57,3 +57,38 @@ TEST_F(ForceCalculationTest, testLJwithU0AoS) {
     ASSERT_NEAR(expectedForces[id][2], p->getF()[2], tolerance);
   }
 }
+
+TEST_F(ForceCalculationTest, testLJwithU0AoS) {
+  AutoPas<autopas::MoleculeLJ, autopas::FullParticleCell<autopas::MoleculeLJ>>
+      autoPas;
+
+  double cutoff = 1.1;
+  double epsilon = 1.;
+  double sigma = 1.;
+  array<double, 3> boxMin = {0., 0., 0.};
+  array<double, 3> boxMax = {3., 3., 3.};
+
+  double expectedForces[4][3] = {
+      {-24, -24, 0}, {24, -24, 0}, {-24, 24, 0}, {24, 24, 0}};
+  double tolerance = 1e-13;
+
+  autoPas.init(boxMin, boxMax, cutoff, linkedCells);
+
+  fillWithParticles(autoPas, {2, 2, 1}, 1);
+
+  autopas::MoleculeLJ::setEpsilon(epsilon);
+  autopas::MoleculeLJ::setSigma(sigma);
+
+  LJFunctor<MoleculeLJ, FullParticleCell<MoleculeLJ>>::setGlobals(
+      cutoff, epsilon, sigma, 0.0);
+  LJFunctor<MoleculeLJ, FullParticleCell<MoleculeLJ>> functor;
+
+  autoPas.iteratePairwise(&functor, autopas::soa);
+
+  for (auto p = autoPas.begin(); p.isValid(); ++p) {
+    auto id = p->getID();
+    ASSERT_NEAR(expectedForces[id][0], p->getF()[0], tolerance);
+    ASSERT_NEAR(expectedForces[id][1], p->getF()[1], tolerance);
+    ASSERT_NEAR(expectedForces[id][2], p->getF()[2], tolerance);
+  }
+}
