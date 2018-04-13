49ba7303b27c5c0801952fc1902695473d291b1a
Steffen Seckler <steffen.seckler@tum.de>
2018-04-13 18:21:44 +0900

    adds code coverage target moves cmake files to a more proper place

---
+++ cmake/modules/CodeCoverageAutopas.cmake
@@ -0,0 +1,10 @@
+option(CodeCoverage "CodeCoverage" OFF)
+
+if (CMAKE_BUILD_TYPE MATCHES Debug)
+    if (CodeCoverage MATCHES ON)
+        include(CodeCoverage)
+        set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_COVERAGE} ${CMAKE_CXX_FLAGS_DEBUG}")
+        setup_target_for_coverage(${PROJECT_NAME}_coverage runTests coverage)
+        setup_target_for_coverage_cobertura(${PROJECT_NAME}_cobertura runTests coverage --gtest_output=xml:coverage.junit.xml)
+    endif()
+endif()
