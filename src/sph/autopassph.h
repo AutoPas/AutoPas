ac8f3d780d577259ee8aac9324bc41596c09033f
Steffen Seckler <steffen.seckler@tum.de>
2018-04-16 13:23:37 +0900

    fixes wrong header guard in autopassph.h

--- src/sph/autopassph.h
+++ src/sph/autopassph.h
@@ -2,12 +2,10 @@
 // Created by seckler on 22.01.18.
 //
 
-#ifndef AUTOPAS_AUTOPASMD_H
-#define AUTOPAS_AUTOPASMD_H
+#pragma once
 
 #include "SPHCalcDensityFunctor.h"
 #include "SPHCalcHydroForceFunctor.h"
 #include "SPHKernels.h"
 #include "SPHParticle.h"
 
-#endif  // AUTOPAS_AUTOPASMD_H
