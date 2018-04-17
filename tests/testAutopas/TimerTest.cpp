ebc3844e861741e93bc5cacbc26740f38ac1ec5c
Steffen Seckler <steffen.seckler@tum.de>
2018-04-17 16:36:14 +0900

    adds timertest

---
+++ tests/testAutopas/TimerTest.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file TimerTest.cpp
+ * @author seckler
+ * @date 17.04.18
+ */
+
+
+#include "gtest/gtest.h"
+#include "utils/Timer.h"
+
+TEST(TimerTest, testTimer){
+  autopas::utils::Timer timer;
+  timer.start();
+  auto time = timer.stop();
+  ASSERT_LE(time, 0.01);
+
+  timer.start();
+  // sleeping for 10ms
+  usleep(10e3);
+  time = timer.stop();
+  // time should be close to 10ms
+  ASSERT_NEAR(time, 10e-3, 1e-3);
+
+}
