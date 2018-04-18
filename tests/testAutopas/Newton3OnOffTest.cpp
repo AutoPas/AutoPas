edf26a65e14744aa9642817d792b128d93445721
Steffen Seckler <steffen.seckler@tum.de>
2018-04-18 09:43:41 +0900

    adds tests for newton3 on/off setting for SPH functors

---
+++ tests/testAutopas/Newton3OnOffTest.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file Newton3OnOffTest.cpp
+ * @author seckler
+ * @date 18.04.18
+ */
+
+#include "Newton3OnOffTest.h"
+
+
+
---
+++ tests/testAutopas/Newton3OnOffTest.h
@@ -0,0 +1,18 @@
+/**
+ * @file Newton3OnOffTest.h
+ * @author seckler
+ * @date 18.04.18
+ */
+
+#pragma once
+
+#include "autopasIncludes.h"
+#include "sph/autopassph.h"
+#include "gtest/gtest.h"
+
+/**
+ * Test to check if newton3 and non-newton3 work as expected
+ */
+class Newton3OnOffTest : public ::testing::Test {
+
+};
