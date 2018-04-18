f92a144b0c71e125829aa654909a1561b74ffce8
FG-TUM <f.gratl@tum.de>
2018-04-18 18:40:15 +0200

    added traversal test

---
+++ tests/testAutopas/TraversalRaceConditionTest.h
@@ -0,0 +1,57 @@
+#ifndef AUTOPAS_TRAVERSALRACECONDITIONTEST_H
+#define AUTOPAS_TRAVERSALRACECONDITIONTEST_H
+
+#include <AutoPas.h>
+#include <gtest/gtest.h>
+#include <vector>
+#include "../../examples/md/mdutils.h"
+#include "containers/cellPairTraversals/SlicedTraversal.h"
+
+class TraversalRaceConditionTest : public ::testing::Test {
+ public:
+  TraversalRaceConditionTest() = default;
+
+  ~TraversalRaceConditionTest() override = default;
+
+  void fillWithParticles(
+      AutoPas<PrintableMolecule, autopas::FullParticleCell<PrintableMolecule>>
+          &autoPas,
+      std::array<size_t, 3> particlesPerDim);
+
+  /*
+   * Simple AoS only functor which repulses paritcles from each other with a constant force of 1.
+   */
+  class SimpleFunctor
+      : public autopas::Functor<PrintableMolecule,
+                                autopas::FullParticleCell<PrintableMolecule>> {
+    void AoSFunctor(PrintableMolecule &i, PrintableMolecule &j) override {
+      auto coordsI = i.getR();
+      auto coordsJ = j.getR();
+
+      std::array<double, 3> dr = arrayMath::sub(coordsI, coordsJ);
+      double dr2 = arrayMath::dot(dr, dr);
+
+      if (dr2 > CUTOFFSQUARE) return;
+
+      std::array<double, 3> f;
+
+      for (int dim = 0; dim < 3; ++dim) {
+        if (coordsI[dim] < coordsJ[dim]) {
+          f[dim] = -1;
+        } else if (coordsI[dim] > coordsJ[dim]) {
+          f[dim] = 1;
+        } else {
+          f[dim] = 0;
+        }
+      }
+
+      i.addF(f);
+      j.subF(f);
+    }
+   private:
+    // in a grid with separation 1 this includes all neighbors with a Chebyshev distance of 1
+    constexpr static double CUTOFFSQUARE = 2;
+  };
+};
+
+#endif  // AUTOPAS_TRAVERSALRACECONDITIONTEST_H
