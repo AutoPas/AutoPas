46527983a1d0c6dfb75dc0988260fbe49753fa9c
Steffen Seckler <steffen.seckler@tum.de>
2018-04-19 16:38:14 +0900

    adds working generation of verletlists in VerletLists.h + unit tests

--- src/containers/VerletLists.h
+++ src/containers/VerletLists.h
@@ -18,18 +18,24 @@ namespace autopas {
  */
 template <class Particle, class ParticleCell>
 class VerletLists : public LinkedCells<Particle, ParticleCell> {
+  typedef std::vector<std::vector<size_t>> verletlist_storage_type;
+  typedef std::map<decltype(Particle().getID()), size_t>
+      particleid_to_verletlist_index_map_type;
+
  public:
   /**
    * Constructor of the VerletLists class
    * @param boxMin
    * @param boxMax
    * @param cutoff
+   * @param skin
    */
   VerletLists(const std::array<double, 3> boxMin,
-              const std::array<double, 3> boxMax, double cutoff)
-      : LinkedCells<Particle, ParticleCell>(boxMin, boxMax, cutoff) {}
+              const std::array<double, 3> boxMax, double cutoff, double skin)
+      : LinkedCells<Particle, ParticleCell>(boxMin, boxMax, cutoff + skin),
+        _skin(skin) {}
 
-  void iteratePairwiseAoS(Functor<Particle, ParticleCell> *f,
+  void iteratePairwiseAoS(Functor<Particle, ParticleCell>* f,
                           bool useNewton3 = true) override {
     iteratePairwiseAoS2(f, useNewton3);
   }
@@ -43,7 +49,7 @@ class VerletLists : public LinkedCells<Particle, ParticleCell> {
    * @param useNewton3 defines whether newton3 should be used
    */
   template <class ParticleFunctor>
-  void iteratePairwiseAoS2(ParticleFunctor *f, bool useNewton3 = true) {
+  void iteratePairwiseAoS2(ParticleFunctor* f, bool useNewton3 = true) {
     if (useNewton3) {
       this->updateVerletListsN3();
       CellFunctor<Particle, ParticleCell, ParticleFunctor, false, true>
@@ -70,24 +76,71 @@ class VerletLists : public LinkedCells<Particle, ParticleCell> {
     }
   }
 
+  verletlist_storage_type& getVerletLists(){
+    return _verletLists;
+  }
+
  private:
   class VerletListGeneratorFunctor
-      : public autopas::Functor<Particle, ParticleCell> {};
+      : public autopas::Functor<Particle, ParticleCell> {
+   public:
+    VerletListGeneratorFunctor(
+        verletlist_storage_type& verletLists,
+        particleid_to_verletlist_index_map_type& particleIDtoVerletListIndexMap,
+        double cutoffskinsquared)
+        : _verletLists(verletLists),
+          _particleIDtoVerletListIndexMap(particleIDtoVerletListIndexMap),
+          _cutoffskinsquared(cutoffskinsquared) {}
+
+    void AoSFunctor(Particle& i, Particle& j, bool newton3 = true) override {
+      auto dist = arrayMath::sub(i.getR(), j.getR());
+      double distsquare = arrayMath::dot(dist, dist);
+
+      if (distsquare < _cutoffskinsquared)
+        _verletLists[_particleIDtoVerletListIndexMap[i.getID()]].push_back(
+            _particleIDtoVerletListIndexMap[j.getID()]);
+    }
+
+   private:
+    verletlist_storage_type& _verletLists;
+    particleid_to_verletlist_index_map_type& _particleIDtoVerletListIndexMap;
+    double _cutoffskinsquared;
+  };
 
   void updateVerletListsN3() {
-    VerletListGeneratorFunctor f;
-    CellFunctor<Particle, ParticleCell, VerletListGeneratorFunctor, false, true>
-        cellFunctor(&f);
-    //		cellFunctor.processCellAoSN3(this->_data[13]);
-    SlicedTraversal<ParticleCell,
-                    CellFunctor<Particle, ParticleCell,
-                                VerletListGeneratorFunctor, false, true>>
-        traversal(this->_data, this->_cellBlock.getCellsPerDimensionWithHalo(),
-                  &cellFunctor);
+    size_t particleNumber = updateIdMap();
+    _verletLists.clear();
+    _verletLists.resize(particleNumber);
+    VerletListGeneratorFunctor f(_verletLists, _particleIDtoVerletListIndexMap,
+                                 (this->getCutoff() * this->getCutoff()));
 
-    traversal.traverseCellPairs();
+    LinkedCells<Particle, ParticleCell>::iteratePairwiseAoS2(&f,true);
   }
-  // ThreeDimensionalCellHandler
+
+  size_t updateIdMap() {
+    _particleIDtoVerletListIndexMap.clear();  // this is not necessary, but it
+                                              // does not hurt too much,
+                                              // probably...
+    size_t i = 0;
+    for (auto iter = this->begin(); iter.isValid(); ++iter, ++i) {
+      _particleIDtoVerletListIndexMap[iter->getID()] = i;
+    }
+
+    return i;
+  }
+
+  /// map that converts the id type of the particle to the actual position in
+  /// the verlet list.
+  /// This is needed, as the particles don't have a local id.
+  /// @todo remove this and add local id to the particles (this saves a
+  /// relatively costly lookup)
+  particleid_to_verletlist_index_map_type _particleIDtoVerletListIndexMap;
+
+  /// verlet lists.
+  verletlist_storage_type _verletLists;
+
+  double _skin;
+
 };
 
 } /* namespace autopas */
--- tests/testAutopas/VerletListsTest.cpp
+++ tests/testAutopas/VerletListsTest.cpp
@@ -10,6 +10,108 @@ TEST_F(VerletListsTest, VerletListConstructor) {
   std::array<double, 3> min = {1, 1, 1};
   std::array<double, 3> max = {3, 3, 3};
   double cutoff = 1.;
+  double skin = 0.2;
+  autopas::VerletLists<autopas::Particle,
+                       autopas::FullParticleCell<autopas::Particle>>
+      verletLists(min, max, cutoff, skin);
+}
 
-  autopas::VerletLists<autopas::Particle, autopas::FullParticleCell<autopas::Particle>> verletLists(min, max, cutoff);
+TEST_F(VerletListsTest, testVerletListBuild) {
+  std::array<double, 3> min = {1, 1, 1};
+  std::array<double, 3> max = {3, 3, 3};
+  double cutoff = 1.;
+  double skin = 0.2;
+  autopas::VerletLists<autopas::Particle,
+                       autopas::FullParticleCell<autopas::Particle>>
+      verletLists(min, max, cutoff, skin);
+
+  std::array<double, 3> r = {2, 2, 2};
+  autopas::Particle p(r, {0., 0., 0.}, 0);
+  verletLists.addParticle(p);
+  std::array<double, 3> r2 = {1.5, 2, 2};
+  autopas::Particle p2(r2, {0., 0., 0.}, 1);
+  verletLists.addParticle(p2);
+
+  autopas::Functor<autopas::Particle,
+                   autopas::FullParticleCell<autopas::Particle>>
+      emptyFunctor;
+  verletLists.iteratePairwiseAoS2(&emptyFunctor, true);
+
+  auto& list = verletLists.getVerletLists();
+
+  EXPECT_EQ(list.size(), 2);
+  ASSERT_EQ(list[0].size() + list[1].size(), 1);
+  if (list[0].size() == 1) {
+    EXPECT_EQ(list[0][0], 1);
+  } else if (list[1].size() == 1) {
+    EXPECT_EQ(list[1][0], 0);
+  }
+}
+
+TEST_F(VerletListsTest, testVerletListBuildTwice) {
+  std::array<double, 3> min = {1, 1, 1};
+  std::array<double, 3> max = {3, 3, 3};
+  double cutoff = 1.;
+  double skin = 0.2;
+  autopas::VerletLists<autopas::Particle,
+                       autopas::FullParticleCell<autopas::Particle>>
+      verletLists(min, max, cutoff, skin);
+
+  std::array<double, 3> r = {2, 2, 2};
+  autopas::Particle p(r, {0., 0., 0.}, 0);
+  verletLists.addParticle(p);
+  std::array<double, 3> r2 = {1.5, 2, 2};
+  autopas::Particle p2(r2, {0., 0., 0.}, 1);
+  verletLists.addParticle(p2);
+
+  autopas::Functor<autopas::Particle,
+                   autopas::FullParticleCell<autopas::Particle>>
+      emptyFunctor;
+  verletLists.iteratePairwiseAoS2(&emptyFunctor, true);
+
+  verletLists.iteratePairwiseAoS2(&emptyFunctor, true);
+
+  auto& list = verletLists.getVerletLists();
+
+  EXPECT_EQ(list.size(), 2);
+  ASSERT_EQ(list[0].size() + list[1].size(), 1);
+  if (list[0].size() == 1) {
+    EXPECT_EQ(list[0][0], 1);
+  } else if (list[1].size() == 1) {
+    EXPECT_EQ(list[1][0], 0);
+  }
+}
+
+
+TEST_F(VerletListsTest, testVerletListBuildFarAway) {
+  std::array<double, 3> min = {1, 1, 1};
+  std::array<double, 3> max = {5, 5, 5};
+  double cutoff = 1.;
+  double skin = 0.2;
+  autopas::VerletLists<autopas::Particle,
+                       autopas::FullParticleCell<autopas::Particle>>
+      verletLists(min, max, cutoff, skin);
+
+  std::array<double, 3> r = {2, 2, 2};
+  autopas::Particle p(r, {0., 0., 0.}, 0);
+  verletLists.addParticle(p);
+
+  std::array<double, 3> r2 = {1.5, 2, 2};
+  autopas::Particle p2(r2, {0., 0., 0.}, 1);
+  verletLists.addParticle(p2);
+
+  std::array<double, 3> r3 = {4.5, 4.5, 4.5};
+  autopas::Particle p3(r3, {0., 0., 0.}, 2);
+  verletLists.addParticle(p3);
+
+  autopas::Functor<autopas::Particle,
+                   autopas::FullParticleCell<autopas::Particle>>
+      emptyFunctor;
+  verletLists.iteratePairwiseAoS2(&emptyFunctor, true);
+
+  auto& list = verletLists.getVerletLists();
+
+  EXPECT_EQ(list.size(), 3);
+  EXPECT_EQ(list[0].size() + list[1].size() + list[2].size(), 1);
+
 }
