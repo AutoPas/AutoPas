62f02a1455f7ccccf219c68dc7fff237b63d607d
Steffen Seckler <steffen.seckler@tum.de>
2018-05-24 11:02:04 +0900

    fixes warning of gcc8

--- src/iterators/ParticleIterator.h
+++ src/iterators/ParticleIterator.h
@@ -154,6 +154,8 @@ class ParticleIterator {
       case ownedOnly:
         return _flagManager->isOwningCell(_iteratorAcrossCells -
                                           _vectorOfCells->begin());
+      default:
+        utils::ExceptionHandler::exception("unknown iterator behavior");
     }
   }
 
