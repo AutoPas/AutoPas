7e0e8f1e5993784056b16dd71abb9059d44bd8eb
Steffen Seckler <steffen.seckler@tum.de>
2018-05-25 18:53:16 +0900

    adds a test the should be reported by archer

--- tests/testAutopas/ArcherShouldReportThis.cpp
+++ tests/testAutopas/ArcherShouldReportThis.cpp
@@ -4,4 +4,21 @@
  * @date 25.05.18
  */
 
+#include <gtest/gtest.h>
 
+#define N 100
+
+#ifdef AUTOPAS_OPENMP
+TEST(ArcherShouldReportThis, test) {
+  int a[N];
+  for(int i=0;i<N;i++){
+    a[i]=i%7;
+  }
+
+#pragma omp parallel for
+  for (int i = 0; i < N - 1; i++) {
+    a[i] = a[i + 1];
+  }
+  std::cout << "a[80]" << a[80] << std::endl;
+}
+#endif
