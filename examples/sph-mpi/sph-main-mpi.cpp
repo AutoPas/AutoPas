7d997c699c1da485e91fd02aabe5b488fd2c8e26
Steffen Seckler <steffen.seckler@tum.de>
2018-06-01 13:13:13 +0900

    modification to sph-main-mpi to only print on rank 0

--- examples/sph-mpi/sph-main-mpi.cpp
+++ examples/sph-mpi/sph-main-mpi.cpp
@@ -520,8 +520,12 @@ int main(int argc, char* argv[]) {
 
   // 1 ---- START MAIN LOOP ----
   size_t step = 0;
+  int rank;
+  MPI_Comm_rank(comm, &rank);
   for (double time = 0.; time < t_end && step < 55; time += dt, ++step) {
-    std::cout << "\n-------------------------\ntime step " << step << "(t = " << time << ")..." << std::endl;
+    if (rank == 0) {
+      std::cout << "\n-------------------------\ntime step " << step << "(t = " << time << ")..." << std::endl;
+    }
     // 1.1 Leap frog: Initial Kick & Full Drift
     leapfrogInitialKick(sphSystem, dt);
     leapfrogFullDrift(sphSystem, dt);  // changes position
