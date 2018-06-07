ecd1cb91bd16aa3ca827df5dcd2a4464bb194231
Steffen Seckler <steffen.seckler@tum.de>
2018-06-07 17:23:04 +0900

    briefs and details for namespaces

--- src/namespaces.h
+++ src/namespaces.h
@@ -9,25 +9,26 @@
 #pragma once
 
 /**
- * This is the main namespace of AutoPas. Everything defined within AutoPas directory should be in this namespace.
+ * @brief This is the main namespace of AutoPas.
+ * @details Everything defined within AutoPas should be in this namespace.
  */
 namespace autopas {
 
 /**
- * This namespace is used for implementation specifics.
- * If you are a developer of AutoPas you might want to take a look inside here.
+ * @brief This namespace is used for implementation specifics.
+ * @details If you are a developer of AutoPas you might want to take a look inside here.
  */
 namespace internal {}  // namespace internal
 
 /**
- * In this namespace all functionality of the Smoothed Particle Hydrodynamics module of autopas is present.
- * This mainly includes kernels and particles.
+ * @brief In this namespace all functionality of the Smoothed Particle Hydrodynamics module of autopas is present.
+ * @details This mainly includes kernels and particles.
  */
 namespace sph {}  // namespace sph
 
 /**
- * In this namespace some helper classes and functions can be found used inside of AutoPas.
- * These classes reside mostly in the utils directory. However, most commonly used function inside the utils directory
+ * @brief In this namespace some helper classes and functions can be found used inside of AutoPas.
+ * @details These classes reside mostly in the utils directory. However, most commonly used function inside the utils directory
  * might not be in the utils namespace.
  */
 namespace utils {}  // namespace utils
