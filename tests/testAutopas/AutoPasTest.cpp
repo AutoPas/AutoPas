51e904803c6a5a19107a9f40ab53e88598d13da1
Steffen Seckler <steffen.seckler@tum.de>
2018-06-07 17:29:14 +0900

    comments

--- tests/testAutopas/AutoPasTest.cpp
+++ tests/testAutopas/AutoPasTest.cpp
@@ -7,6 +7,10 @@
 #include "AutoPasTest.h"
 #include "AutoPas.h"
 
+/**
+ * test whether the RegionIterator can be generated and something is returned.
+ * This mainly makes certain, that the specific parts of the code can be compiled.
+ */
 TEST_F(AutoPasTest, getRegionParticleIterator) {
   auto iter = autoPas.getRegionIterator({0., 0., 0.}, {4., 4., 4.});
 
