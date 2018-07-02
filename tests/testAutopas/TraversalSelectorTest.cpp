fe8c6b3a1cab49cbefa4c7d57e65ea75fbbc1f5f
FG-TUM <f.gratl@tum.de>
2018-07-02 16:28:15 +0200

    more resilient selector test

--- tests/testAutopas/TraversalSelectorTest.cpp
+++ tests/testAutopas/TraversalSelectorTest.cpp
@@ -15,15 +15,19 @@ TEST(TraversalSelectorTest, testGetOptimalTraversalOneOption) {
   std::vector<autopas::TraversalOptions> optionVectorC08 = {autopas::TraversalOptions::c08};
   std::vector<autopas::TraversalOptions> optionVectorSliced = {autopas::TraversalOptions::sliced};
 
-  autopas::TraversalSelector<FPCell> traversalSelectorC08({30, 30, 30}, optionVectorC08);
-  autopas::TraversalSelector<FPCell> traversalSelectorSlice({30, 30, 30}, optionVectorSliced);
+  // this should be high enough so that sliced is still valid for the current processors thread count.
+  constexpr size_t domainSize = 1000;
+
+  autopas::TraversalSelector<FPCell> traversalSelectorC08({domainSize, domainSize, domainSize}, optionVectorC08);
+  autopas::TraversalSelector<FPCell> traversalSelectorSlice({domainSize, domainSize, domainSize}, optionVectorSliced);
 
   auto traversalC08 = traversalSelectorC08.getOptimalTraversal(cf);
   auto traversalSlice = traversalSelectorSlice.getOptimalTraversal(cf);
 
   // check that traversals are of the expected type
   ASSERT_TRUE((dynamic_cast<autopas::C08Traversal<FPCell, CellFunctorAoSN3> *>(traversalC08.get())));
-  ASSERT_TRUE((dynamic_cast<autopas::SlicedTraversal<FPCell, CellFunctorAoSN3> *>(traversalSlice.get())));
+  ASSERT_TRUE((dynamic_cast<autopas::SlicedTraversal<FPCell, CellFunctorAoSN3> *>(traversalSlice.get())))
+                << "Is the domain size large enough for the processors' thread count?";
 }
 
 TEST(TraversalSelectorTest, testGetOptimalTraversalBadFirstOption) {
