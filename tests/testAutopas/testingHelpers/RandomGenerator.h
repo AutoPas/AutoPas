60c93678f09713018601f0ef94383cd7910fd311
FG-TUM <f.gratl@tum.de>
2018-07-05 13:48:17 +0200

    added halo filler

--- tests/testAutopas/testingHelpers/RandomGenerator.h
+++ tests/testAutopas/testingHelpers/RandomGenerator.h
@@ -6,6 +6,7 @@
 
 #pragma once
 
+#include <utils/inBox.h>
 #include <array>
 
 class RandomGenerator {
@@ -38,6 +39,31 @@ class RandomGenerator {
       particle.setR(randomPosition(boxMin, boxMax));
       particle.setID(id);
       container.addParticle(particle);
+    }
+  }
+
+  template <class Container, class Particle>
+  static void fillWithHaloParticles(Container& container, Particle defaultParticle, double haloWidth, unsigned long numParticles = 100) {
+    srand(42);  // fixed seedpoint
+
+    auto haloBoxMin = container.getBoxMin();
+    auto haloBoxMax = container.getBoxMax();
+
+    // increase the box size not exactly by the width to make it exclusive
+    for (int i = 0; i < 3; ++i) {
+      haloBoxMin[i] -= haloWidth * .99;
+      haloBoxMax[i] += haloWidth * .99;
+    }
+
+    for (unsigned long i = 0; i < numParticles;) {
+      Particle particle = defaultParticle;
+      auto pos = randomPosition(haloBoxMax, haloBoxMax);
+      // we only want  to add particles not in the actual box
+      if (autopas::inBox(pos, container.getBoxMin(), container.getBoxMax())) continue;
+      particle.setR(pos);
+      particle.setID(i);
+      container.addHaloParticle(particle);
+      ++i;
     }
   }
 };
