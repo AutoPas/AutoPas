c76b052bc0ed064932a7136c9965cc01d8ac7e60
FG-TUM <f.gratl@tum.de>
2018-08-02 09:49:41 +0200

    quotes around column header

--- examples/md/measurePerf.sh
+++ examples/md/measurePerf.sh
@@ -22,21 +22,21 @@ done
 iCont=2;
 	echo "Container ${iCont}:"
 	# iterate over molecules with the correct repetition
-	echo -e "Number of Molecules\tNumber of Force updates\tElapsed time\tMFUPS\tFLOPs\thit rate\tGFLOP/sec" > output-${iCont}-verlet-1-0.0.txt
+	echo -e "\"Number of Molecules\"\t\"Number of Force updates\"\t\"Elapsed time\"\t\"MFUPS\"\t\"FLOPs\"\t\"hit rate\"\t\"GFLOP/sec\"" > output-${iCont}-verlet-1-0.0.txt
 	for i in {0..8}; do ./md-main ${iCont} ${Mols[$i]} ${Reps[$i]} 1 0.; done >> output-${iCont}-verlet-1-0.0.txt
-    echo -e "Number of Molecules\tNumber of Force updates\tElapsed time\tMFUPS\tFLOPs\thit rate\tGFLOP/sec" > output-${iCont}-verlet-5-0.1.txt
+    echo -e "\"Number of Molecules\"\t\"Number of Force updates\"\t\"Elapsed time\"\t\"MFUPS\"\t\"FLOPs\"\t\"hit rate\"\t\"GFLOP/sec\"" > output-${iCont}-verlet-5-0.1.txt
 	for i in {0..8}; do ./md-main ${iCont} ${Mols[$i]} ${Reps[$i]} 5 0.1; done >> output-${iCont}-verlet-5-0.1.txt
-    echo -e "Number of Molecules\tNumber of Force updates\tElapsed time\tMFUPS\tFLOPs\thit rate\tGFLOP/sec" > output-${iCont}-verlet-10-0.2.txt
+    echo -e "\"Number of Molecules\"\t\"Number of Force updates\"\t\"Elapsed time\"\t\"MFUPS\"\t\"FLOPs\"\t\"hit rate\"\t\"GFLOP/sec\"" > output-${iCont}-verlet-10-0.2.txt
 	for i in {0..8}; do ./md-main ${iCont} ${Mols[$i]} ${Reps[$i]} 10 0.2; done >> output-${iCont}-verlet-10-0.2.txt
-    echo -e "Number of Molecules\tNumber of Force updates\tElapsed time\tMFUPS\tFLOPs\thit rate\tGFLOP/sec" > output-${iCont}-verlet-20-0.3.txt
+    echo -e "\"Number of Molecules\"\t\"Number of Force updates\"\t\"Elapsed time\"\t\"MFUPS\"\t\"FLOPs\"\t\"hit rate\"\t\"GFLOP/sec\"" > output-${iCont}-verlet-20-0.3.txt
 	for i in {0..8}; do ./md-main ${iCont} ${Mols[$i]} ${Reps[$i]} 20 0.3; done >> output-${iCont}-verlet-20-0.3.txt
 
 	echo "Container ${iCont} soa:"
-    echo -e "Number of Molecules\tNumber of Force updates\tElapsed time\tMFUPS\tFLOPs\thit rate\tGFLOP/sec" > output-${iCont}-verlet-1-0.0-soa.txt
+    echo -e "\"Number of Molecules\"\t\"Number of Force updates\"\t\"Elapsed time\"\t\"MFUPS\"\t\"FLOPs\"\t\"hit rate\"\t\"GFLOP/sec\"" > output-${iCont}-verlet-1-0.0-soa.txt
 	for i in {0..8}; do ./md-main ${iCont} ${Mols[$i]} ${Reps[$i]} 1 0. soa; done >> output-${iCont}-verlet-1-0.0-soa.txt
-    echo -e "Number of Molecules\tNumber of Force updates\tElapsed time\tMFUPS\tFLOPs\thit rate\tGFLOP/sec" > output-${iCont}-verlet-5-0.1-soa.txt
+    echo -e "\"Number of Molecules\"\t\"Number of Force updates\"\t\"Elapsed time\"\t\"MFUPS\"\t\"FLOPs\"\t\"hit rate\"\t\"GFLOP/sec\"" > output-${iCont}-verlet-5-0.1-soa.txt
 	for i in {0..8}; do ./md-main ${iCont} ${Mols[$i]} ${Reps[$i]} 5 0.1 soa; done >> output-${iCont}-verlet-5-0.1-soa.txt
-    echo -e "Number of Molecules\tNumber of Force updates\tElapsed time\tMFUPS\tFLOPs\thit rate\tGFLOP/sec" > output-${iCont}-verlet-10-0.2-soa.txt
+    echo -e "\"Number of Molecules\"\t\"Number of Force updates\"\t\"Elapsed time\"\t\"MFUPS\"\t\"FLOPs\"\t\"hit rate\"\t\"GFLOP/sec\"" > output-${iCont}-verlet-10-0.2-soa.txt
 	for i in {0..8}; do ./md-main ${iCont} ${Mols[$i]} ${Reps[$i]} 10 0.2 soa; done >> output-${iCont}-verlet-10-0.2-soa.txt
-    echo -e "Number of Molecules\tNumber of Force updates\tElapsed time\tMFUPS\tFLOPs\thit rate\tGFLOP/sec" > output-${iCont}-verlet-20-0.3-soa.txt
+    echo -e "\"Number of Molecules\"\t\"Number of Force updates\"\t\"Elapsed time\"\t\"MFUPS\"\t\"FLOPs\"\t\"hit rate\"\t\"GFLOP/sec\"" > output-${iCont}-verlet-20-0.3-soa.txt
 	for i in {0..8}; do ./md-main ${iCont} ${Mols[$i]} ${Reps[$i]} 20 0.3 soa; done >> output-${iCont}-verlet-20-0.3-soa.txt
