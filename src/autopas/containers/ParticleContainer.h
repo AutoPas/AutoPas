ffc470c92f8a366e6fb89fede688dd4231d7246d
seckler <steffen.seckler@tum.de>
2018-09-24 16:30:09 +0200

    openmp parallelization of ParticleContainer

--- src/autopas/containers/ParticleContainer.h
+++ src/autopas/containers/ParticleContainer.h
@@ -137,6 +137,13 @@ class ParticleContainer : public ParticleContainerInterface<Particle, ParticleCe
 
   unsigned long getNumParticles() override {
     size_t numParticles = 0ul;
+#ifdef AUTOPAS_OPENMP
+    // @todo: find a sensible value for magic number
+    // numThreads should be at least 1 and maximal max_threads
+    int numThreads = std::max(1, std::min(omp_get_max_threads(), (int)(this->_cells.size() / 1000)));
+    AutoPasLog(trace, "Using {} threads", numThreads);
+#pragma omp parallel for num_threads(numThreads) reduction(+:numParticles)
+#endif
     for(size_t index = 0; index < _cells.size(); ++index){
       numParticles += _cells[index].numParticles();
     }
