6838fac3b132716e7661386fd1d01f3dfbc18fc2
FG-TUM <f.gratl@tum.de>
2018-11-09 09:57:38 +0100

    helping lazy ppl

--- examples/md-flexible/MDFlexParser.cpp
+++ examples/md-flexible/MDFlexParser.cpp
@@ -48,7 +48,7 @@ bool MDFlexParser::parseInput(int argc, char **argv) {
       case 'c': {
         // delete default argument
         containerOptions.clear();
-        if (strArg.find("direct") != string::npos) {
+        if (strArg.find("direct") != string::npos or strArg.find("ds") != string::npos) {
           containerOptions.push_back(autopas::directSum);
         }
         if (strArg.find("linked") != string::npos or strArg.find("lc") != string::npos) {
