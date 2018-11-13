9cbaee1904c92dd9bb0fc13228c5a807bb6e3ae2
FG-TUM <f.gratl@tum.de>
2018-11-13 17:07:06 +0100

    Logging and openmp now do not mess with cmake variables

--- cmake/modules/OpenMP.cmake
+++ cmake/modules/OpenMP.cmake
@@ -2,7 +2,6 @@ option(OPENMP "Activates OpenMP shared memory parallelization." OFF)
 
 if (CMAKE_CXX_COMPILER MATCHES "archer")
     message(STATUS "archer detected, OpenMP enabled by default, so skipping OpenMP package search")
-    add_definitions(-DAUTOPAS_OPENMP)
     set(OPENMP ON)
     return()
 endif()
@@ -12,10 +11,18 @@ if (OPENMP)
     find_package(OpenMP)
 
     if (OPENMP_FOUND)
-        set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
-        set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-        set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
-        add_definitions(-DAUTOPAS_OPENMP)
+
+        # For CMake < 3.9, we need to make the target ourselves
+        if(NOT TARGET OpenMP::OpenMP_CXX)
+            find_package(Threads REQUIRED)
+            add_library(OpenMP::OpenMP_CXX IMPORTED INTERFACE)
+            set_property(TARGET OpenMP::OpenMP_CXX
+                    PROPERTY INTERFACE_COMPILE_OPTIONS ${OpenMP_CXX_FLAGS})
+            # Only works if the same flag is passed to the linker; use CMake 3.9+ otherwise (Intel, AppleClang)
+            set_property(TARGET OpenMP::OpenMP_CXX
+                    PROPERTY INTERFACE_LINK_LIBRARIES ${OpenMP_CXX_FLAGS} Threads::Threads)
+
+        endif()
     else()
         message(FATAL_ERROR "Could not find OpenMP!")
     endif()
--- cmake/modules/logging.cmake
+++ cmake/modules/logging.cmake
@@ -1,6 +1,2 @@
 # option for more verbose log messages.
 option(VERBOSE_LOGGING "Print Filename and line in every log line." OFF)
-target_compile_definitions(autopas
-        INTERFACE
-        $<$<BOOL:${VERBOSE_LOGGING}>:AUTOPAS_VERBOSE_LOG>
-)
