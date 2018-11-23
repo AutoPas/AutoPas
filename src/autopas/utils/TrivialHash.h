91c7e2c92661600f5b79c608910b99e49029ca10
FG-TUM <f.gratl@tum.de>
2018-11-23 14:28:42 +0100

    documentation

--- src/autopas/utils/TrivialHash.h
+++ src/autopas/utils/TrivialHash.h
@@ -11,7 +11,16 @@
 
 namespace autopas {
 
+/**
+ * Trivial hash for enums.
+ */
 struct TrivialHash {
+  /**
+   * Trivial hash function
+   * @tparam T
+   * @param t
+   * @return Integer representation of t
+   */
   template <typename T>
   std::size_t operator()(T t) const {
     return static_cast<std::size_t>(t);
