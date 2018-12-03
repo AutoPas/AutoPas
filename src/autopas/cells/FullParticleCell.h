f8a205f6f0fcf4e9a64a18090dc6235fd26da830
SteffenSeckler <steffen.seckler@tum.de>
2018-12-03 14:30:20 +0100

    cherry pick: copy/move/assignment operators/constructors for fullparticlecell

--- src/autopas/cells/FullParticleCell.h
+++ src/autopas/cells/FullParticleCell.h
@@ -25,7 +25,39 @@ class FullParticleCell : public ParticleCell<Particle> {
 
   ~FullParticleCell() { autopas_destroy_lock(&particlesLock); }
 
-  void addParticle(Particle &m) override {
+  /**
+   * Move Constructor
+   * @param other
+   */
+  FullParticleCell(FullParticleCell&& other) {
+    _particles = std::move(other._particles);
+    _particleSoABuffer = std::move(other._particleSoABuffer);
+    autopas_init_lock(&particlesLock);
+  }
+
+  /**
+   * Copy constructor
+   * @param other
+   */
+  FullParticleCell(const FullParticleCell& other) {
+    _particles = other._particles;
+    _particleSoABuffer = other._particleSoABuffer;
+    autopas_init_lock(&particlesLock);
+  }
+
+  /**
+   * Assignment operator
+   * @param other
+   * @return
+   */
+  FullParticleCell& operator=(FullParticleCell& other) {
+    _particles = other._particles;
+    _particleSoABuffer = other._particleSoABuffer;
+    autopas_init_lock(&particlesLock);
+    return *this;
+  }
+
+  void addParticle(Particle& m) override {
     autopas_set_lock(&particlesLock);
     _particles.push_back(m);
     autopas_unset_lock(&particlesLock);
@@ -36,6 +68,13 @@ class FullParticleCell : public ParticleCell<Particle> {
   }
 
   unsigned long numParticles() const override { return _particles.size(); }
+
+  /**
+   * Returns a reference to the element at position n in the container
+   * @param n Position of an element in the container
+   * @return Reference to the element
+   */
+  Particle& operator[](size_t n) { return _particles[n]; }
 
   bool isNotEmpty() const override { return numParticles() > 0; }
 
@@ -51,6 +90,27 @@ class FullParticleCell : public ParticleCell<Particle> {
     _particles.pop_back();
     autopas_unset_lock(&particlesLock);
   }
+
+  /**
+   * Resizes the container so that it contains n elements.
+   * @param n New container size
+   */
+  void resize(size_t n) { _particles.resize(n); }
+
+  /**
+   * Sort the particles in the cell by a dimension
+   * @param dim dimension to sort
+   */
+  void sortByDim(const size_t dim) {
+    std::sort(_particles.begin(), _particles.end(),
+              [dim](const Particle& a, const Particle& b) -> bool { return a.getR()[dim] < b.getR()[dim]; });
+  }
+
+  /**
+   * Requests that the vector capacity be at least enough to contain n elements.
+   * @param n Minimum capacity for the vector.
+   */
+  void reserve(size_t n) { _particles.reserve(n); }
 
   /**
    * storage of the molecules of the cell
@@ -70,5 +130,4 @@ class FullParticleCell : public ParticleCell<Particle> {
  private:
   autopas_lock_t particlesLock;
 };
-
 }  // namespace autopas
