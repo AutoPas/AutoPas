fc0a25bc63ec161353a17b81a3b71bc1eeaef7f4
SteffenSeckler <steffen.seckler@tum.de>
2018-12-04 17:15:43 +0100

    clang format

--- tests/testAutopas/allTests.cpp
+++ tests/testAutopas/allTests.cpp
@@ -9,6 +9,6 @@
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   // set the gtest death test style to threadsafe
-  testing::FLAGS_gtest_death_test_style="threadsafe";
+  testing::FLAGS_gtest_death_test_style = "threadsafe";
   return RUN_ALL_TESTS();
 }
