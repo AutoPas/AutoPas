edfed83586e833f864d574283a4ee2875e2c18a0
Steffen Seckler <38119442+SteffenSeckler@users.noreply.github.com>
2018-12-05 13:48:58 +0100

    added dot
    Co-Authored-By: FG-TUM <FG-TUM@users.noreply.github.com>

--- src/autopas/selectors/TraversalSelector.h
+++ src/autopas/selectors/TraversalSelector.h
@@ -333,7 +333,7 @@ void TraversalSelector<ParticleCell>::findFastestMedianTraversal() {
   std::unordered_map<TraversalOptions, std::vector<long>, TrivialHash> measurementsMap;
   AutoPasLog(debug, "TraversalSelector: Collected traversal times:");
   for (auto &&t : _traversalTimes) {
-    AutoPasLog(debug, "Traversal {} took {} nanoseconds", t.traversal, t.time);
+    AutoPasLog(debug, "Traversal {} took {} nanoseconds.", t.traversal, t.time);
     measurementsMap[t.traversal].push_back(t.time);
   }
 
