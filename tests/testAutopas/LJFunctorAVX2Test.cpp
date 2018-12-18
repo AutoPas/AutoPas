3be2454bc6e20453b4c54ee614dff42bdc193be3
FG-TUM <f.gratl@tum.de>
2018-12-18 10:13:39 +0100

    wip

--- tests/testAutopas/LJFunctorAVX2Test.cpp
+++ tests/testAutopas/LJFunctorAVX2Test.cpp
@@ -6,6 +6,7 @@
 
 #include "LJFunctorAVX2Test.h"
 #include <autopas/cells/FullParticleCell.h>
+#include <autopas/pairwiseFunctors/LJFunctorAVX2.h>
 #include <autopas/particles/Particle.h>
 #include <autopas/utils/SoA.h>
 #include <testingHelpers/RandomGenerator.h>
@@ -13,12 +14,22 @@
 
 TEST_F(LJFunctorAVX2Test, testLJFunctorVSLJFunctorAVX2) {
   //  auto soa1 = autopas::Particle::SoAArraysType();
-  FPCell cell1NoAVX2;
-  FPCell cell2NoAVX2;
   FPCell cell1AVX2;
   FPCell cell2AVX2;
 
   Particle defaultParticle;
-  RandomGenerator::fillWithParticles(cell1AVX2, defaultParticle, {0, 0, 0}, {1, 1, 1}, 8);
-  RandomGenerator::fillWithParticles(cell2AVX2, defaultParticle, {1, 0, 0}, {2, 1, 1}, 8);
+  RandomGenerator::fillWithParticles(cell1AVX2, defaultParticle, _lowCorner,
+                                     {_lowCorner[0] / 2, _lowCorner[1], _lowCorner[2]}, 8);
+  RandomGenerator::fillWithParticles(cell2AVX2, defaultParticle, {_lowCorner[0] / 2, _lowCorner[1], _lowCorner[2]},
+                                     _highCorner, 8);
+
+  // copy cells
+  FPCell cell1NoAVX2(cell1AVX2);
+  FPCell cell2NoAVX2(cell2AVX2);
+
+  autopas::LJFunctor<Particle, FPCell, false, false> ljFunctor(_cutoff, _epsilon, _sigma, 0.0, _lowCorner, _highCorner);
+  autopas::LJFunctorAVX2<Particle, FPCell, false, false> ljFunctorAVX2(_cutoff, _epsilon, _sigma, 0.0, _lowCorner,
+                                                                       _highCorner);
+
+  ljFunctor.S
 }
--- tests/testAutopas/LJFunctorAVX2Test.h
+++ tests/testAutopas/LJFunctorAVX2Test.h
@@ -9,11 +9,13 @@
 #include "AutoPasTestBase.h"
 
 class LJFunctorAVX2Test : public AutoPasTestBase {
-
  public:
-  LJFunctorAVX2Test() : AutoPasTestBase(), _cutoff(1.), _epsilon(1.), _sigma(1.) {}
+  LJFunctorAVX2Test()
+      : AutoPasTestBase(), _cutoff{1.}, _epsilon{1.}, _sigma{1.}, _lowCorner{0, 0, 0}, _highCorner{2, 1, 1} {}
 
   const double _cutoff;
   const double _epsilon;
   const double _sigma;
+  const std::array<double, 3> _lowCorner;
+  const std::array<double, 3> _highCorner;
 };
