756cf8995c505c3c7670603f8950156f2545fef6
SteffenSeckler <steffen.seckler@tum.de>
2019-01-11 12:13:02 +0100

    refactoring and fix innerCellNumber

--- tests/testAutopas/C18TraversalTest.cpp
+++ tests/testAutopas/C18TraversalTest.cpp
@@ -9,9 +9,7 @@
 using ::testing::_;
 using ::testing::AtLeast;
 
-TEST_F(C18TraversalTest, testTraversalCube) {
-  std::array<size_t, 3> edgeLength = {2, 2, 2};
-
+void testTraversal(const std::array<size_t, 3>& edgeLength){
   MFunctor functor;
   std::vector<FPCell> cells;
   cells.resize(edgeLength[0] * edgeLength[1] * edgeLength[2]);
@@ -30,7 +28,7 @@ TEST_F(C18TraversalTest, testTraversalCube) {
   size_t boundaryXinteractions = faceXcells * (9 + 8);
   size_t faceYcells = (edgeLength[0] - 2) * (edgeLength[2] - 1);
   size_t boundaryYinteractions = faceYcells * (7 + 10);
-  size_t innercells = (edgeLength[0] - 2) * (edgeLength[1] - 2) * (edgeLength[2] - 2);
+  size_t innercells = (edgeLength[0] - 2) * (edgeLength[1] - 2) * (edgeLength[2] - 1);
   size_t innercellinteractions = innercells * 13;
 
   EXPECT_CALL(functor, AoSFunctor(_, _, true))
@@ -40,72 +38,24 @@ TEST_F(C18TraversalTest, testTraversalCube) {
 #ifdef AUTOPAS_OPENMP
   omp_set_num_threads(numThreadsBefore);
 #endif
+}
+
+TEST_F(C18TraversalTest, testTraversal10x10x10) {
+  std::array<size_t, 3> edgeLength = {10, 10, 10};
+  testTraversal(edgeLength);
 }
 
 TEST_F(C18TraversalTest, testTraversal2x2x2) {
   std::array<size_t, 3> edgeLength = {2, 2, 2};
-
-  MFunctor functor;
-  std::vector<FPCell> cells;
-  cells.resize(edgeLength[0] * edgeLength[1] * edgeLength[2]);
-  autopas::Particle defaultParticle;
-
-  GridGenerator::fillWithParticles<autopas::Particle>(cells, edgeLength, defaultParticle);
-#ifdef AUTOPAS_OPENMP
-  int numThreadsBefore = omp_get_max_threads();
-  omp_set_num_threads(4);
-#endif
-  autopas::C18Traversal<FPCell, MFunctor, false, true> C18Traversal(edgeLength, &functor);
-
-  size_t boundaryXYcells = (edgeLength[2] - 1);
-  size_t boundaryXYinteractions = boundaryXYcells * (7 + 5 + 6 + 4);
-  size_t faceXcells = (edgeLength[1] - 2) * (edgeLength[2] - 1);
-  size_t boundaryXinteractions = faceXcells * (9 + 8);
-  size_t faceYcells = (edgeLength[0] - 2) * (edgeLength[2] - 1);
-  size_t boundaryYinteractions = faceYcells * (7 + 10);
-  size_t innercells = (edgeLength[0] - 2) * (edgeLength[1] - 2) * (edgeLength[2] - 2);
-  size_t innercellinteractions = innercells * 13;
-
-  EXPECT_CALL(functor, AoSFunctor(_, _, true))
-      .Times(boundaryXYinteractions + boundaryXinteractions + boundaryYinteractions + innercellinteractions);
-
-  C18Traversal.traverseCellPairs(cells);
-#ifdef AUTOPAS_OPENMP
-  omp_set_num_threads(numThreadsBefore);
-#endif
+  testTraversal(edgeLength);
 }
 
 TEST_F(C18TraversalTest, testTraversal2x3x4) {
   std::array<size_t, 3> edgeLength = {2, 3, 4};
+  testTraversal(edgeLength);
+}
 
-  MFunctor functor;
-  std::vector<FPCell> cells;
-  cells.resize(edgeLength[0] * edgeLength[1] * edgeLength[2]);
-  autopas::Particle defaultParticle;
-
-  GridGenerator::fillWithParticles<autopas::Particle>(cells, {edgeLength[0], edgeLength[1], edgeLength[2]},
-                                                      defaultParticle);
-#ifdef AUTOPAS_OPENMP
-  int numThreadsBefore = omp_get_max_threads();
-  omp_set_num_threads(4);
-#endif
-  autopas::C18Traversal<FPCell, MFunctor, false, true> C18Traversal({edgeLength[0], edgeLength[1], edgeLength[2]},
-                                                                    &functor);
-
-  size_t boundaryXYcells = (edgeLength[2] - 1);
-  size_t boundaryXYinteractions = boundaryXYcells * (7 + 5 + 6 + 4);
-  size_t faceXcells = (edgeLength[1] - 2) * (edgeLength[2] - 1);
-  size_t boundaryXinteractions = faceXcells * (9 + 8);
-  size_t faceYcells = (edgeLength[0] - 2) * (edgeLength[2] - 1);
-  size_t boundaryYinteractions = faceYcells * (7 + 10);
-  size_t innercells = (edgeLength[0] - 2) * (edgeLength[1] - 2) * (edgeLength[2] - 2);
-  size_t innercellinteractions = innercells * 13;
-
-  EXPECT_CALL(functor, AoSFunctor(_, _, true))
-      .Times(boundaryXYinteractions + boundaryXinteractions + boundaryYinteractions + innercellinteractions);
-
-  C18Traversal.traverseCellPairs(cells);
-#ifdef AUTOPAS_OPENMP
-  omp_set_num_threads(numThreadsBefore);
-#endif
+TEST_F(C18TraversalTest, testTraversal7x8x9) {
+  std::array<size_t, 3> edgeLength = {7, 8, 9};
+  testTraversal(edgeLength);
 }
