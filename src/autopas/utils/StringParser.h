db9e6ebe22c9e2a089267aab9baaf5895411618f
FG-TUM <f.gratl@tum.de>
2019-01-15 14:19:56 +0100

    include all options in parsing

--- src/autopas/utils/StringParser.h
+++ src/autopas/utils/StringParser.h
@@ -21,36 +21,55 @@ namespace StringParser {
 
 /**
  * Converts a string of options to a vector of enums. The options are expected to be lower case.
+ *
+ * Possible options: c01, c08, c18, direct, sliced, v01, v18, vsl
+ *
  * @param traversalOptionsString String containing traversal options.
  * @return Vector of TraversalOption enums. If no valid option was found the empty vector is returned.
  */
 static std::vector<autopas::TraversalOptions> parseTraversalOptions(std::string &traversalOptionsString) {
   std::vector<autopas::TraversalOptions> traversalOptions;
-  if (traversalOptionsString.find("c08") != std::string::npos) {
-    traversalOptions.push_back(autopas::TraversalOptions::c08);
-  }
+
+  // @TODO: rewrite such that sli and slicedVerlet don't crash
   if (traversalOptionsString.find("c01") != std::string::npos) {
     traversalOptions.push_back(autopas::TraversalOptions::c01);
+  }
+  if (traversalOptionsString.find("c08") != std::string::npos) {
+    traversalOptions.push_back(autopas::TraversalOptions::c08);
   }
   if (traversalOptionsString.find("c18") != std::string::npos) {
     traversalOptions.push_back(autopas::TraversalOptions::c18);
   }
+  if (traversalOptionsString.find("dir") != std::string::npos) {
+    traversalOptions.push_back(autopas::TraversalOptions::directSumTraversal);
+  }
   if (traversalOptionsString.find("sli") != std::string::npos) {
     traversalOptions.push_back(autopas::TraversalOptions::sliced);
   }
-  if (traversalOptionsString.find("dir") != std::string::npos) {
-    traversalOptions.push_back(autopas::TraversalOptions::directSumTraversal);
+  if (traversalOptionsString.find("v01") != std::string::npos) {
+    traversalOptions.push_back(autopas::TraversalOptions::c01Verlet);
+  }
+  if (traversalOptionsString.find("v18") != std::string::npos) {
+    traversalOptions.push_back(autopas::TraversalOptions::c18Verlet);
+  }
+  if (traversalOptionsString.find("vsl") != std::string::npos) {
+    traversalOptions.push_back(autopas::TraversalOptions::slicedVerlet);
   }
   return traversalOptions;
 }
 
 /**
  * Converts a string of options to a vector of enums. The options are expected to be lower case.
+ *
+ * Possible options: directSum, linkedCells, verletLists, vcells, vcluster
+ *
  * @param containerOptionsString String containing container options.
  * @return Vector of ContainerOption enums. If no valid option was found the empty vector is returned.
  */
 static std::vector<autopas::ContainerOptions> parseContainerOptions(std::string &containerOptionsString) {
   std::vector<autopas::ContainerOptions> containerOptions;
+
+  // @TODO: rewrite such that verlet stuff doesn't crash
   if (containerOptionsString.find("direct") != std::string::npos or
       containerOptionsString.find("ds") != std::string::npos) {
     containerOptions.push_back(autopas::directSum);
@@ -63,7 +82,7 @@ static std::vector<autopas::ContainerOptions> parseContainerOptions(std::string 
       containerOptionsString.find("vl") != std::string::npos) {
     containerOptions.push_back(autopas::verletLists);
   }
-  if (containerOptionsString.find("vcells") != std::string::npos) {
+  if (containerOptionsString.find("vcell") != std::string::npos) {
     containerOptions.push_back(autopas::verletListsCells);
   }
   if (containerOptionsString.find("vcluster") != std::string::npos) {
@@ -74,6 +93,9 @@ static std::vector<autopas::ContainerOptions> parseContainerOptions(std::string 
 
 /**
  * Converts a string containing a selector strategy to an enum. The option is expected to be lower case.
+ *
+ * Possible options: absolute, mean, median
+ *
  * @param selectorStrategyString String containing the selector option.
  * @return An enum representing the selector Strategy. If no valid option was found 'autopas::SelectorStrategy(-1)' is
  * returned.
@@ -93,6 +115,9 @@ static autopas::SelectorStrategy parseSelectorStrategy(std::string &selectorStra
 
 /**
  * Converts a string containing a data layout to an enum. The option is expected to be lower case.
+ *
+ * Possible options: aos, soa
+ *
  * @param dataLayoutSting String containing the data layout option.
  * @return An enum representing the data layout. If no valid option was found 'autopas::DataLayoutOption(-1)' is
  * returned.
