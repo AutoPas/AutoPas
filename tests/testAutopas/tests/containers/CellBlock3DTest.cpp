4ba9a56b2d3871c8c5dc82f7c57323d250da8bc1
SteffenSeckler <steffen.seckler@tum.de>
2019-01-16 14:35:11 +0100

    adds test for which CellBlock3D is producing failures.

--- tests/testAutopas/tests/containers/CellBlock3DTest.cpp
+++ tests/testAutopas/tests/containers/CellBlock3DTest.cpp
@@ -82,6 +82,119 @@ TEST_F(CellBlock3DTest, testBoundaries) {
   }
 }
 
+TEST_F(CellBlock3DTest, testCloseBoundaries) {
+  std::array<double, 3> position{0, 0, 0};
+  std::array<int, 3> shift = {};
+  for (shift[0] = -1; shift[0] < 2; ++shift[0]) {
+    for (shift[1] = -1; shift[1] < 2; ++shift[1]) {
+      for (shift[2] = -1; shift[2] < 2; ++shift[2]) {
+        for (int d = 0; d < 3; ++d) {
+          switch (shift[d]) {
+            case -1:
+              position[d] = std::nextafter(-0., -1.);  // for shift=-1 test close below 0. (outside)
+              // cell index should be 0
+              break;
+            case 0:
+              position[d] = 0.;
+              // cell index should be 1
+              break;
+            case 1:
+              position[d] = std::nextafter(10., -1.);  // for shift=1 test close below 10. (inside)
+              // cell index should be (size-2)
+              break;
+            default: { FAIL(); }
+          }
+        }
+
+        auto pos = _cells_1x1x1.get3DIndexOfPosition(position);
+
+        for (int d = 0; d < 3; ++d) {
+          EXPECT_EQ(pos[d], shift[d] == -1 ? 0 : 0 * shift[d] + 1)
+              << " for d = " << d << ", shift[d] = " << shift[d] << ", position[d] = " << position[d] << ".";
+        }
+
+        pos = _cells_2x2x2.get3DIndexOfPosition(position);
+
+        for (int d = 0; d < 3; ++d) {
+          EXPECT_EQ(pos[d], shift[d] == -1 ? 0 : 1 * shift[d] + 1)
+              << " for d = " << d << ", shift[d] = " << shift[d] << ", position[d] = " << position[d] << ".";
+        }
+
+        pos = _cells_3x3x3.get3DIndexOfPosition(position);
+
+        for (int d = 0; d < 3; ++d) {
+          EXPECT_EQ(pos[d], shift[d] == -1 ? 0 : 2 * shift[d] + 1)
+              << " for d = " << d << ", shift[d] = " << shift[d] << ", position[d] = " << position[d] << ".";
+        }
+
+        for (int d = 0; d < 3; ++d) {
+          double boxmin = d == 0 ? 2. / 3 : 0.;
+          double boxmax = d == 0 ? 1. : .125;
+          switch (shift[d]) {
+            case -1:
+              position[d] = std::nextafter(boxmin, -1.);  // for shift=-1 test close below 0. (outside)
+              // cell index should be 0
+              break;
+            case 0:
+              position[d] = boxmin;
+              // cell index should be 1
+              break;
+            case 1:
+              position[d] = std::nextafter(boxmax, -1.);  // for shift=1 test close below 10. (inside)
+              // cell index should be (size-2)
+              break;
+            default: { FAIL(); }
+          }
+        }
+
+        pos = _cells_11x4x4_nonZeroBoxMin.get3DIndexOfPosition(position);
+
+        EXPECT_EQ(pos[0], shift[0] == -1 ? 0 : 10 * shift[0] + 1)
+            << " for d = " << 0 << ", shift[d] = " << shift[0] << ", position[d] = " << position[0] << ".";
+        EXPECT_EQ(pos[1], shift[1] == -1 ? 0 : 3 * shift[1] + 1)
+            << " for d = " << 1 << ", shift[d] = " << shift[1] << ", position[d] = " << position[1] << ".";
+        EXPECT_EQ(pos[2], shift[2] == -1 ? 0 : 3 * shift[2] + 1)
+            << " for d = " << 2 << ", shift[d] = " << shift[2] << ", position[d] = " << position[2] << ".";
+      }
+    }
+  }
+}
+
+TEST_F(CellBlock3DTest, testCloseBoundaries19) {
+  std::array<double, 3> position{0, 0, 0};
+  std::array<int, 3> shift = {};
+  for (shift[0] = -1; shift[0] < 2; ++shift[0]) {
+    for (shift[1] = -1; shift[1] < 2; ++shift[1]) {
+      for (shift[2] = -1; shift[2] < 2; ++shift[2]) {
+        for (int d = 0; d < 3; ++d) {
+          switch (shift[d]) {
+            case -1:
+              position[d] = std::nextafter(-0., -1.);  // for shift=-1 test close below 0. (outside)
+              // cell index should be 0
+              break;
+            case 0:
+              position[d] = 0.;
+              // cell index should be 1
+              break;
+            case 1:
+              position[d] = std::nextafter(58.5, -1.);  // for shift=1 test close below 10. (inside)
+              // cell index should be (size-2)
+              break;
+            default: { FAIL(); }
+          }
+        }
+
+        auto pos = _cells_19x19x19.get3DIndexOfPosition(position);
+
+        for (int d = 0; d < 3; ++d) {
+          EXPECT_EQ(pos[d], shift[d] == -1 ? 0 : 18 * shift[d] + 1)
+                    << " for d = " << d << ", shift[d] = " << shift[d] << ", position[d] = " << position[d] << ".";
+        }
+      }
+    }
+  }
+}
+
 std::vector<std::array<double, 3>> CellBlock3DTest::getMesh(std::array<double, 3> start, std::array<double, 3> dr,
                                                             std::array<int, 3> numParts) const {
   std::vector<std::array<double, 3>> ret;
