336e1a818283244588697fc6491e2c3d7d77e218
FG-TUM <f.gratl@tum.de>
2019-01-16 14:26:18 +0100

    Now actually renamed tests

--- tests/testAutopas/tests/utils/StringUtilsTest.cpp
+++ tests/testAutopas/tests/utils/StringUtilsTest.cpp
@@ -6,40 +6,40 @@
 
 #include "StringUtilsTest.h"
 
-TEST(StringParserTest, parseTraversalOptionsTest) {
+TEST(StringUtilsTest, parseTraversalOptionsTest) {
   testParseMultiple<autopas::TraversalOptions>(autopas::allTraversalOptions,
                                                "c01, c08, c18, direct; sliced v01, c18verlet, verlet-sliced",
                                                autopas::utils::StringUtils::parseTraversalOptions);
 }
 
-TEST(StringParserTest, parseContainerOptionsTest) {
+TEST(StringUtilsTest, parseContainerOptionsTest) {
   testParseMultiple<autopas::ContainerOptions>(autopas::allContainerOptions,
                                                "directSum, linkedCells, verletLists, verlet-cells, vcluster",
                                                autopas::utils::StringUtils::parseContainerOptions);
 }
 
-TEST(StringParserTest, parseDataLayoutOptionsTest) {
+TEST(StringUtilsTest, parseDataLayoutOptionsTest) {
   testParseSingle<autopas::DataLayoutOption>(autopas::allDataLayoutOptions, {"soa", "aos"},
                                              autopas::utils::StringUtils::parseDataLayout);
 }
 
-TEST(StringParserTest, parseSelectorOptionsTest) {
+TEST(StringUtilsTest, parseSelectorOptionsTest) {
   testParseSingle<autopas::SelectorStrategy>(autopas::allSelectorStrategies, {"absolute", "median", "mean"},
                                              autopas::utils::StringUtils::parseSelectorStrategy);
 }
 
-TEST(StringParserTest, to_stringDataLayoutTest) {
+TEST(StringUtilsTest, to_stringDataLayoutTest) {
   testToString(autopas::allDataLayoutOptions, {autopas::DataLayoutOption(-1)});
 }
 
-TEST(StringParserTest, to_stringSelectorStrategiesTest) {
+TEST(StringUtilsTest, to_stringSelectorStrategiesTest) {
   testToString(autopas::allSelectorStrategies, {autopas::SelectorStrategy(-1)});
 }
 
-TEST(StringParserTest, to_stringContainerOptionsTest) {
+TEST(StringUtilsTest, to_stringContainerOptionsTest) {
   testToString(autopas::allContainerOptions, {autopas::ContainerOptions(-1)});
 }
 
-TEST(StringParserTest, to_stringTraversalOptionsTest) {
+TEST(StringUtilsTest, to_stringTraversalOptionsTest) {
   testToString(autopas::allTraversalOptions, {autopas::TraversalOptions(-1)});
 }
