a603f4f893272eaefed085132fed29ac8db5a838
FG-TUM <f.gratl@tum.de>
2019-02-20 17:23:26 +0100

    simplified traversalSelector to be more like a factory

--- src/autopas/selectors/TraversalSelector.h
+++ src/autopas/selectors/TraversalSelector.h
@@ -40,26 +40,13 @@ class TraversalSelector {
   /**
    * Dummy constructor such that this class can be used in maps
    */
-  TraversalSelector() : _currentTraversal(TraversalOption(-1)), _dims({0, 0, 0}), _allowedTraversalOptions({}) {}
+  TraversalSelector() : _dims({0, 0, 0}) {}
   /**
    * Constructor of the TraversalSelector class.
    * @param dims Array with the dimension lengths of the domain.
    * @param allowedTraversalOptions Vector of traversals the selector can choose from.
    */
-  TraversalSelector(const std::array<unsigned long, 3> &dims,
-                    const std::vector<TraversalOption> &allowedTraversalOptions)
-      : _currentTraversal(TraversalOption(-1)), _dims(dims), _allowedTraversalOptions(allowedTraversalOptions) {}
-
-  /**
-   * Gets the optimal traversal for a given cell functor. If no traversal is selected yet a optimum search is started.
-   * @tparam PairwiseFunctor The functor that defines the interaction of two particles.
-   * @tparam useSoA
-   * @tparam useNewton3
-   * @param pairwiseFunctor The functor that defines the interaction of two particles.
-   * @return Smartpointer to the optimal traversal.
-   */
-  template <class PairwiseFunctor, bool useSoA, bool useNewton3>
-  std::unique_ptr<CellPairTraversal<ParticleCell>> getCurrentTraversal(PairwiseFunctor &pairwiseFunctor);
+  TraversalSelector(const std::array<unsigned long, 3> &dims) : _dims(dims) {}
 
   /**
    * Generates a given Traversal for the given properties.
@@ -73,114 +60,53 @@ class TraversalSelector {
   template <class PairwiseFunctor, bool useSoA, bool useNewton3>
   std::unique_ptr<CellPairTraversal<ParticleCell>> generateTraversal(TraversalOption traversalType,
                                                                      PairwiseFunctor &pairwiseFunctor);
-
-  /**
-   * Sets the traversal to the given Option.
-   * @param traversalOption
-   */
-  void selectTraversal(TraversalOption traversalOption);
-
  private:
-  /**
-   * The optimal traversal for all functors that are marked relevant.#
-   */
-  TraversalOption _currentTraversal;
   /**
    * indicating whether or not the optimalTraversalOption is already initialized
    */
   const std::array<unsigned long, 3> _dims;
-  const std::vector<TraversalOption> _allowedTraversalOptions;
-
- public:
-  const std::vector<TraversalOption> &getAllowedTraversalOptions() const;
-
- private:
-  struct TimeMeasurement {
-    TraversalOption traversal;
-    long time;
-  };
-  // vector of (traversal type, execution time)
-  std::vector<TraversalSelector::TimeMeasurement> _traversalTimes;
 };
 
 template <class ParticleCell>
 template <class PairwiseFunctor, bool useSoA, bool useNewton3>
 std::unique_ptr<CellPairTraversal<ParticleCell>> TraversalSelector<ParticleCell>::generateTraversal(
     TraversalOption traversalType, PairwiseFunctor &pairwiseFunctor) {
-  std::unique_ptr<CellPairTraversal<ParticleCell>> traversal;
   switch (traversalType) {
     case TraversalOption::directSumTraversal: {
-      traversal =
-          std::make_unique<DirectSumTraversal<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(&pairwiseFunctor);
-      break;
+      return std::make_unique<DirectSumTraversal<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(&pairwiseFunctor);
     }
     case TraversalOption::c08: {
-      traversal =
-          std::make_unique<C08Traversal<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(_dims, &pairwiseFunctor);
-      break;
+      return std::make_unique<C08Traversal<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(_dims, &pairwiseFunctor);
     }
     case TraversalOption::sliced: {
-      traversal =
-          std::make_unique<SlicedTraversal<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(_dims, &pairwiseFunctor);
-      break;
+      return std::make_unique<SlicedTraversal<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(_dims,
+                                                                                                  &pairwiseFunctor);
     }
     case TraversalOption::c18: {
-      traversal =
-          std::make_unique<C18Traversal<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(_dims, &pairwiseFunctor);
-      break;
+      return std::make_unique<C18Traversal<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(_dims, &pairwiseFunctor);
     }
     case TraversalOption::c01: {
-      traversal =
-          std::make_unique<C01Traversal<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(_dims, &pairwiseFunctor);
-      break;
+      return std::make_unique<C01Traversal<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(_dims, &pairwiseFunctor);
     }
     case TraversalOption::slicedVerlet: {
-      traversal = std::make_unique<SlicedTraversalVerlet<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(
+      return std::make_unique<SlicedTraversalVerlet<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(
           _dims, &pairwiseFunctor);
-      break;
     }
     case TraversalOption::c18Verlet: {
-      traversal = std::make_unique<C18TraversalVerlet<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(
-          _dims, &pairwiseFunctor);
-      break;
+      return std::make_unique<C18TraversalVerlet<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(_dims,
+                                                                                                     &pairwiseFunctor);
     }
     case TraversalOption::c01Verlet: {
-      traversal = std::make_unique<C01TraversalVerlet<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(
-          _dims, &pairwiseFunctor);
-      break;
+      return std::make_unique<C01TraversalVerlet<ParticleCell, PairwiseFunctor, useSoA, useNewton3>>(_dims,
+                                                                                                     &pairwiseFunctor);
     }
     case TraversalOption::dummyTraversal: {
-      traversal = std::make_unique<DummyTraversal<ParticleCell>>(_dims);
-      break;
-    }
-    default: {
-      AutoPasLog(warn, "Traversal type {} is not a known type!", utils::StringUtils::to_string(traversalType));
+      return std::make_unique<DummyTraversal<ParticleCell>>(_dims);
     }
   }
-  return traversal;
-}
-
-template <class ParticleCell>
-template <class PairwiseFunctor, bool useSoA, bool useNewton3>
-std::unique_ptr<CellPairTraversal<ParticleCell>> TraversalSelector<ParticleCell>::getCurrentTraversal(
-    PairwiseFunctor &pairwiseFunctor) {
-  std::unique_ptr<CellPairTraversal<ParticleCell>> traversal;
-
-  if (_currentTraversal == TraversalOption(-1))
-    utils::ExceptionHandler::exception(
-        "TraversalSelector::getCurrentTraversal(): Traversal selector not initialized (nothing selected yet)!");
-
-  traversal = generateTraversal<PairwiseFunctor, useSoA, useNewton3>(_currentTraversal, pairwiseFunctor);
-  return traversal;
-}
-
-template <class ParticleCell>
-void TraversalSelector<ParticleCell>::selectTraversal(TraversalOption traversalOption) {
-  _currentTraversal = traversalOption;
-}
-template <class ParticleCell>
-const std::vector<TraversalOption> &TraversalSelector<ParticleCell>::getAllowedTraversalOptions() const {
-  return _allowedTraversalOptions;
+  autopas::utils::ExceptionHandler::exception("Traversal type {} is not a known type!",
+                                              utils::StringUtils::to_string(traversalType));
+  return std::unique_ptr<CellPairTraversal<ParticleCell>>(nullptr);
 }
 
 }  // namespace autopas
