dba9afe300664e21d8a03b272b2137abea41830e
Julian Spahl <julian.spahl@in.tum.de>
2019-03-05 20:38:48 +0100

    fixed error for non cuda use

--- src/autopas/utils/CudaStreamHandler.h
+++ src/autopas/utils/CudaStreamHandler.h
@@ -17,31 +17,31 @@ namespace autopas {
 namespace utils {
 
 class CudaStreamHandler {
-public:
-	CudaStreamHandler(size_t nStreams) : _streams(nStreams), random_index(0){
-		for(int i = 0; i < nStreams; ++i){
-			cudaStreamCreate(_streams.data() + i);
-		}
-	}
+#if defined(AUTOPAS_CUDA)
+ public:
+  CudaStreamHandler(size_t nStreams) : _streams(nStreams), random_index(0) {
+    for (int i = 0; i < nStreams; ++i) {
+      cudaStreamCreate(_streams.data() + i);
+    }
+  }
 
-	virtual ~CudaStreamHandler() {
-		for(auto it : _streams){
-			cudaStreamDestroy(it);
-		}
-	}
+  virtual ~CudaStreamHandler() {
+    for (auto it : _streams) {
+      cudaStreamDestroy(it);
+    }
+  }
 
-	cudaStream_t & getStream(int index){
-		return _streams.at(index);
-	}
-	cudaStream_t & getStreambyHash(size_t hash){
-		return _streams[hash % _streams.size()];
-	}
-	cudaStream_t & getStreamRandom(){
-		return _streams[(++random_index) % _streams.size()];
-	}
-private:
-	std::vector<cudaStream_t> _streams;
-	size_t random_index;
+  cudaStream_t& getStream(int index) { return _streams.at(index); }
+  cudaStream_t& getStreambyHash(size_t hash) { return _streams[hash % _streams.size()]; }
+  cudaStream_t& getStreamRandom() { return _streams[(++random_index) % _streams.size()]; }
+
+ private:
+  std::vector<cudaStream_t> _streams;
+  size_t random_index;
+#else
+ public:
+  CudaStreamHandler() {}
+#endif
 };
 
 }  // namespace utils
