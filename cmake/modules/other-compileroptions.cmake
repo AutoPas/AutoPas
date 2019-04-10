b10cd9c043c91b83a4a495f4ac3cebecafc248da
Julian Spahl <ga97naw@mytum.de>
2019-04-10 13:47:58 +0200

    Revert "reomved ENABLE_CUDA ifs from cmake"
    This reverts commit e0383d69e158fdd6661742ea0975a445c3c8afd5.

--- cmake/modules/other-compileroptions.cmake
+++ cmake/modules/other-compileroptions.cmake
@@ -4,19 +4,37 @@ if (ENABLE_FAST_MATH)
     message(WARNING "Fast-Math might cause particle loss! Only use this if you know what you are doing!")
 endif()
 
-target_compile_options(autopas
-PUBLIC
-    # Needed to vectorize sqrt()
-    $<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-fno-math-errno
-    # fast math for better vectorization
-    $<$<AND:$<BOOL:${ENABLE_FAST_MATH}>,$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>>:$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-ffast-math>
-    # INTEL: per default fast math is on. Disable via fp-model precise
-    $<$<AND:$<NOT:$<BOOL:${ENABLE_FAST_MATH}>>,$<CXX_COMPILER_ID:Intel>>:$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-fp-model precise>
-    # Warnings:
-    # no warnings for intel because it's mainly spam
-    $<$<CXX_COMPILER_ID:GNU>:$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-Wsuggest-override $<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-Wall $<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-Wno-unused-variable $<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-Wno-unused-function>
-    $<$<CXX_COMPILER_ID:Clang>:$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-Wall>
-    # @TODO clean up code with -Weffc++
-    )
+if(ENABLE_CUDA)
+    target_compile_options(autopas
+    PUBLIC
+        # Needed to vectorize sqrt()
+        $<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-fno-math-errno
+        # fast math for better vectorization
+        $<$<AND:$<BOOL:${ENABLE_FAST_MATH}>,$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>>:$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-ffast-math>
+        # INTEL: per default fast math is on. Disable via fp-model precise
+        $<$<AND:$<NOT:$<BOOL:${ENABLE_FAST_MATH}>>,$<CXX_COMPILER_ID:Intel>>:$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-fp-model precise>
+        # Warnings:
+        # no warnings for intel because it's mainly spam
+        $<$<CXX_COMPILER_ID:GNU>:$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-Wsuggest-override $<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-Wall $<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-Wno-unused-variable $<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-Wno-unused-function>
+        $<$<CXX_COMPILER_ID:Clang>:$<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler=>-Wall>
+        # @TODO clean up code with -Weffc++
+        )
+else()
+    target_compile_options(autopas
+    PUBLIC
+        # Needed to vectorize sqrt()
+        -fno-math-errno
+        # fast math for better vectorization
+        $<$<AND:$<BOOL:${ENABLE_FAST_MATH}>,$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>>:-ffast-math>
+        # INTEL: per default fast math is on. Disable via fp-model precise
+        $<$<AND:$<NOT:$<BOOL:${ENABLE_FAST_MATH}>>,$<CXX_COMPILER_ID:Intel>>:-fp-model precise>
+        # Warnings:
+        # no warnings for intel because it's mainly spam
+        $<$<CXX_COMPILER_ID:GNU>:-Wsuggest-override -Wall -Wno-unused-variable -Wno-unused-function>
+        $<$<CXX_COMPILER_ID:Clang>:-Wall>
+        # @TODO clean up code with -Weffc++
+        )
+endif()
+        
 
 message(STATUS "fno-math-errno set. This is needed to vectorize, e.g., sqrt().")
