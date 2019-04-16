ba04368747e7a0350dfe5460cc1c5b31747c54be
Christian Menges <christian.menges@tum.de>
2019-04-16 13:44:09 +0200

    Refactoring of testing helpers

--- tests/testAutopas/testingHelpers/GridGenerator.h
+++ tests/testAutopas/testingHelpers/GridGenerator.h
@@ -19,15 +19,16 @@ class GridGenerator {
    * @tparam ParticleCell
    * @param cells
    * @param particlesPerDim Number of particles per dimension
+   * @param defaultParticle inserted particle
    * @param spacing Factor for distance between two particles along one
    * dimension (default is 1)
    * @param offset Offset to move all particles
    */
   template <class Particle, class ParticleCell>
-  static void fillWithParticles(std::vector<ParticleCell> &cells, std::array<size_t, 3> particlesPerDim,
-                                const Particle &defaultParicle = autopas::Particle(),
-                                std::array<double, 3> spacing = std::array<double, 3>{1, 1, 1},
-                                std::array<double, 3> offset = std::array<double, 3>{.5, .5, .5});
+  static void fillWithParticles(std::vector<ParticleCell>& cells, const std::array<size_t, 3>& particlesPerDim,
+                                const Particle& defaultParticle = autopas::Particle(),
+                                const std::array<double, 3>& spacing = {1.0, 1.0, 1.0},
+                                const std::array<double, 3>& offset = {.5, .5, .5});
 
   /**
    * fills a autopas object with a cuboid mesh of particles
@@ -35,28 +36,29 @@ class GridGenerator {
    * @tparam ParticleCell
    * @param autoPas
    * @param particlesPerDim Number of particles per dimension.
+   * @param defaultParticle inserted particle
    * @param spacing Factor for distance between two particles along one
    * dimension (default is 1).
    * @param offset Offset to move all particles.
    */
   template <class Particle, class ParticleCell>
-  static void fillWithParticles(autopas::AutoPas<Particle, ParticleCell> &autoPas,
-                                std::array<size_t, 3> particlesPerDim,
-                                const Particle &defaultParticle = autopas::Particle(),
-                                std::array<double, 3> spacing = std::array<double, 3>{1, 1, 1},
-                                std::array<double, 3> offset = std::array<double, 3>{.5, .5, .5});
+  static void fillWithParticles(autopas::AutoPas<Particle, ParticleCell>& autoPas,
+                                const std::array<size_t, 3>& particlesPerDim,
+                                const Particle& defaultParticle = autopas::Particle(),
+                                const std::array<double, 3>& spacing = {1.0, 1.0, 1.0},
+                                const std::array<double, 3>& offset = {.5, .5, .5});
 };
 
 template <class Particle, class ParticleCell>
-void GridGenerator::fillWithParticles(std::vector<ParticleCell> &cells, std::array<size_t, 3> particlesPerDim,
-                                      const Particle &defaultParicle, std::array<double, 3> spacing,
-                                      std::array<double, 3> offset) {
+void GridGenerator::fillWithParticles(std::vector<ParticleCell>& cells, const std::array<size_t, 3>& particlesPerDim,
+                                      const Particle& defaultParticle, const std::array<double, 3>& spacing,
+                                      const std::array<double, 3>& offset) {
   size_t id = 0;
   size_t cellId = 0;
   for (unsigned int z = 0; z < particlesPerDim[2]; ++z) {
     for (unsigned int y = 0; y < particlesPerDim[1]; ++y) {
       for (unsigned int x = 0; x < particlesPerDim[0]; ++x) {
-        Particle p(defaultParicle);
+        Particle p(defaultParticle);
         p.setR({x * spacing[0] + offset[0], y * spacing[1] + offset[1], z * spacing[2] + offset[2]});
         p.setID(id++);
         cells[cellId++].addParticle(p);
@@ -66,9 +68,9 @@ void GridGenerator::fillWithParticles(std::vector<ParticleCell> &cells, std::arr
 }
 
 template <class Particle, class ParticleCell>
-void GridGenerator::fillWithParticles(autopas::AutoPas<Particle, ParticleCell> &autoPas,
-                                      std::array<size_t, 3> particlesPerDim, const Particle &defaultParticle,
-                                      std::array<double, 3> spacing, std::array<double, 3> offset) {
+void GridGenerator::fillWithParticles(autopas::AutoPas<Particle, ParticleCell>& autoPas,
+                                      const std::array<size_t, 3>& particlesPerDim, const Particle& defaultParticle,
+                                      const std::array<double, 3>& spacing, const std::array<double, 3>& offset) {
   size_t id = 0;
   for (unsigned int z = 0; z < particlesPerDim[2]; ++z) {
     for (unsigned int y = 0; y < particlesPerDim[1]; ++y) {
--- tests/testAutopas/testingHelpers/RandomGenerator.h
+++ tests/testAutopas/testingHelpers/RandomGenerator.h
@@ -11,60 +11,106 @@
 
 class RandomGenerator {
  private:
+  /**
+   * returns a random number between fMin and fMax
+   * @param fMin min. number (inclusive)
+   * @param fMax max. number (inclusive)
+   * @return double
+   */
   static double fRand(double fMin, double fMax);
 
  public:
+  /**
+   * returns a random position in the space between boxMin and boxMax
+   * @param boxMin min. position
+   * @param boxMax max. position
+   * @return std::array<double, 3>
+   */
   static std::array<double, 3> randomPosition(const std::array<double, 3>& boxMin, const std::array<double, 3>& boxMax);
 
+  /**
+   * fills the given container with random distributed particles
+   * @tparam Container
+   * @tparam Particle Type of particle to be generated
+   * @param container
+   * @param defaultParticle inserted particle
+   * @param numParticles number of particles
+   */
   template <class Container, class Particle>
-  static void fillWithParticles(Container& container, Particle defaultParticle, unsigned long numParticles = 100) {
-    srand(42);  // fixed seedpoint
+  static void fillWithParticles(Container& container, const Particle& defaultParticle,
+                                unsigned long numParticles = 100ul);
 
-    for (unsigned long i = 0; i < numParticles; ++i) {
-      Particle particle = defaultParticle;
-      particle.setR(randomPosition(container.getBoxMin(), container.getBoxMax()));
-      particle.setID(i);
-      container.addParticle(particle);
-    }
-  }
-
+  /**
+   * fills the given container with random distributed particles between boxMin and boxMax
+   * @tparam Container
+   * @tparam Particle Type of particle to be generated
+   * @param container
+   * @param defaultParticle inserted particle
+   * @param boxMin min. position
+   * @param boxMax max. position
+   * @param numParticles number of particles
+   */
   template <class Container, class Particle>
-  static void fillWithParticles(Container& container, Particle defaultParticle, std::array<double, 3> boxMin,
-                                std::array<double, 3> boxMax, int numParticles = 100) {
-    srand(42);  // fixed seedpoint
+  static void fillWithParticles(Container& container, const Particle& defaultParticle,
+                                const std::array<double, 3>& boxMin, const std::array<double, 3>& boxMax,
+                                unsigned long numParticles = 100ul);
 
-    for (int i = 0; i < numParticles; ++i) {
-      auto id = static_cast<unsigned long>(i);
-      Particle particle = defaultParticle;
-      particle.setR(randomPosition(boxMin, boxMax));
-      particle.setID(id);
-      container.addParticle(particle);
-    }
-  }
-
+  /**
+   * fills halo of given container with n = numParticles particles
+   * @tparam Container
+   * @tparam Particle Type of particle to be generated
+   * @param container
+   * @param defaultParticle inserted particle
+   * @param haloWidth width of halo
+   * @param numParticles number of particles
+   */
   template <class Container, class Particle>
-  static void fillWithHaloParticles(Container& container, Particle defaultParticle, double haloWidth,
-                                    unsigned long numParticles = 100) {
-    srand(42);  // fixed seedpoint
-
-    auto haloBoxMin = container.getBoxMin();
-    auto haloBoxMax = container.getBoxMax();
-
-    // increase the box size not exactly by the width to make it exclusive
-    for (int i = 0; i < 3; ++i) {
-      haloBoxMin[i] -= haloWidth * .99;
-      haloBoxMax[i] += haloWidth * .99;
-    }
-
-    for (unsigned long i = 0; i < numParticles;) {
-      Particle particle = defaultParticle;
-      auto pos = randomPosition(haloBoxMax, haloBoxMax);
-      // we only want  to add particles not in the actual box
-      if (autopas::utils::inBox(pos, container.getBoxMin(), container.getBoxMax())) continue;
-      particle.setR(pos);
-      particle.setID(i);
-      container.addHaloParticle(particle);
-      ++i;
-    }
-  }
+  static void fillWithHaloParticles(Container& container, const Particle& defaultParticle, double haloWidth,
+                                    unsigned long numParticles = 100ul);
 };
+
+template <class Container, class Particle>
+void RandomGenerator::fillWithParticles(Container& container, const Particle& defaultParticle,
+                                        unsigned long numParticles) {
+  RandomGenerator::fillWithParticles(container, defaultParticle, container.getBoxMin(), container.getBoxMax(),
+                                     numParticles);
+}
+
+template <class Container, class Particle>
+void RandomGenerator::fillWithParticles(Container& container, const Particle& defaultParticle,
+                                        const std::array<double, 3>& boxMin, const std::array<double, 3>& boxMax,
+                                        unsigned long numParticles) {
+  srand(42);  // fixed seedpoint
+
+  for (unsigned long i = 0; i < numParticles; ++i) {
+    Particle particle(defaultParticle);
+    particle.setR(randomPosition(boxMin, boxMax));
+    particle.setID(i);
+    container.addParticle(particle);
+  }
+}
+
+template <class Container, class Particle>
+void RandomGenerator::fillWithHaloParticles(Container& container, const Particle& defaultParticle, double haloWidth,
+                                            unsigned long numParticles) {
+  srand(42);  // fixed seedpoint
+
+  auto haloBoxMin = container.getBoxMin();
+  auto haloBoxMax = container.getBoxMax();
+
+  // increase the box size not exactly by the width to make it exclusive
+  for (unsigned int i = 0; i < 3; ++i) {
+    haloBoxMin[i] -= haloWidth * .99;
+    haloBoxMax[i] += haloWidth * .99;
+  }
+
+  for (unsigned long i = 0; i < numParticles; ++i) {
+    const auto pos = randomPosition(haloBoxMax, haloBoxMax);
+    // we only want  to add particles not in the actual box
+    if (autopas::utils::inBox(pos, container.getBoxMin(), container.getBoxMax())) continue;
+    Particle particle(defaultParticle);
+    particle.setR(pos);
+    particle.setID(i);
+    container.addHaloParticle(particle);
+  }
+}
