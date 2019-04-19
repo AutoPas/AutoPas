3be0ec590989a76cbcec295d10a53d611ed044c3
Julian Spahl <julian.spahl@in.tum.de>
2019-04-20 01:01:08 +0200

    added clangformat for cuda source and header files

--- src/autopas/pairwiseFunctors/FunctorCuda.cuh
+++ src/autopas/pairwiseFunctors/FunctorCuda.cuh
@@ -11,44 +11,37 @@
 
 namespace autopas {
 
-template<typename floatingPointType>
-class FunctorCudaSoA{
+template <typename floatingPointType>
+class FunctorCudaSoA {};
 
-};
+template <typename floatingPointType>
+class FunctorCudaConstants {};
 
-template<typename floatingPointType>
-class FunctorCudaConstants{
-
-};
-
-template<typename floatingPointType>
+template <typename floatingPointType>
 class CudaWrapperInterface {
-public:
-	using floatType = floatingPointType;
+ public:
+  using floatType = floatingPointType;
 
-	virtual void setNumThreads(int num_threads) = 0;
-	virtual void loadConstants(FunctorCudaConstants<floatType>* constants) = 0;
+  virtual void setNumThreads(int num_threads) = 0;
+  virtual void loadConstants(FunctorCudaConstants<floatType>* constants) = 0;
 
-	virtual void SoAFunctorNoN3Wrapper(FunctorCudaSoA<floatType>* cell1,
-			cudaStream_t stream = 0) = 0;
-	virtual void SoAFunctorNoN3PairWrapper(FunctorCudaSoA<floatType>* cell1,
-			FunctorCudaSoA<floatType>* cell2, cudaStream_t stream)= 0;
+  virtual void SoAFunctorNoN3Wrapper(FunctorCudaSoA<floatType>* cell1, cudaStream_t stream = 0) = 0;
+  virtual void SoAFunctorNoN3PairWrapper(FunctorCudaSoA<floatType>* cell1, FunctorCudaSoA<floatType>* cell2,
+                                         cudaStream_t stream) = 0;
 
-	virtual void SoAFunctorN3Wrapper(FunctorCudaSoA<floatType>* cell1,
-			cudaStream_t stream = 0)= 0;
-	virtual void SoAFunctorN3PairWrapper(FunctorCudaSoA<floatType>* cell1,
-			FunctorCudaSoA<floatType>* cell2, cudaStream_t stream)= 0;
+  virtual void SoAFunctorN3Wrapper(FunctorCudaSoA<floatType>* cell1, cudaStream_t stream = 0) = 0;
+  virtual void SoAFunctorN3PairWrapper(FunctorCudaSoA<floatType>* cell1, FunctorCudaSoA<floatType>* cell2,
+                                       cudaStream_t stream) = 0;
 
-	virtual void LinkedCellsTraversalNoN3Wrapper(FunctorCudaSoA<floatType>* cell1,unsigned int reqThreads,
-			unsigned int cids_size, unsigned int* cids,
-			unsigned int cellSizes_size, size_t* cellSizes,
-			unsigned int offsets_size, int* offsets, cudaStream_t stream)= 0;
+  virtual void LinkedCellsTraversalNoN3Wrapper(FunctorCudaSoA<floatType>* cell1, unsigned int reqThreads,
+                                               unsigned int cids_size, unsigned int* cids, unsigned int cellSizes_size,
+                                               size_t* cellSizes, unsigned int offsets_size, int* offsets,
+                                               cudaStream_t stream) = 0;
 
-	virtual void LinkedCellsTraversalN3Wrapper(FunctorCudaSoA<floatType>* cell1,unsigned int reqThreads,
-			unsigned int cids_size, unsigned int* cids,
-			unsigned int cellSizes_size, size_t* cellSizes,
-			unsigned int offsets_size, int* offsets, cudaStream_t stream)= 0;
+  virtual void LinkedCellsTraversalN3Wrapper(FunctorCudaSoA<floatType>* cell1, unsigned int reqThreads,
+                                             unsigned int cids_size, unsigned int* cids, unsigned int cellSizes_size,
+                                             size_t* cellSizes, unsigned int offsets_size, int* offsets,
+                                             cudaStream_t stream) = 0;
 };
 
-} // namespace autopas
-
+}  // namespace autopas
