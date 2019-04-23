1256a54e823fde0d8a5817946940c375257da997
Julian Spahl <julian.spahl@in.tum.de>
2019-04-23 16:43:08 +0200

    updated cuda becnchmark

--- examples/benchmark-cuda/main.cpp
+++ examples/benchmark-cuda/main.cpp
@@ -21,34 +21,48 @@ typedef MyMolecule::ParticleFloatingPointType precision;
 
 int numSamples = 100;
 
-template <class Container>
-void fillSpaceWithGrid(Container& pc, std::array<precision, 3> boxMin, std::array<precision, 3> boxMax,
-                       precision gridsize, int maxN = 10000) {
-  int i = 0;
-
-  for (precision x = boxMin[0]; x < boxMax[0]; x += gridsize) {
-    for (precision y = boxMin[1]; y < boxMax[1]; y += gridsize) {
-      for (precision z = boxMin[2]; z < boxMax[2]; z += gridsize) {
-        std::array<precision, 3> arr({x, y, z});
-        MyMolecule m(arr, {0., 0., 0.}, static_cast<unsigned long>(i));
-        pc.addParticle(m);
-        if (++i >= maxN) {
-          return;
-        }
+template <class Particle, class Container>
+void fillWithParticles(Container& container, std::array<size_t, 3> particlesPerDim, Particle& defaultParticle,
+                       std::array<typename Particle::ParticleFloatingPointType, 3> spacing,
+                       std::array<typename Particle::ParticleFloatingPointType, 3> offset) {
+  size_t id = 0;
+  for (unsigned int z = 0; z < particlesPerDim[2]; ++z) {
+    for (unsigned int y = 0; y < particlesPerDim[1]; ++y) {
+      for (unsigned int x = 0; x < particlesPerDim[0]; ++x) {
+        Particle p(defaultParticle);
+        p.setR({x * spacing[0] + offset[0], y * spacing[1] + offset[1], z * spacing[2] + offset[2]});
+        p.setID(id++);
+        container.addParticle(p);
       }
     }
   }
 }
 
+template <class Container>
+void fillSpaceWithGrid(Container& pc, std::array<precision, 3> boxMin, std::array<precision, 3> boxMax,
+                       precision gridsize) {
+  array<size_t, 3> nParticles;
+  for (int i = 0; i < 3; ++i) {
+    nParticles[i] = (size_t)((boxMax[0] - boxMin[0]) / gridsize) - 1;
+  }
+
+  cout << "numParticles: " << nParticles[0] * nParticles[0] * nParticles[2] << endl;
+  MyMolecule dummyParticle;
+  fillWithParticles(pc, nParticles, dummyParticle, {gridsize, gridsize, gridsize},
+                    {gridsize / 2, gridsize / 2, gridsize / 2});
+}
+
 template <class Container, class Traversal, class Functor>
 void run(Container& pc, Traversal* t, Functor* f) {
+  pc.iteratePairwise(f, t, false);
+
   auto start = std::chrono::high_resolution_clock::now();
   for (int i = 0; i < numSamples; ++i) {
     pc.iteratePairwise(f, t, false);
   }
   auto stop = std::chrono::high_resolution_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
-  cout << pc.getContainerType() << ", " << setw(2) << t->getTraversalType() << ", " << setw(8) << duration << "ms"
+  cout << pc.getContainerType() << ", " << setw(2) << t->getTraversalType() << ", " << setw(12) << duration << "ms"
        << endl;
 }
 
@@ -62,24 +76,24 @@ void build(Container& pc, PairwiseFunctor* pairwiseFunctor, TraversalOption trav
 
 int main(int argc, char** argv) {
   autopas::Logger::create();
-  long numParticles = 200000;
 
   if (argc == 2) {
     numSamples = stoi(argv[1]);
   }
 
-  std::array<precision, 3> boxMin({0., 0., 0.}), boxMax({33., 33., 33.});
-  precision cutoff = 1.0;
+  std::array<precision, 3> boxMin({0., 0., 0.}), boxMax({12., 12., 12.});
+  precision cutoff = 3.0;
   precision epsilon = 2.0;
   precision sigma = 0.4;
+  precision gridSize = 0.3;
 
   DirectSum<MyMolecule, FullParticleCell<MyMolecule>> dir(boxMin, boxMax, cutoff);
   LinkedCells<MyMolecule, FullParticleCell<MyMolecule>> lc(boxMin, boxMax, cutoff);
-  VerletClusterCells<MyMolecule> vcc(boxMin, boxMax, cutoff, 0.1, 100, 64);
+  VerletClusterCells<MyMolecule> vcc(boxMin, boxMax, cutoff, 0.1, 1000, 64);
 
-  fillSpaceWithGrid<>(dir, boxMin, boxMax, 0.3, numParticles);
-  fillSpaceWithGrid<>(lc, boxMin, boxMax, 0.3, numParticles);
-  fillSpaceWithGrid<>(vcc, boxMin, boxMax, 0.3, numParticles);
+  fillSpaceWithGrid<>(dir, boxMin, boxMax, gridSize);
+  fillSpaceWithGrid<>(lc, boxMin, boxMax, gridSize);
+  fillSpaceWithGrid<>(vcc, boxMin, boxMax, gridSize);
 
   typedef LJFunctor<MyMolecule, FullParticleCell<MyMolecule>> Func;
 
@@ -106,7 +120,7 @@ int main(int argc, char** argv) {
 
   // build<LinkedCells<MyMolecule, FullParticleCell<MyMolecule>>, Func, DataLayoutOption::soa, true>(lc, &func,
   // TraversalOption::c18);
-  // run(lc, &traversalc08N3, &func);
+  run(lc, &traversalc08N3, &func);
   run(lc, &traversalLCcuda, &func);
   run(lc, &traversalLCcudaN3, &func);
 
