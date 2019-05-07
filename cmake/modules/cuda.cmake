beed18384496a4236e4c8ba2d212936a1300d50c
Julian Spahl <julian.spahl@in.tum.de>
2019-05-07 23:03:22 +0200

    added variable for cuda compute capability

--- cmake/modules/cuda.cmake
+++ cmake/modules/cuda.cmake
@@ -3,6 +3,6 @@ if(ENABLE_CUDA)
 target_compile_options(autopas
         PUBLIC
         #architecture flags and -Xcompiler to prepend to -fopenmp
-        $<$<COMPILE_LANGUAGE:CUDA>:$<$<STREQUAL:${CMAKE_BUILD_TYPE},Debug>:-lineinfo -pg> -gencode arch=compute_61,code=sm_61 $<$<BOOL:${OPENMP}>:-Xcompiler>>
+        $<$<COMPILE_LANGUAGE:CUDA>:$<$<STREQUAL:${CMAKE_BUILD_TYPE},Debug>:-lineinfo -pg> -gencode arch=compute_${CUDA_COMPUTE_CAPABILITY},code=sm_${CUDA_COMPUTE_CAPABILITY} $<$<BOOL:${OPENMP}>:-Xcompiler>>
         )
 endif()
