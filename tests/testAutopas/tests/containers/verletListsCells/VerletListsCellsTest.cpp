ec0fda7c3355c0a708957aefd75cedc65ce42f5c
Christian Menges <christian.menges@tum.de>
2019-05-14 21:02:33 +0200

    Adapt VerletListsCellsTest to test cell size factors > 1.0

--- tests/testAutopas/tests/containers/verletListsCells/VerletListsCellsTest.cpp
+++ tests/testAutopas/tests/containers/verletListsCells/VerletListsCellsTest.cpp
@@ -18,12 +18,13 @@ TEST_F(VerletListsCellsTest, VerletListConstructor) {
   autopas::VerletListsCells<Particle> verletLists(min, max, cutoff, autopas::TraversalOption::c18, skin);
 }
 
-TEST_F(VerletListsCellsTest, testVerletListBuild) {
+void applyFunctor(MockFunctor<Particle, FPCell> &functor, const double cellSizefactor) {
   std::array<double, 3> min = {1, 1, 1};
   std::array<double, 3> max = {3, 3, 3};
   double cutoff = 1.;
   double skin = 0.2;
-  autopas::VerletListsCells<Particle> verletLists(min, max, cutoff, autopas::TraversalOption::c18, skin);
+  autopas::VerletListsCells<Particle> verletLists(min, max, cutoff, autopas::TraversalOption::c18, skin, 1,
+                                                  cellSizefactor);
 
   std::array<double, 3> r = {2, 2, 2};
   Particle p(r, {0., 0., 0.}, 0);
@@ -32,14 +33,11 @@ TEST_F(VerletListsCellsTest, testVerletListBuild) {
   Particle p2(r2, {0., 0., 0.}, 1);
   verletLists.addParticle(p2);
 
-  MockFunctor<Particle, FPCell> emptyFunctor;
-  EXPECT_CALL(emptyFunctor, AoSFunctor(_, _, true)).Times(AtLeast(1));
-
   autopas::C18TraversalVerlet<FPCell, MFunctor, autopas::DataLayoutOption::aos, true> traversal(
-      verletLists.getCellsPerDimension(), &emptyFunctor);
-  verletLists.iteratePairwise(&emptyFunctor, &traversal, true);
+      verletLists.getCellsPerDimension(), &functor);
+  verletLists.iteratePairwise(&functor, &traversal, true);
 
-  std::vector<Particle*> list;
+  std::vector<Particle *> list;
   for (auto iter = verletLists.begin(); iter.isValid(); ++iter) list.push_back(&*iter);
 
   EXPECT_EQ(list.size(), 2);
@@ -50,35 +48,21 @@ TEST_F(VerletListsCellsTest, testVerletListBuild) {
   EXPECT_EQ(partners, 1);
 }
 
+TEST_F(VerletListsCellsTest, testVerletListBuild) {
+  MockFunctor<Particle, FPCell> emptyFunctor;
+  EXPECT_CALL(emptyFunctor, AoSFunctor(_, _, true)).Times(AtLeast(1));
+
+  applyFunctor(emptyFunctor, 1.0);
+
+  MockFunctor<Particle, FPCell> emptyFunctor_cs2;
+  EXPECT_CALL(emptyFunctor_cs2, AoSFunctor(_, _, true)).Times(AtLeast(1));
+
+  applyFunctor(emptyFunctor_cs2, 2.0);
+}
+
 TEST_F(VerletListsCellsTest, testVerletList) {
-  std::array<double, 3> min = {1, 1, 1};
-  std::array<double, 3> max = {3, 3, 3};
-  double cutoff = 1.;
-  double skin = 0.2;
-  autopas::VerletListsCells<Particle> verletLists(min, max, cutoff, autopas::TraversalOption::c18, skin);
-
-  std::array<double, 3> r = {2, 2, 2};
-  Particle p(r, {0., 0., 0.}, 0);
-  verletLists.addParticle(p);
-  std::array<double, 3> r2 = {1.5, 2, 2};
-  Particle p2(r2, {0., 0., 0.}, 1);
-  verletLists.addParticle(p2);
-
   MockFunctor<Particle, FPCell> mockFunctor;
-  using ::testing::_;  // anything is ok
   EXPECT_CALL(mockFunctor, AoSFunctor(_, _, true));
 
-  autopas::C18TraversalVerlet<FPCell, MFunctor, autopas::DataLayoutOption::aos, true> traversal(
-      verletLists.getCellsPerDimension(), &mockFunctor);
-  verletLists.iteratePairwise(&mockFunctor, &traversal, true);
-
-  std::vector<Particle*> list;
-  for (auto iter = verletLists.begin(); iter.isValid(); ++iter) list.push_back(&*iter);
-
-  EXPECT_EQ(list.size(), 2);
-  int partners = 0;
-  for (auto p : list) {
-    partners += verletLists.getVerletList(p).size();
-  }
-  EXPECT_EQ(partners, 1);
+  applyFunctor(mockFunctor, 1.0);
 }
