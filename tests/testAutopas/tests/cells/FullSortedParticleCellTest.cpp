69c35425171da327aa013d0978322d9c89cdec8f
Christian Menges <christian.menges@tum.de>
2019-05-28 10:45:14 +0200

    Add test for FullSortedParticleCell

---
+++ tests/testAutopas/tests/cells/FullSortedParticleCellTest.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file FullSortedParticleCellTest.cpp
+ * @author C. Menges
+ * @date 26.05.2019
+ */
+
+#include "FullSortedParticleCellTest.h"
+#include "autopas/cells/FullParticleCell.h"
+
+TEST_F(FullSortedParticleCellTest, testParticleAccess) {
+  auto fpc = autopas::FullParticleCell<Particle>();
+  Particle p1 = Particle();
+  fpc.addParticle(p1);
+  auto fspc = autopas::FullSortedParticleCell<Particle>(fpc, {1., 0., 0.});
+  EXPECT_EQ(fspc._particles.size(), 1);
+  std::array<double, 3> force{3.1416, 2.7183, 9.8067};
+  fspc._particles.front().second->addF(force);
+  EXPECT_THAT(fpc._particles.front().getF(), testing::ContainerEq(force));
+}
---
+++ tests/testAutopas/tests/cells/FullSortedParticleCellTest.h
@@ -0,0 +1,15 @@
+/**
+ * @file FullSortedParticleCellTest.h
+ * @author C. Menges
+ * @date 26.05.2019
+ */
+
+#pragma once
+
+#include <gtest/gtest.h>
+#include <testingHelpers/commonTypedefs.h>
+#include "AutoPasTestBase.h"
+#include "autopas/AutoPas.h"
+#include "mocks/MockFunctor.h"
+
+class FullSortedParticleCellTest : public AutoPasTestBase {};
