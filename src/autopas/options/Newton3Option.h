ebd1746c8c9bb204919306a5c2c0437d2877f7d3
FG-TUM <f.gratl@tum.de>
2019-06-18 14:07:41 +0200

    comment

--- src/autopas/options/Newton3Option.h
+++ src/autopas/options/Newton3Option.h
@@ -10,9 +10,9 @@
 
 namespace autopas {
 /**
- * Possible choices for the particle data layout.
+ * Possible choices for the particle data layout. Values consistent with bool.
  */
-enum Newton3Option { disabled, enabled };
+enum Newton3Option { disabled = 0, enabled = 1 };
 
 /**
  * Provides a way to iterate over the possible choices of data layouts.
