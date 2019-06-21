b877830f10e47cc071e8b6ff91687ff88f94ce6c
Christian Menges <christian.menges@tum.de>
2019-06-21 18:44:13 +0200

    Add test case for sorting of particles in SortedCellView

--- tests/testAutopas/tests/cells/SortedCellViewTest.cpp
+++ tests/testAutopas/tests/cells/SortedCellViewTest.cpp
@@ -17,3 +17,38 @@ TEST_F(SortedCellViewTest, testParticleAccess) {
   fspc._particles.front().second->addF(force);
   EXPECT_THAT(fpc._particles.front().getF(), testing::ContainerEq(force));
 }
+
+TEST_F(SortedCellViewTest, testParticleSorting) {
+  auto fpc = autopas::FullParticleCell<Particle>();
+  Particle p1 = Particle({0., 3., 0.}, {0., 0., 0.}, 0);
+  fpc.addParticle(p1);
+  Particle p2 = Particle({2., 1., 2.}, {0., 0., 0.}, 2);
+  fpc.addParticle(p2);
+  Particle p3 = Particle({1., 2., 1.}, {0., 0., 0.}, 1);
+  fpc.addParticle(p3);
+  Particle p4 = Particle({3., 0., 3.}, {0., 0., 0.}, 3);
+  fpc.addParticle(p4);
+
+  unsigned int id = 0u;
+
+  {
+    auto fspc = autopas::SortedCellView<Particle, autopas::FullParticleCell<Particle>>(fpc, {1., 0., 0.});
+    EXPECT_EQ(fspc.numParticles(), 4);
+
+    for (auto &p : fspc._particles) {
+      EXPECT_DOUBLE_EQ(p.first, static_cast<double>(id));
+      EXPECT_EQ(p.second->getID(), id);
+      id++;
+    }
+  }
+  {
+    auto fspc = autopas::SortedCellView<Particle, autopas::FullParticleCell<Particle>>(fpc, {0., 1., 0.});
+    EXPECT_EQ(fspc.numParticles(), 4);
+
+    for (auto &p : fspc._particles) {
+      id--;
+      EXPECT_DOUBLE_EQ(p.first, static_cast<double>(3u - id));
+      EXPECT_EQ(p.second->getID(), id);
+    }
+  }
+}
