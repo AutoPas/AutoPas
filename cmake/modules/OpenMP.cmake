db68ee0ab7e2c60d79f0c6bfb7c1a6b2527bd84c
Christian Menges <christian.menges@tum.de>
2019-06-22 13:04:39 +0200

    Add work around for clang

--- cmake/modules/OpenMP.cmake
+++ cmake/modules/OpenMP.cmake
@@ -6,9 +6,15 @@ if (ARCHER)
 elseif (AUTOPAS_OPENMP)
     message(STATUS "OpenMP enabled.")
     find_package(OpenMP REQUIRED)
-
     # OpenMP version 4.5 was specified in 11.2015
-    if (OpenMP_CXX_SPEC_DATE LESS 201511)
+    if (
+        (OpenMP_CXX_SPEC_DATE LESS 201511)
+        AND
+            NOT
+            (("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+             AND (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 6)
+             )
+    )
         message(
             FATAL_ERROR
                 "OpenMP version not supported (specification date: ${OpenMP_CXX_SPEC_DATE}). Required version: 4.5+"
