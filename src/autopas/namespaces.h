708d84c08100b5c8475d0d2b9467df7efa0725b2
FG-TUM <f.gratl@tum.de>
2019-06-29 09:58:15 +0200

    removed briefs

--- src/autopas/namespaces.h
+++ src/autopas/namespaces.h
@@ -9,26 +9,26 @@
 #pragma once
 
 /**
- * @brief This is the main namespace of AutoPas.
- * @details Everything defined within AutoPas should be in this namespace.
+ * This is the main namespace of AutoPas.
+ * Everything defined within AutoPas should be in this namespace.
  */
 namespace autopas {
 
 /**
- * @brief This namespace is used for implementation specifics.
- * @details If you are a developer of AutoPas you might want to take a look inside here.
+ * This namespace is used for implementation specifics.
+ * If you are a developer of AutoPas you might want to take a look inside here.
  */
 namespace internal {}  // namespace internal
 
 /**
- * @brief In this namespace all functionality of the Smoothed Particle Hydrodynamics module of autopas is present.
- * @details This mainly includes kernels and particles.
+ * In this namespace all functionality of the Smoothed Particle Hydrodynamics module of autopas is present.
+ * This mainly includes kernels and particles.
  */
 namespace sph {}  // namespace sph
 
 /**
- * @brief In this namespace some helper classes and functions can be found used inside of AutoPas.
- * @details These classes reside mostly in the utils directory. However, most commonly used function inside the utils
+ * In this namespace some helper classes and functions can be found used inside of AutoPas.
+ * These classes reside mostly in the utils directory. However, most commonly used function inside the utils
  * directory might not be in the utils namespace.
  */
 namespace utils {
@@ -39,5 +39,4 @@ namespace utils {
 namespace StringUtils {}  // namespace StringUtils
 
 }  // namespace utils
-
 }  // namespace autopas
