67cf96d48998badc04b7bbc0e89708dda7849759
SteffenSeckler <steffen.seckler@tum.de>
2019-07-15 12:14:00 +0200

    fix BayesianSearchTest.cpp

--- tests/testAutopas/tests/selectors/tuningStrategy/BayesianSearchTest.cpp
+++ tests/testAutopas/tests/selectors/tuningStrategy/BayesianSearchTest.cpp
@@ -5,18 +5,19 @@
  */
 
 #include "BayesianSearchTest.h"
+#include "testingHelpers/commonTypedefs.h"
 #include <gmock/gmock-matchers.h>
 #include <gmock/gmock-more-matchers.h>
 
 TEST_F(BayesianSearchTest, testSearchSpaceEmpty) {
-  autopas::BayesianSearch bayesianSearch(std::set<autopas::ContainerOption>({}));
+  autopas::BayesianSearch<Particle, FPCell> bayesianSearch(std::set<autopas::ContainerOption>({}));
   EXPECT_TRUE(bayesianSearch.searchSpaceIsEmpty());
   EXPECT_FALSE(bayesianSearch.searchSpaceIsTrivial());
   EXPECT_THAT(bayesianSearch.getAllowedContainerOptions(), ::testing::IsEmpty());
 }
 
 TEST_F(BayesianSearchTest, testSearchSpaceOneOption) {
-  autopas::BayesianSearch bayesianSearch({autopas::ContainerOption::directSum}, autopas::NumberSetFinite<double>({1.}),
+  autopas::BayesianSearch<Particle, FPCell> bayesianSearch({autopas::ContainerOption::directSum}, autopas::NumberSetFinite<double>({1.}),
                                          {autopas::TraversalOption::directSumTraversal},
                                          {autopas::DataLayoutOption::soa}, {autopas::Newton3Option::enabled});
   EXPECT_FALSE(bayesianSearch.searchSpaceIsEmpty());
@@ -25,7 +26,7 @@ TEST_F(BayesianSearchTest, testSearchSpaceOneOption) {
 }
 
 TEST_F(BayesianSearchTest, testSearchSpaceMoreOptions) {
-  autopas::BayesianSearch bayesianSearch(
+  autopas::BayesianSearch<Particle, FPCell> bayesianSearch(
       {autopas::ContainerOption::linkedCells}, autopas::NumberSetFinite<double>({1.}), {autopas::TraversalOption::c08},
       {autopas::DataLayoutOption::soa}, {autopas::Newton3Option::enabled, autopas::Newton3Option::disabled});
   EXPECT_FALSE(bayesianSearch.searchSpaceIsEmpty());
@@ -35,7 +36,7 @@ TEST_F(BayesianSearchTest, testSearchSpaceMoreOptions) {
 }
 
 TEST_F(BayesianSearchTest, testRemoveN3OptionRemoveAll) {
-  autopas::BayesianSearch bayesianSearch(
+  autopas::BayesianSearch<Particle, FPCell> bayesianSearch(
       {autopas::ContainerOption::linkedCells}, autopas::NumberSetFinite<double>({1.}),
       {autopas::TraversalOption::c08, autopas::TraversalOption::sliced},
       {autopas::DataLayoutOption::soa, autopas::DataLayoutOption::aos}, {autopas::Newton3Option::enabled});
@@ -45,7 +46,7 @@ TEST_F(BayesianSearchTest, testRemoveN3OptionRemoveAll) {
 }
 
 TEST_F(BayesianSearchTest, testRemoveN3OptionRemoveSome) {
-  autopas::BayesianSearch bayesianSearch({autopas::ContainerOption::linkedCells},
+  autopas::BayesianSearch<Particle, FPCell> bayesianSearch({autopas::ContainerOption::linkedCells},
                                          autopas::NumberSetFinite<double>({1.}),
                                          {autopas::TraversalOption::c08, autopas::TraversalOption::sliced},
                                          {autopas::DataLayoutOption::soa, autopas::DataLayoutOption::aos},
@@ -58,7 +59,7 @@ TEST_F(BayesianSearchTest, testRemoveN3OptionRemoveSome) {
 
 TEST_F(BayesianSearchTest, testMaxEvidence) {
   size_t maxEvidence = 4;
-  autopas::BayesianSearch bayesSearch(
+  autopas::BayesianSearch<Particle, FPCell> bayesSearch(
       {autopas::ContainerOption::linkedCells}, autopas::NumberSetFinite<double>({1}),
       {autopas::TraversalOption::c08, autopas::TraversalOption::c01, autopas::TraversalOption::sliced},
       {autopas::DataLayoutOption::soa}, {autopas::Newton3Option::disabled}, maxEvidence);
@@ -77,7 +78,7 @@ TEST_F(BayesianSearchTest, testMaxEvidence) {
 TEST_F(BayesianSearchTest, testFindBest) {
   size_t maxEvidence = 16;
   unsigned long seed = 21;
-  autopas::BayesianSearch bayesSearch({autopas::ContainerOption::linkedCells}, autopas::NumberSetFinite<double>({1, 2}),
+  autopas::BayesianSearch<Particle, FPCell> bayesSearch({autopas::ContainerOption::linkedCells}, autopas::NumberSetFinite<double>({1, 2}),
                                       {autopas::TraversalOption::c08, autopas::TraversalOption::c01},
                                       {autopas::DataLayoutOption::soa, autopas::DataLayoutOption::aos},
                                       {autopas::Newton3Option::disabled, autopas::Newton3Option::enabled}, maxEvidence,
