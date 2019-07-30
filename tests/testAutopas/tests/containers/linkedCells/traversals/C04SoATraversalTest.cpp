4fcdc5e09a4b815a1ff661b9b15ed980d5973094
FG-TUM <f.gratl@tum.de>
2019-07-30 15:22:47 +0200

    fixed template type + reformant

--- tests/testAutopas/tests/containers/linkedCells/traversals/C04SoATraversalTest.cpp
+++ tests/testAutopas/tests/containers/linkedCells/traversals/C04SoATraversalTest.cpp
@@ -15,21 +15,21 @@ using ::testing::_;
  */
 TEST_F(C04SoATraversalTest, testTraversal) {
   std::array<size_t, 3> edgeLength = {3, 3, 3};
-double universalValue=1; //epsilon=sigma=mass=1.0
-ParticleClassLibrary PCL = ParticleClassLibrary(universalValue,universalValue,universalValue);
+  double universalValue = 1;  // epsilon=sigma=mass=1.0
+  ParticleClassLibrary PCL = ParticleClassLibrary(universalValue, universalValue, universalValue);
   autopas::LJFunctor<Molecule, FMCell> functor(1., PCL, 1.);
-  std::vector<FPCell> cells;
+  std::vector<FMCell> cells;
   cells.resize(edgeLength[0] * edgeLength[1] * edgeLength[2]);
 
-  autopas::MoleculeLJ<> default1 = Molecule({1.75, 2.1, 1.75},{0.,0.,0.},0);
-    autopas::MoleculeLJ<> default2 = Molecule({1.75, 1.6, 1.75},{0.,0.,0.},1);
+  autopas::MoleculeLJ<> default1 = Molecule({1.75, 2.1, 1.75}, {0., 0., 0.}, 0);
+  autopas::MoleculeLJ<> default2 = Molecule({1.75, 1.6, 1.75}, {0., 0., 0.}, 1);
 
-    cells[autopas::utils::ThreeDimensionalMapping::threeToOneD(1ul, 2ul, 1ul, edgeLength)].addParticle(default1);
+  cells[autopas::utils::ThreeDimensionalMapping::threeToOneD(1ul, 2ul, 1ul, edgeLength)].addParticle(default1);
   cells[autopas::utils::ThreeDimensionalMapping::threeToOneD(1ul, 1ul, 1ul, edgeLength)].addParticle(default2);
 
   NumThreadGuard numThreadGuard(1);
 
-  autopas::C04SoATraversal<FPCell, autopas::LJFunctor<Molecule, FMCell>, autopas::DataLayoutOption::soa, true>
+  autopas::C04SoATraversal<FMCell, autopas::LJFunctor<Molecule, FMCell>, autopas::DataLayoutOption::soa, true>
       c04SoATraversal(edgeLength, &functor, 1);
   c04SoATraversal.setCellsToTraverse(cells);
   c04SoATraversal.initTraversal();
@@ -37,7 +37,7 @@ ParticleClassLibrary PCL = ParticleClassLibrary(universalValue,universalValue,un
   c04SoATraversal.endTraversal();
 
   size_t num = 0;
-  for (auto cell : cells) {
+  for (auto &cell : cells) {
     num += cell.numParticles();
   }
   EXPECT_EQ(num, 2);
