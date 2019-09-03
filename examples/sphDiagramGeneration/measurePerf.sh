2c8c8057d9c21e9ffe658f93a034f9cc77931aac
SteffenSeckler <steffen.seckler@tum.de>
2019-09-03 17:32:11 +0200

    adapts sph-diagram-generation to AutoPas interface

--- examples/sphDiagramGeneration/measurePerf.sh
+++ examples/sphDiagramGeneration/measurePerf.sh
@@ -2,21 +2,20 @@
 
 Mols=(32 64 128 256 512 1024 2048 4096 8192)
 Reps=(100000 10000 10000 1000 1000 1000 100 10 5)
-cont_to_name=("lc" "direct" "verlet")
 func_to_name=("density" "hydro")
 
 
 for func in {0..1};
 do
     echo ----- functor $func
-    for iCont in {0..2} ;
+    for cont in "lc" "direct" "verlet";
     do
-        echo Container ${iCont}:
-        filename="output-${cont_to_name[$iCont]}-${func_to_name[$func]}.txt"
+        echo Container ${cont}:
+        filename="output-${cont}-${func_to_name[$func]}.txt"
 
         echo -e "numparticles\tnumIterations\tMFUPS_AOS\tMFUPS_SOA" > ${filename}
         # iterate over molecules with the correct repetition
-        for i in {0..8}; do ./sph-diagram-generation ${Mols[$i]} ${Reps[$i]} ${iCont} ${func} ; done >> ${filename}
+        for i in {0..8}; do ./sph-diagram-generation ${Mols[$i]} ${Reps[$i]} ${cont} ${func} ; done >> ${filename}
 
     done
 done
