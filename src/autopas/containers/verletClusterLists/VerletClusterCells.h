46f2f3bad913ad27288b1a6129ac47e467888024
Julian Spahl <julian.spahl@in.tum.de>
2019-09-23 18:21:09 +0200

    excluded dummy particles from count

--- src/autopas/containers/verletClusterLists/VerletClusterCells.h
+++ src/autopas/containers/verletClusterLists/VerletClusterCells.h
@@ -340,6 +340,25 @@ class VerletClusterCells : public ParticleContainer<FullParticleCell<Particle>> 
   }
 
   /**
+   * Get the number of particles excluding dummy Particles saved in the container.
+   * @return Number of particles in the container.
+   */
+  unsigned long getNumParticles() const override {
+    size_t numParticles = 0ul;
+#ifdef AUTOPAS_OPENMP
+    // @todo: find a sensible value for magic number
+    // numThreads should be at least 1 and maximal max_threads
+    int numThreads = std::max(1, std::min(omp_get_max_threads(), (int)(this->_cells.size() / 1000)));
+    AutoPasLog(trace, "Using {} threads", numThreads);
+#pragma omp parallel for num_threads(numThreads) reduction(+ : numParticles)
+#endif
+    for (size_t index = 0; index < _dummyStarts.size(); ++index) {
+      numParticles += _dummyStarts[index];
+    }
+    return numParticles;
+  }
+
+  /**
    * Deletes all Dummy Particles in the container
    */
   void deleteDummyParticles() {
