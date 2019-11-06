a0f4a2c792858c79e3d7d8f46cd928f48c2743a5
FG-TUM <f.gratl@tum.de>
2019-11-06 17:34:11 +0100

    use propper normal distribution for maxwell boltzmann

--- examples/md-flexible/Thermostat.h
+++ examples/md-flexible/Thermostat.h
@@ -85,35 +85,10 @@ class Thermostat {
    * @param factor
    */
   void maxwellBoltzmannDistribution(autopas::Particle &p, const ThermostatFloatType factor) {
-    p.setV(autopas::ArrayMath::addScalar(p.getV(), factor * gaussDeviate()));
-  }
+    std::default_random_engine randomEngine(42); // constant seed for repeatability
+    std::normal_distribution<double> normalDistribution{0, 1};
 
-  /**
-   * helper function for MaxwellBoltzmannDistribution().
-   * Generates a gauss deviate, i.e. values according to the normal distribution.
-   * Code taken from:
-   * Griebel et. al.: Numerical Simulation in Molecular Dynamics, p. 427
-   */
-  static ThermostatFloatType gaussDeviate() {
-    ThermostatFloatType a1, a2, s, r, b1;
-    static bool iset = false;
-    static ThermostatFloatType b2;
-
-    if (!iset) {
-      do {
-        a1 = 2.0 * rand() / (RAND_MAX + 1.0) - 1.0;
-        a2 = 2.0 * rand() / (RAND_MAX + 1.0) - 1.0;
-        r = a1 * a1 + a2 * a2;
-      } while (r >= 1.0);
-      s = sqrt(-2.0 * log(r) / r);
-      b1 = a1 * s;
-      b2 = a2 * s;
-      iset = true;
-      return b1;
-    } else {
-      iset = false;
-      return b2;
-    }
+    p.setV(autopas::ArrayMath::addScalar(p.getV(), factor * normalDistribution(randomEngine)));
   }
 
   /**
