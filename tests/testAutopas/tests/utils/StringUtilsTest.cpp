fcde3413f77a5066bee363df279f80101b2ea62e
FG-TUM <f.gratl@tum.de>
2019-11-16 11:52:16 +0100

    remove unnecessary cast

--- tests/testAutopas/tests/utils/StringUtilsTest.cpp
+++ tests/testAutopas/tests/utils/StringUtilsTest.cpp
@@ -17,8 +17,6 @@ TEST(StringUtilsTest, parseNumberSetTest) {
             std::set<double>({1., 1.5, 2., 3., 20.}));
 
   auto numberSet = autopas::utils::StringUtils::parseNumberSet("[1.,2e1]");
-  auto *numberInterval = dynamic_cast<autopas::NumberInterval<double> *>(numberSet.get());
-  ASSERT_NE(numberInterval, nullptr);
-  EXPECT_EQ(numberInterval->getMin(), 1.);
-  EXPECT_EQ(numberInterval->getMax(), 2e1);
+  EXPECT_EQ(numberSet->getMin(), 1.);
+  EXPECT_EQ(numberSet->getMax(), 2e1);
 }
