1a9c8e4c24c7f3a19ff89870874ad39837feea8f
FG-TUM <f.gratl@tum.de>
2019-11-17 12:34:38 +0100

    only pad domain if necessary

--- examples/md-flexible/parsing/MDFlexConfig.cpp
+++ examples/md-flexible/parsing/MDFlexConfig.cpp
@@ -105,57 +105,36 @@ std::string MDFlexConfig::to_string() const {
 
 void MDFlexConfig::calcSimulationBox() {
   const double interactionLength = cutoff + verletSkinRadius;
-  // also account for explicitly set box sizes
-  std::vector<double> xMins = {boxMin[0]};
-  std::vector<double> yMins = {boxMin[1]};
-  std::vector<double> zMins = {boxMin[2]};
-  std::vector<double> xMaxs = {boxMax[0]};
-  std::vector<double> yMaxs = {boxMax[1]};
-  std::vector<double> zMaxs = {boxMax[2]};
-  for (auto &c : cubeGridObjects) {
-    xMins.emplace_back(c.getBoxMin()[0]);
-    yMins.emplace_back(c.getBoxMin()[1]);
-    zMins.emplace_back(c.getBoxMin()[2]);
-    xMaxs.emplace_back(c.getBoxMax()[0]);
-    yMaxs.emplace_back(c.getBoxMax()[1]);
-    zMaxs.emplace_back(c.getBoxMax()[2]);
-  }
-  for (auto &c : cubeGaussObjects) {
-    xMins.emplace_back(c.getBoxMin()[0]);
-    yMins.emplace_back(c.getBoxMin()[1]);
-    zMins.emplace_back(c.getBoxMin()[2]);
-    xMaxs.emplace_back(c.getBoxMax()[0]);
-    yMaxs.emplace_back(c.getBoxMax()[1]);
-    zMaxs.emplace_back(c.getBoxMax()[2]);
-  }
-  for (auto &c : cubeUniformObjects) {
-    xMins.emplace_back(c.getBoxMin()[0]);
-    yMins.emplace_back(c.getBoxMin()[1]);
-    zMins.emplace_back(c.getBoxMin()[2]);
-    xMaxs.emplace_back(c.getBoxMax()[0]);
-    yMaxs.emplace_back(c.getBoxMax()[1]);
-    zMaxs.emplace_back(c.getBoxMax()[2]);
-  }
-  for (auto &c : sphereObjects) {
-    xMins.emplace_back(c.getBoxMin()[0]);
-    yMins.emplace_back(c.getBoxMin()[1]);
-    zMins.emplace_back(c.getBoxMin()[2]);
-    xMaxs.emplace_back(c.getBoxMax()[0]);
-    yMaxs.emplace_back(c.getBoxMax()[1]);
-    zMaxs.emplace_back(c.getBoxMax()[2]);
-  }
-  if (not xMins.empty()) {
-    boxMin = {*std::min_element(xMins.begin(), xMins.end()), *std::min_element(yMins.begin(), yMins.end()),
-              *std::min_element(zMins.begin(), zMins.end())};
-    boxMax = {*std::max_element(xMaxs.begin(), xMaxs.end()), *std::max_element(yMaxs.begin(), yMaxs.end()),
-              *std::max_element(zMaxs.begin(), zMaxs.end())};
-  }
-  // needed for 2D Simulation, that BoxLength >= interactionLength for all Dimensions
+  std::array<std::vector<double>, 3> mins;
+  std::array<std::vector<double>, 3> maxs;
+
+  // helper function
+  auto emplaceObjectLimits = [&] (const auto &objectCollection) {
+    for (auto &object : objectCollection) {
+      for (size_t i = 0; i < 3; ++i) {
+        mins[i].emplace_back(object.getBoxMin()[i]);
+        maxs[i].emplace_back(object.getBoxMax()[i]);
+      }
+    }
+  };
+
+  emplaceObjectLimits(cubeGaussObjects);
+  emplaceObjectLimits(cubeGridObjects);
+  emplaceObjectLimits(cubeUniformObjects);
+  emplaceObjectLimits(sphereObjects);
+
+  std::array<double, 3> objectMin = {*std::min_element(mins[0].begin(), mins[0].end()), *std::min_element(mins[1].begin(), mins[1].end()),
+            *std::min_element(mins[2].begin(), mins[2].end())};
+  std::array<double, 3> objectMax = {*std::max_element(maxs[0].begin(), maxs[0].end()), *std::max_element(maxs[1].begin(), maxs[1].end()),
+            *std::max_element(maxs[2].begin(), maxs[2].end())};
+
   for (int i = 0; i < 3; i++) {
     // pad domain such that periodic boundaries can work.
-    boxMin[i] -= particleSpacing / 2;
-    boxMax[i] += particleSpacing / 2;
+    // This is necessary if the given min/max is not at least half the spacing away of the farthest object.
+    boxMin[i] = std::min(boxMin[i], objectMin[i] - particleSpacing / 2);
+    boxMax[i] = std::max(boxMax[i], objectMax[i] + particleSpacing / 2);
 
+    // needed for 2D Simulation, that BoxLength >= interactionLength for all Dimensions
     if (boxMax[i] - boxMin[i] < interactionLength) {
       std::cout << "WARNING: Simulation box in dimension " << i
                 << " is shorter than interaction length and will be increased." << std::endl;
