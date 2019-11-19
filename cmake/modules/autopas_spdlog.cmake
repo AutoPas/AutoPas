6900786194897a1c0f9a265d3eb5c4479e621446
FG-TUM <f.gratl@tum.de>
2019-11-19 10:20:52 +0100

    cmake file for properly including spdlog

---
+++ cmake/modules/autopas_spdlog.cmake
@@ -0,0 +1,64 @@
+# first try: check if we find any installed version
+set(spdlog_DIR /work/ga68cat/workspace/spdlog/install/lib/cmake/spdlog)
+find_package(spdlog QUIET)
+if (spdlog_FOUND AND ${spdlog_VERSION} VERSION_GREATER_EQUAL 1.3.1)
+    message(STATUS "spdlog - using installed system version ${spdlog_VERSION}")
+    target_link_libraries(autopas PUBLIC spdlog::spdlog)
+else ()
+    # system version not found -> install bundled version
+    message(STATUS "spdlog - not found or version older than 1.3.1")
+    message(STATUS "spdlog - using bundled version 1.4.3 (commit 79259fd)")
+
+    include(ExternalProject)
+    ExternalProject_Add(
+        spdlog
+        URL
+            # spdlog master:
+            # https://github.com/gabime/spdlog/archive/v1.x.zip
+            # spdlog commit 79259fd:
+            ${CMAKE_SOURCE_DIR}/libs/spdlog-1.x.zip
+        URL_HASH MD5=7415a9768f3433bd93d78c1c87fd0576
+        BUILD_BYPRODUCTS ${CMAKE_CURRENT_BINARY_DIR}/spdlog/src/spdlog-build/libspdlog.a
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/spdlog
+        # Disable stuff we don't need. Especially warnings.
+        CMAKE_ARGS
+            -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+            -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+            -DCMAKE_BUILD_TYPE=RELEASE
+            -DSPDLOG_BUILD_EXAMPLE=OFF
+            -DSPDLOG_BUILD_TESTS=OFF
+            -DCMAKE_CXX_FLAGS=-w
+            # Disable install step
+        INSTALL_COMMAND ""
+    )
+    ExternalProject_Get_Property(
+        spdlog
+        source_dir
+        binary_dir
+        install_dir
+    )
+
+    add_library(
+        libspdlog
+        STATIC
+        IMPORTED
+        GLOBAL
+    )
+    add_dependencies(libspdlog spdlog)
+
+    # create directory otherwise cmake will complain during generate step bc it would only be
+    # created by make
+    file(MAKE_DIRECTORY "${install_dir}/src/spdlog/include")
+
+    # define interesting
+    set_target_properties(
+        libspdlog
+        PROPERTIES
+            "IMPORTED_LOCATION"
+            "${binary_dir}/libspdlog.a"
+            "INTERFACE_INCLUDE_DIRECTORIES"
+            "${install_dir}/src/spdlog/include"
+    )
+
+    target_link_libraries(autopas PUBLIC libspdlog)
+endif ()
