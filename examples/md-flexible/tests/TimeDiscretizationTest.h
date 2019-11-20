541e4518baa6fbba1320df5ca514b09f67f2bb2b
FG-TUM <f.gratl@tum.de>
2019-11-20 19:09:57 +0100

    refactored time discretization tests

--- examples/md-flexible/tests/TimeDiscretizationTest.h
+++ examples/md-flexible/tests/TimeDiscretizationTest.h
@@ -23,33 +23,15 @@
 class TimeDiscretizationTest : public AutoPasTestBase {
  public:
   TimeDiscretizationTest()
-      : AutoPasTestBase(),
-        cutoff{1.},
-        boxmin{{0., 0., 0.}},
-        boxmax{{5., 5., 5.}},
-        _particlePropertiesLibrary(),
-        functor{autopas::LJFunctor<PrintableMolecule, autopas::ParticleCell<PrintableMolecule>>(cutoff, 0.0)} {
+      : AutoPasTestBase() {
     functor.setParticleProperties(1., 1.);
     _particlePropertiesLibrary.addType(0, 1, 1, 1);
   }
-  void globalForceTest(autopas::AutoPas<PrintableMolecule, autopas::FullParticleCell<PrintableMolecule>> &auto1,
-                       autopas::AutoPas<PrintableMolecule, autopas::FullParticleCell<PrintableMolecule>> &auto2,
-                       int iterations);
-  void initFillWithParticles(autopas::AutoPas<PrintableMolecule, autopas::FullParticleCell<PrintableMolecule>> &autopas,
-                             std::array<unsigned long, 3> particlesPerDim);
-
-  static std::array<double, 3> nextPosition(std::array<double, 3> position, std::array<double, 3> force,
-                                            std::array<double, 3> velocity, double particle_delta_t);
-  static std::array<double, 3> nextVelocity(std::array<double, 3> velocity, std::array<double, 3> force,
-                                            std::array<double, 3> oldf, double particle_delta_t);
-
-  void Pos_and_Velo_Test(autopas::AutoPas<PrintableMolecule, autopas::FullParticleCell<PrintableMolecule>> &autopas,
-                         size_t numberOfParticles, int iterations);
+  void fillWithParticlesAndInit(autopas::AutoPas<PrintableMolecule,
+                                                 autopas::FullParticleCell<PrintableMolecule>> &autopas);
 
  protected:
-  double cutoff;
-  std::array<double, 3> boxmin;
-  std::array<double, 3> boxmax;
+  double cutoff{2.};
   ParticlePropertiesLibrary<double, size_t> _particlePropertiesLibrary;
-  autopas::LJFunctor<PrintableMolecule, autopas::ParticleCell<PrintableMolecule>> functor;
+  autopas::LJFunctor<PrintableMolecule, autopas::ParticleCell<PrintableMolecule>> functor{autopas::LJFunctor<PrintableMolecule, autopas::ParticleCell<PrintableMolecule>>(cutoff, 0.0)};
 };
