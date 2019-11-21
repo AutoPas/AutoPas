d8263fa2345b18ecca558e5b7decec37c14f6a36
FG-TUM <f.gratl@tum.de>
2019-11-21 17:30:46 +0100

    fix Maxwell Boltzmann stuff: - removed boltzmann const (-> now everywhere assumed to be 1) - brownianMotion adds a velocity according to a given temperature

--- examples/md-flexible/Thermostat.h
+++ examples/md-flexible/Thermostat.h
@@ -21,11 +21,20 @@ namespace {
  * @param p The particle to initialize.
  * @param averageVelocity Average velocity per dimension to be added.
  */
-void maxwellBoltzmannDistribution(autopas::Particle &p, const double averageVelocity) {
-  std::default_random_engine randomEngine(42);  // constant seed for repeatability
-  std::normal_distribution<double> normalDistribution{0, 1};
+void addMaxwellBoltzmannDistributedVelocity(autopas::Particle &p, const double averageVelocity) {
+  // we use a constant seed for repeatability.
+  // random engine needs static lifetime otherwise it would be recreated for every call.
+  static std::default_random_engine randomEngine(42);
 
-  p.setV(autopas::utils::ArrayMath::addScalar(p.getV(), averageVelocity * normalDistribution(randomEngine)));
+  // when adding independent normally distributed to all velocity components the velocity change is maxwell boltzmann
+  // distributed
+  std::normal_distribution<double> normalDistribution{0, 1};
+  std::array<double, 3> randomVelocity{};
+  for (size_t i = 0; i < randomVelocity.size(); ++i) {
+    auto randomNumber = normalDistribution(randomEngine);
+    randomVelocity[i] = averageVelocity * randomNumber;
+  }
+  p.setV(autopas::utils::ArrayMath::add(p.getV(), randomVelocity));
 }
 }  // namespace
 
@@ -65,31 +74,22 @@ double calcTemperature(AutoPasTemplate &autopas, ParticlePropertiesLibraryTempla
  * @tparam ParticlePropertiesLibraryTemplate Type of ParticlePropertiesLibrary Object (no pointer)
  * @param autopas
  * @param particlePropertiesLibrary
- * @param useCurrentTemp
+ * @param targetTemperature temperature of the system after applying the function on a system with temperature = 0.
  */
 template <class AutoPasTemplate, class ParticlePropertiesLibraryTemplate>
 void addBrownianMotion(AutoPasTemplate &autopas, ParticlePropertiesLibraryTemplate &particlePropertiesLibrary,
-                       bool useCurrentTemp) {
+                       const double targetTemperature) {
   // factors for the brownian motion per particle type.
   std::map<size_t, double> factors;
-  if (useCurrentTemp) {
-    // brownian motion with disturbance depending on current temperature and mass
-    constexpr double boltzmannConst = 1.38064852e-23;  // Boltzmann constant in J/K
-    double currentTempMulKB = calcTemperature(autopas, particlePropertiesLibrary) * boltzmannConst;
-    for (auto typeID : particlePropertiesLibrary.getTypes()) {
-      factors.emplace(typeID, std::sqrt(currentTempMulKB / particlePropertiesLibrary.getMass(typeID)));
-    }
-  } else {
-    // simple version of brownian motion with constant disturbance for all particles
-    for (auto typeID : particlePropertiesLibrary.getTypes()) {
-      factors.emplace(typeID, 0.1);
-    }
+  // brownian motion with disturbance depending on current temperature and mass
+  for (auto typeID : particlePropertiesLibrary.getTypes()) {
+    factors.emplace(typeID, std::sqrt(targetTemperature / particlePropertiesLibrary.getMass(typeID)));
   }
 #ifdef AUTOPAS_OPENMP
 #pragma omp parallel
 #endif
   for (auto iter = autopas.begin(); iter.isValid(); ++iter) {
-    maxwellBoltzmannDistribution(*iter, factors[iter->getTypeId()]);
+    addMaxwellBoltzmannDistributedVelocity(*iter, factors[iter->getTypeId()]);
   }
 }
 
