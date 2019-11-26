c742ff8cda66a02ff8c5b75a0bbd7a8b14b5dc22
Jakob Englhauser <jakob@englhauer.de>
2019-11-26 09:07:20 +0100

    More Tests.

--- tests/testAutopas/tests/selectors/tuningStrategy/ActiveHarmonyTest.cpp
+++ tests/testAutopas/tests/selectors/tuningStrategy/ActiveHarmonyTest.cpp
@@ -12,6 +12,7 @@ TEST_F(ActiveHarmonyTest, testSearchSpaceEmpty) {
   autopas::ActiveHarmony activeHarmony(std::set<autopas::ContainerOption>({}), autopas::NumberInterval<double>(),
                                        std::set<autopas::TraversalOption>({}), std::set<autopas::DataLayoutOption>({}),
                                        std::set<autopas::Newton3Option>({}));
+
   EXPECT_TRUE(activeHarmony.searchSpaceIsEmpty());
   EXPECT_FALSE(activeHarmony.searchSpaceIsTrivial());
   EXPECT_THAT(activeHarmony.getAllowedContainerOptions(), ::testing::IsEmpty());
@@ -23,7 +24,43 @@ TEST_F(ActiveHarmonyTest, testSearchSpaceOneOption) {
                                        {autopas::TraversalOption::directSumTraversal},
                                        {autopas::DataLayoutOption::soa},
                                        {autopas::Newton3Option::enabled});
+
   EXPECT_FALSE(activeHarmony.searchSpaceIsEmpty());
   EXPECT_TRUE(activeHarmony.searchSpaceIsTrivial());
   EXPECT_THAT(activeHarmony.getAllowedContainerOptions(), ::testing::ElementsAre(autopas::ContainerOption::directSum));
+}
+
+TEST_F(ActiveHarmonyTest, testSearchSpaceMoreOptions) {
+  autopas::ActiveHarmony activeHarmony({autopas::ContainerOption::linkedCells},
+                                     autopas::NumberInterval<double>(),
+                                     {autopas::TraversalOption::c08},
+                                     {autopas::DataLayoutOption::soa},
+                                     autopas::Newton3Option::getAllOptions());
+
+  EXPECT_FALSE(activeHarmony.searchSpaceIsEmpty());
+  EXPECT_FALSE(activeHarmony.searchSpaceIsTrivial());
+  EXPECT_THAT(activeHarmony.getAllowedContainerOptions(), ::testing::ElementsAre(autopas::ContainerOption::linkedCells));
+}
+
+TEST_F(ActiveHarmonyTest, testRemoveN3OptionRemoveAll) {
+  autopas::ActiveHarmony activeHarmony({autopas::ContainerOption::linkedCells},
+                                     autopas::NumberInterval<double>(),
+                                     {autopas::TraversalOption::c08, autopas::TraversalOption::sliced},
+                                     autopas::DataLayoutOption::getAllOptions(),
+                                     {autopas::Newton3Option::enabled});
+
+  EXPECT_THROW(activeHarmony.removeN3Option(autopas::Newton3Option::enabled),
+          autopas::utils::ExceptionHandler::AutoPasException);
+}
+
+TEST_F(ActiveHarmonyTest, testRemoveN3OptionRemoveSome) {
+  autopas::ActiveHarmony activeHarmony({autopas::ContainerOption::linkedCells},
+                                     autopas::NumberInterval<double>(),
+                                     {autopas::TraversalOption::c08, autopas::TraversalOption::sliced},
+                                     autopas::DataLayoutOption::getAllOptions(),
+                                     autopas::Newton3Option::getAllOptions());
+
+  EXPECT_NO_THROW(activeHarmony.removeN3Option(autopas::Newton3Option::enabled));
+  EXPECT_FALSE(activeHarmony.searchSpaceIsEmpty());
+  EXPECT_FALSE(activeHarmony.searchSpaceIsTrivial());
 }
