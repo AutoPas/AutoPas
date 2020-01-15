0757fbc5b8748048a9a3d922efc96bc37cb0ed26
Steffen Seckler <steffen.seckler@tum.de>
2020-01-15 17:34:20 +0100

    make sph functors use templates

--- examples/sph-mpi/sph-main-mpi.cpp
+++ examples/sph-mpi/sph-main-mpi.cpp
@@ -13,8 +13,9 @@
 #include "autopas/AutoPas.h"
 #include "autopas/sph/autopassph.h"
 
-typedef autopas::AutoPas<autopas::sph::SPHParticle, autopas::FullParticleCell<autopas::sph::SPHParticle>>
-    AutoPasContainer;
+using Particle = autopas::sph::SPHParticle;
+using Cell = autopas::FullParticleCell<Particle>;
+using AutoPasContainer = autopas::AutoPas<Particle, Cell>;
 
 void SetupIC(AutoPasContainer &sphSystem, double *end_time, const std::array<double, 3> &bBoxMax) {
   // Place SPH particles
@@ -24,7 +25,7 @@ void SetupIC(AutoPasContainer &sphSystem, double *end_time, const std::array<dou
   for (double x = 0; x < bBoxMax[0] * 0.5; x += dx) {  // NOLINT
     for (double y = 0; y < bBoxMax[1]; y += dx) {      // NOLINT
       for (double z = 0; z < bBoxMax[2]; z += dx) {    // NOLINT
-        autopas::sph::SPHParticle ith({x, y, z}, {0, 0, 0}, i++, 0.75, 0.012, 0.);
+        Particle ith({x, y, z}, {0, 0, 0}, i++, 0.75, 0.012, 0.);
         ith.setDensity(1.0);
         ith.setEnergy(2.5);
         if (autopas::utils::inBox(ith.getR(), sphSystem.getBoxMin(), sphSystem.getBoxMax())) {
@@ -36,7 +37,7 @@ void SetupIC(AutoPasContainer &sphSystem, double *end_time, const std::array<dou
   for (double x = bBoxMax[0] * 0.5; x < bBoxMax[0] * 1.; x += dx * 2.0) {  // NOLINT
     for (double y = 0; y < bBoxMax[1]; y += dx) {                          // NOLINT
       for (double z = 0; z < bBoxMax[2]; z += dx) {                        // NOLINT
-        autopas::sph::SPHParticle ith({x, y, z}, {0, 0, 0}, i++, 0.75, 0.012, 0.);
+        Particle ith({x, y, z}, {0, 0, 0}, i++, 0.75, 0.012, 0.);
         ith.setDensity(0.5);
         ith.setEnergy(2.5);
         if (autopas::utils::inBox(ith.getR(), sphSystem.getBoxMin(), sphSystem.getBoxMax())) {
@@ -142,7 +143,7 @@ int getSendRecvPartner(const std::array<int, 3> diff, MPI_Comm &comm, bool recvP
 
 int getReceivePartner(const std::array<int, 3> diff, MPI_Comm &comm) { return getSendRecvPartner(diff, comm, true); }
 
-void issueSend(std::vector<autopas::sph::SPHParticle> &sendParticles, const std::array<int, 3> diff, MPI_Comm &comm,
+void issueSend(std::vector<Particle> &sendParticles, const std::array<int, 3> diff, MPI_Comm &comm,
                MPI_Request &sendRequest, std::vector<double> &buffer) {
   int neighbor = getSendRecvPartner(diff, comm, false);
 
@@ -153,7 +154,7 @@ void issueSend(std::vector<autopas::sph::SPHParticle> &sendParticles, const std:
   MPI_Isend(buffer.data(), buffer.size(), MPI_DOUBLE, neighbor, 3, comm, &sendRequest);
 }
 
-void receive(std::vector<autopas::sph::SPHParticle> &receiveParticles, const std::array<int, 3> diff, MPI_Comm &comm) {
+void receive(std::vector<Particle> &receiveParticles, const std::array<int, 3> diff, MPI_Comm &comm) {
   int neighbor = getReceivePartner(diff, comm);
 
   int length;
@@ -164,7 +165,7 @@ void receive(std::vector<autopas::sph::SPHParticle> &receiveParticles, const std
   MPI_Recv(recvBuffer.data(), length, MPI_DOUBLE, neighbor, 3, comm, MPI_STATUS_IGNORE);
 
   for (size_t i = 0; i < (size_t)length;) {
-    auto p = autopas::sph::SPHParticle::deserialize(recvBuffer.data(), i);
+    auto p = Particle::deserialize(recvBuffer.data(), i);
     receiveParticles.push_back(p);
   }
 }
@@ -272,7 +273,7 @@ void updateHaloParticles(AutoPasContainer &sphSystem, MPI_Comm &comm, const std:
   for (diff[0] = -1; diff[0] < 2; diff[0]++) {
     for (diff[1] = -1; diff[1] < 2; diff[1]++) {
       for (diff[2] = -1; diff[2] < 2; diff[2]++) {
-        std::vector<autopas::sph::SPHParticle> sendParticles;
+        std::vector<Particle> sendParticles;
         if (not diff[0] and not diff[1] and not diff[2]) {
           // at least one dimension has to be non-zero
           continue;
@@ -286,14 +287,14 @@ void updateHaloParticles(AutoPasContainer &sphSystem, MPI_Comm &comm, const std:
         for (auto iterator =
                  sphSystem.getRegionIterator(requiredHaloMin, requiredHaloMax, autopas::IteratorBehavior::ownedOnly);
              iterator.isValid(); ++iterator) {
-          autopas::sph::SPHParticle p = *iterator;  // copies Particle
+          Particle p = *iterator;  // copies Particle
           p.addR(shift);
           sendParticles.push_back(p);
         }
         MPI_Request sendRequest;
         issueSend(sendParticles, diff, comm, sendRequest, buffer);
 
-        std::vector<autopas::sph::SPHParticle> receiveParticles;
+        std::vector<Particle> receiveParticles;
         receive(receiveParticles, diff, comm);
         for (auto &particle : receiveParticles) {
           sphSystem.addOrUpdateHaloParticle(particle);
@@ -305,8 +306,7 @@ void updateHaloParticles(AutoPasContainer &sphSystem, MPI_Comm &comm, const std:
   }
 }
 
-void periodicBoundaryUpdate(AutoPasContainer &sphSystem, MPI_Comm &comm,
-                            const std::vector<autopas::sph::SPHParticle> &invalidParticles,
+void periodicBoundaryUpdate(AutoPasContainer &sphSystem, MPI_Comm &comm, const std::vector<Particle> &invalidParticles,
                             std::array<double, 3> globalBoxMin, std::array<double, 3> globalBoxMax) {
   std::array<double, 3> boxMin = sphSystem.getBoxMin();
   std::array<double, 3> boxMax = sphSystem.getBoxMax();
@@ -319,7 +319,7 @@ void periodicBoundaryUpdate(AutoPasContainer &sphSystem, MPI_Comm &comm,
   for (diff[0] = -1; diff[0] < 2; diff[0]++) {
     for (diff[1] = -1; diff[1] < 2; diff[1]++) {
       for (diff[2] = -1; diff[2] < 2; diff[2]++) {
-        std::vector<autopas::sph::SPHParticle> sendParticles;
+        std::vector<Particle> sendParticles;
         if (not diff[0] and not diff[1] and not diff[2]) {
           // at least one dimension has to be non-zero
           continue;
@@ -341,7 +341,7 @@ void periodicBoundaryUpdate(AutoPasContainer &sphSystem, MPI_Comm &comm,
         MPI_Request sendRequest;
         issueSend(sendParticles, diff, comm, sendRequest, buffer);
 
-        std::vector<autopas::sph::SPHParticle> receiveParticles;
+        std::vector<Particle> receiveParticles;
         receive(receiveParticles, diff, comm);
         for (auto &particle : receiveParticles) {
           for (int i = 0; i < 3; i++) {
@@ -365,8 +365,8 @@ void periodicBoundaryUpdate(AutoPasContainer &sphSystem, MPI_Comm &comm,
 void densityPressureHydroForce(AutoPasContainer &sphSystem, MPI_Comm &comm, const std::array<double, 3> &globalBoxMin,
                                const std::array<double, 3> &globalBoxMax) {
   // declare the used functors
-  autopas::sph::SPHCalcDensityFunctor densityFunctor;
-  autopas::sph::SPHCalcHydroForceFunctor hydroForceFunctor;
+  autopas::sph::SPHCalcDensityFunctor<Particle, Cell> densityFunctor;
+  autopas::sph::SPHCalcHydroForceFunctor<Particle, Cell> hydroForceFunctor;
 
   // 1.first calculate density
   // 1.1 to calculate the density we need the halo particles
