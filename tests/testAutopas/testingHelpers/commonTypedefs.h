b78c795c0bbe05abdc2b222d4f0d88418d9db484
Steffen Seckler <steffen.seckler@tum.de>
2020-01-15 16:04:11 +0100

    commonTypedefs.h: replace typedefs with using

--- tests/testAutopas/testingHelpers/commonTypedefs.h
+++ tests/testAutopas/testingHelpers/commonTypedefs.h
@@ -10,28 +10,28 @@
 #include "autopas/particles/Particle.h"
 #include "mocks/MockFunctor.h"
 
-// a place for typedefs that are commonly used in tests
+// a place for usings that are commonly used in tests
 
 /**
  * Short for AutoPas Particle
  */
-typedef autopas::Particle Particle;
+using Particle = autopas::Particle;
 /**
  * Short for a FullParticle Cell with the AutoPas Particle
  */
-typedef autopas::FullParticleCell<autopas::Particle> FPCell;
+using FPCell = autopas::FullParticleCell<autopas::Particle>;
 
 /**
  * Short for the AutoPas single center Lennard-Jones molecule
  */
-typedef autopas::MoleculeLJ<> Molecule;
+using Molecule = autopas::MoleculeLJ<>;
 /**
  * Short for the Full Particle Cell with the single center Lennard-Jones molecule
  */
-typedef autopas::FullParticleCell<Molecule> FMCell;
+using FMCell = autopas::FullParticleCell<Molecule>;
 
 // M prefix for mocks
 /**
  * Short for Mock Functor
  */
-typedef MockFunctor<autopas::Particle, autopas::FullParticleCell<autopas::Particle>> MFunctor;
+using MFunctor = MockFunctor<autopas::Particle, autopas::FullParticleCell<autopas::Particle>>;
