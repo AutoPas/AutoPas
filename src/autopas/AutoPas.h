96d2d9c80c4d337ee61e8ffb28e9540dd8e22623
Steffen Seckler <steffen.seckler@tum.de>
2020-01-28 12:11:10 +0100

    Revert "test whether doxygen fails properly"
    This reverts commit a921cfcf7c41f03028b50c442eae662a1ed6464d.

--- src/autopas/AutoPas.h
+++ src/autopas/AutoPas.h
@@ -38,7 +38,9 @@ static unsigned int _instanceCounter = 0;
 template <class Particle, class ParticleCell>
 class AutoPas {
  public:
-
+  /**
+   * Particle type to be accessible after initialization.
+   */
   using Particle_t = Particle;
 
   /**
