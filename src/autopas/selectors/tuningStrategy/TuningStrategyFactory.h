d70e6fed405a2656557502d2bafe798dd807870f
Steffen Seckler <steffen.seckler@tum.de>
2020-02-07 16:52:08 +0100

    fix doc

--- src/autopas/selectors/tuningStrategy/TuningStrategyFactory.h
+++ src/autopas/selectors/tuningStrategy/TuningStrategyFactory.h
@@ -24,8 +24,9 @@ namespace autopas::TuningStrategyFactory {
  * @return Pointer to the tuning strategy object or the nullpointer if an exception was suppressed.
  */
 std::unique_ptr<autopas::TuningStrategyInterface> generateTuningStrategy(
-    TuningStrategyOption tuningStrategyOption, const std::set<ContainerOption> &allowedContainers,
-    NumberSet<double> &allowedCellSizeFactors, const std::set<TraversalOption> &allowedTraversals,
-    const std::set<DataLayoutOption> &allowedDataLayouts, const std::set<Newton3Option> &allowedNewton3Options,
-    unsigned int maxEvidence, AcquisitionFunctionOption acquisitionFunctionOption);
+    autopas::TuningStrategyOption tuningStrategyOption, const std::set<autopas::ContainerOption> &allowedContainers,
+    autopas::NumberSet<double> &allowedCellSizeFactors, const std::set<autopas::TraversalOption> &allowedTraversals,
+    const std::set<autopas::DataLayoutOption> &allowedDataLayouts,
+    const std::set<autopas::Newton3Option> &allowedNewton3Options, unsigned int maxEvidence,
+    AcquisitionFunctionOption acquisitionFunctionOption);
 }  // namespace autopas::TuningStrategyFactory
