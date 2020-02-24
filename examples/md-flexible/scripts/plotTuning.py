48b7ef3b920c7b307802a3efc02f661262035611
FG-TUM <f.gratl@tum.de>
2020-02-24 17:35:19 +0100

    color markers by container

--- examples/md-flexible/scripts/plotTuning.py
+++ examples/md-flexible/scripts/plotTuning.py
@@ -1,11 +1,18 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 import re
 import plotly.graph_objects as go
 
-datafile='testTuning_2020-02-22_23-44-40/MDFlex_Case1.out'
+datafile='testTuning_2020-02-24_14-11-02/MDFlex_Case1.out'
 
 regex='.* Collected times for +({.*}).*\[(.*)\].*: *([0-9]+)'
+
+# ------------------------------------------------- Functions -------------------------------------------------
+
+def parseConfigToDict(confStr):
+    return {key.strip():val.strip() for key,val in [pair.split(":") for pair in confStr[1:-1].split(',')]}
+
+# --------------------------------------------------- Script --------------------------------------------------
 
 # lists to gather data series
 configs = []
@@ -19,7 +26,7 @@ with open(datafile) as f:
     for line in f.readlines():
         match = re.search(regex, line)
         if match:
-            configs.append(match.group(1))
+            configs.append(parseConfigToDict(match.group(1)))
             thisTimes=[int(s) for s in match.group(2).split()]
             value=int(match.group(3))
             times.append(thisTimes)
@@ -27,14 +34,28 @@ with open(datafile) as f:
             timesErrorMinus.append(value-min(thisTimes))
             values.append(value)
 
-fig = go.Figure(data=go.Scatter(
-        x=[n*3 for n in range(0, len(configs))],
-        y=values,
-        error_y=dict(
-            type='data',
-            symmetric=False,
-            array=timesErrorPlus,
-            arrayminus=timesErrorMinus,
-            )
-        ))
+# create figure and define layout
+fig = go.Figure(
+        layout=dict(
+                showlegend=True,
+            ),
+        )
+
+# add data
+fig.add_trace(go.Scatter(
+            x=[n*3 for n in range(0, len(configs))],
+            y=values,
+            mode="lines+markers",
+            error_y=dict(
+                type='data',
+                symmetric=False,
+                array=timesErrorPlus,
+                arrayminus=timesErrorMinus,
+                ),
+            hovertext=configs,
+            marker=dict(
+                color=["#%0.6X" % (hash(c["Container"]) % (256**3)) for c in configs],
+                )
+            ),
+        )
 fig.show()
