ff49501eac808eb9e50902a90b35842e1b2cdcd3
FG-TUM <f.gratl@tum.de>
2020-03-09 15:23:54 +0100

    justify if

--- examples/md-flexible/Simulation.h
+++ examples/md-flexible/Simulation.h
@@ -267,6 +267,7 @@ void Simulation<Particle, ParticleCell>::calculateForces() {
 
   FunctorType functor{_autopas.getCutoff(), *_particlePropertiesLibrary};
   bool tuningIteration = _autopas.iteratePairwise(&functor);
+  // only generate this output if the logger is set to debug
   if (autopas::Logger::get()->level() <= autopas::Logger::LogLevel::debug) {
     std::cout << "Tuning: " << std::boolalpha << tuningIteration << std::endl;
   }
