6c983938efbc4a06d6a34caec7d6cdb040e8e834
FG-TUM <f.gratl@tum.de>
2020-03-25 21:30:05 +0100

    workaround for stupid icpc

--- tests/testAutopas/tests/pairwiseFunctors/LJFunctorTestNoGlobals.h
+++ tests/testAutopas/tests/pairwiseFunctors/LJFunctorTestNoGlobals.h
@@ -23,8 +23,9 @@ class LJFunctorTestNoGlobals : public LJFunctorTest {
   constexpr static double epsilon2{2.};
   constexpr static double sigma2{2.};
 
-  constexpr static std::array<double, 3> expectedForce{-4547248.8989645941, -9094497.7979291882, -13641746.696893783};
-  constexpr static std::array<double, 3> expectedForceMixing{-835415983.7676939964294, -1670831967.5353879928588,
+  // if we make this static constexpr icpc sets them to 0
+  const std::array<double, 3> expectedForce{-4547248.8989645941, -9094497.7979291882, -13641746.696893783};
+  const std::array<double, 3> expectedForceMixing{-835415983.7676939964294, -1670831967.5353879928588,
                                                              -2506247951.3030819892883};
 
   constexpr static double absDelta{1e-7};
