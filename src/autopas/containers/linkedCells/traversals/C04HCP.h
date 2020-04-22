5c82caf523d5b8c1b8a3daf8a79929325118ac32
sabrinakrallmann <sabrina.krallmann@tum.de>
2020-04-22 17:42:26 +0200

    Modify c04HCP Traversal, clang-format changes.

--- src/autopas/containers/linkedCells/traversals/C04HCP.h
+++ src/autopas/containers/linkedCells/traversals/C04HCP.h
@@ -6,8 +6,7 @@
 
 #pragma once
 
-namespace autopas{
-
+namespace autopas {
 
 /**
  * This class provides the c04 hcp traversal.
@@ -23,10 +22,8 @@ namespace autopas{
 template <class ParticleCell, class PairwiseFunctor, DataLayoutOption::Value dataLayout, bool useNewton3>
 
 class C04HCP : public C08BasedTraversal<ParticleCell, PairwiseFunctor, dataLayout, useNewton3>,
-                     public LinkedCellTraversalInterface<ParticleCell> {
-
-  public:
-
+               public LinkedCellTraversalInterface<ParticleCell> {
+ public:
   /**
    * Constructor of c04hcp
    * @param dims The dimensions of the cellblock, i.e. the number of cells in x,
@@ -35,13 +32,12 @@ class C04HCP : public C08BasedTraversal<ParticleCell, PairwiseFunctor, dataLayou
    * @param interactionLength Interaction length.
    * @param cellLength cell length.
    */
-  C04HCP(const std::array<unsigned long, 3> &dims, PairwiseFunctor *pairwiseFunctor,
-         const double interactionLength, const std::array<double, 3> &cellLength)
-          : C08BasedTraversal<ParticleCell, PairwiseFunctor, dataLayout, useNewton3>(dims, pairwiseFunctor,
-                                                                                     interactionLength, cellLength),
-            _cellHandler(pairwiseFunctor, this->_cellsPerDimension, interactionLength, cellLength, this->_overlap),
-            _end(utils::ArrayMath::subScalar(utils::ArrayUtils::static_cast_array<long>(this->_cellsPerDimension), 1l)){
-  }
+  C04HCP(const std::array<unsigned long, 3> &dims, PairwiseFunctor *pairwiseFunctor, const double interactionLength,
+         const std::array<double, 3> &cellLength)
+      : C08BasedTraversal<ParticleCell, PairwiseFunctor, dataLayout, useNewton3>(dims, pairwiseFunctor,
+                                                                                 interactionLength, cellLength),
+        _cellHandler(pairwiseFunctor, this->_cellsPerDimension, interactionLength, cellLength, this->_overlap),
+        _end(utils::ArrayMath::subScalar(utils::ArrayUtils::static_cast_array<long>(this->_cellsPerDimension), 1l)) {}
 
   void traverseParticlePairs() override;
 
@@ -51,18 +47,17 @@ class C04HCP : public C08BasedTraversal<ParticleCell, PairwiseFunctor, dataLayou
 
   bool getUseNewton3() const override { return useNewton3; }
 
-    bool isApplicable() const override { //TODO: Individual Restrictions
-      if (dataLayout == DataLayoutOption::cuda) {
-        return false;
-      }
-      const double minLength = *std::min_element(this->_cellLength.cbegin(), this->_cellLength.cend());
-      const unsigned long minDim = *std::min_element(this->_cellsPerDimension.cbegin(), this->_cellsPerDimension.cend());
-
-      return minLength >= this->_interactionLength and minDim > 3;
+  bool isApplicable() const override {  // TODO: Individual Restrictions
+    if (dataLayout == DataLayoutOption::cuda) {
+      return false;
     }
+    const double minLength = *std::min_element(this->_cellLength.cbegin(), this->_cellLength.cend());
+    const unsigned long minDim = *std::min_element(this->_cellsPerDimension.cbegin(), this->_cellsPerDimension.cend());
 
-  private:
+    return minLength >= this->_interactionLength and minDim > 3;
+  }
 
+ private:
   void traverseSingleColor(std::vector<ParticleCell> &cells, int color);
 
   void processBasePack6(std::vector<ParticleCell> &cells, const std::array<long, 3> &base3DIndex);
@@ -70,33 +65,30 @@ class C04HCP : public C08BasedTraversal<ParticleCell, PairwiseFunctor, dataLayou
   C08CellHandler<ParticleCell, PairwiseFunctor, dataLayout, useNewton3> _cellHandler;
 
   const std::array<long, 3> _end;
-
 };
 
 template <class ParticleCell, class PairwiseFunctor, DataLayoutOption::Value dataLayout, bool useNewton3>
 void C04HCP<ParticleCell, PairwiseFunctor, dataLayout, useNewton3>::processBasePack6(
-  std::vector<ParticleCell> &cells, const std::array<long, 3> &base3DIndex) {
-
+    std::vector<ParticleCell> &cells, const std::array<long, 3> &base3DIndex) {
   using utils::ThreeDimensionalMapping::threeToOneD;
   std::array<long, 3> index;
   const std::array<long, 3> signedDims = utils::ArrayUtils::static_cast_array<long>(this->_cellsPerDimension);
 
-  for(long z = 0; z < 3; ++z){ //go through the six cells
-    for(long x = 0; x < 2; ++x){
+  for (long z = 0; z < 3; ++z) {  // go through the six cells
+    for (long x = 0; x < 2; ++x) {
       index[0] = base3DIndex[0] + x;
       index[1] = base3DIndex[1];
       index[2] = base3DIndex[2] + z;
 
       bool isIn = true;
-      for (int d = 0; d < 3; ++d){
-        isIn &= (index[d] >= 0l) and (index[d] <= (_end[d] - this->_overlap[d])); //prevent using overlapping cells
+      for (int d = 0; d < 3; ++d) {
+        isIn &= (index[d] >= 0l) and (index[d] <= (_end[d] - this->_overlap[d]));  // prevent using overlapping cells
       }
 
-      if(isIn){ //skip cells outside radius
+      if (isIn) {  // skip cells outside radius
         const unsigned long ulIndex = threeToOneD(index, signedDims);
         _cellHandler.processBaseCell(cells, ulIndex);
       }
-
     }
   }
 }
@@ -122,12 +114,12 @@ void C04HCP<ParticleCell, PairwiseFunctor, dataLayout, useNewton3>::traversePart
 
 template <class ParticleCell, class PairwiseFunctor, DataLayoutOption::Value dataLayout, bool useNewton3>
 void C04HCP<ParticleCell, PairwiseFunctor, dataLayout, useNewton3>::traverseSingleColor(
-        std::vector<ParticleCell> &cells, int color) {
-
+    std::vector<ParticleCell> &cells, int color) {
   // determine a starting point of one of the grids
   std::array<long, 3> startOfThisColor{};
 
-  // only need to shift z-dimension, because in each z dimension the colors behave like the 0-color in the z = 0 dimension
+  // only need to shift z-dimension, because in each z dimension the colors behave like the 0-color in the z = 0
+  // dimension
   switch (color) {
     case 0:
       startOfThisColor = {0l, 0l, 0l};
@@ -143,40 +135,39 @@ void C04HCP<ParticleCell, PairwiseFunctor, dataLayout, useNewton3>::traverseSing
       break;
   }
 
-
   // to fix compiler complaints about perfectly nested loop.
   long startX = startOfThisColor[0], endX = _end[0];
   long startY = startOfThisColor[1], endY = _end[1];
   long startZ = startOfThisColor[2], endZ = _end[2];
 
-  //iterate over cartesian grid
+  // iterate over cartesian grid
 #if defined(AUTOPAS_OPENMP)
 #pragma omp for schedule(dynamic, 1) collapse(3) nowait
 #endif
   for (long z = startZ; z < endZ; z += 4) {
-    for (long y = startY; y < endY; y++) { //iterate over every second y-row
-      for (long x = startX; x < (endX + 4); x += 6) { // color starts every 6th column again, the +4 is needed to prevent ending too early, since it will be shifted inside the loop
+    for (long y = startY; y < endY; y++) {  // iterate over every second y-row
+      for (long x = startX; x < (endX + 4);
+           x += 6) {  // color starts every 6th column again, the +4 is needed to prevent ending too early, since it
+                      // will be shifted inside the loop
         long x_index = x;
-        switch((z-startZ) % 12 / 4){ //shift on x-axis according to z value
+        switch ((z - startZ) % 12 / 4) {  // shift on x-axis according to z value
           case 0:
             break;
           case 1:
-            x_index -=4;
+            x_index -= 4;
             break;
           case 2:
-            x_index -=2;
+            x_index -= 2;
             break;
         }
-        if((y - startY) % 2 != 0){
-          x_index+=3;
+        if ((y - startY) % 2 != 0) {
+          x_index += 3;
         }
         const std::array<long, 3> base3DIndex = {x_index, y, z};
         processBasePack6(cells, base3DIndex);
       }
     }
   }
-
 }
 
-
-}
+}  // namespace autopas
