87a964738f9060956c1b8ee02118263294e31bd3
Vincent Fischer <fischervincent98@gmail.com>
2020-04-28 14:55:59 +0200

    add load estimators for cell based containers
    load estimation heuristics will now be implemented in the namespace
    autopas::loadEstimators.  This commit includes a heuristic which sums up
    the squared number of particles of all cells within the region, as well
    as a constant heuristic for comparison purposes.

---
+++ src/autopas/containers/loadEstimators/cellBasedHeuristics.h
@@ -0,0 +1,77 @@
+/**
+ * @file cellBasedHeuristics.h
+ *
+ * @date 24 Apr 2020
+ * @author fischerv
+ */
+
+#pragma once
+
+#include <array>
+#include <vector>
+
+#include "autopas/utils/ThreeDimensionalMapping.h"
+
+namespace autopas {
+
+/**
+ * Collection of functions for estimating the load required to update a specific region within a containers
+ */
+namespace loadEstimators {
+
+/**
+ * Possible heuristics
+ */
+enum class CellBasedHeuristic {
+  none = 0,
+  squaredCellSize = 1,
+};
+
+/**
+ * Squared Cell Size
+ * @param cells
+ * @param cellsPerdimension
+ * @param lowerCorner lower boundary indices for region
+ * @param upperCorner upper boundary indices for region
+ */
+template <class ParticleCell>
+unsigned long squaredCellSize(const std::vector<ParticleCell> &cells,
+                              const std::array<unsigned long, 3> &cellsPerDimension,
+                              const std::array<unsigned long, 3> &lowerCorner,
+                              const std::array<unsigned long, 3> &upperCorner) {
+  unsigned long sum = 0;
+  for (unsigned long x = lowerCorner[0]; x <= upperCorner[0]; x++) {
+    for (unsigned long y = lowerCorner[1]; y <= upperCorner[1]; y++) {
+      for (unsigned long z = lowerCorner[2]; z <= upperCorner[2]; z++) {
+        auto load =
+            cells[autopas::utils::ThreeDimensionalMapping::threeToOneD(x, y, z, cellsPerDimension)].numParticles();
+        sum += load * load;
+      }
+    }
+  }
+  return sum;
+}
+
+/**
+ * estimateCellBasedLoad
+ * @param heuristic
+ * @param cells
+ * @param cellsPerdimension
+ * @param lowerCorner lower boundary indices for region
+ * @param upperCorner upper boundary indices for region
+ */
+template <class ParticleCell>
+unsigned long estimateCellBasedLoad(const CellBasedHeuristic &heuristic, const std::vector<ParticleCell> &cells,
+                                    const std::array<unsigned long, 3> &cellsPerDimension,
+                                    const std::array<unsigned long, 3> &lowerCorner,
+                                    const std::array<unsigned long, 3> &upperCorner) {
+  switch (heuristic) {
+    case CellBasedHeuristic::none:
+      return 1;
+    case CellBasedHeuristic::squaredCellSize:
+      return squaredCellSize(cells, cellsPerDimension, lowerCorner, upperCorner);
+  }
+  return 1;
+}
+}  // namespace loadEstimators
+}  // namespace autopas
