ebad8198cda5850d8844e0d9eb554d7b43be22c2
Thieme Wolf <thiemewolf@gmail.com>
2020-04-30 21:48:00 +0200

    Provide structure for FullSearchMPI tests

---
+++ tests/MPIParallelAutoPasTests/allTests.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file allTests.cpp
+ * @author seckler
+ * @date 18.01.18
+ */
+
+#include <gtest/gtest.h>
+#include <mpi.h>
+
+int main(int argc, char **argv) {
+  int result = 0;
+
+  testing::InitGoogleTest(&argc, argv);
+  // set the gtest death test style to threadsafe
+  testing::FLAGS_gtest_death_test_style = "threadsafe";
+
+  MPI_Init(&argc, argv);
+  // running only my tests
+  result = RUN_ALL_TESTS();
+  MPI_Finalize();
+  
+  return result;
+}
