17012f1e89132aba38d95723888fe897ad00149d
FG-TUM <f.gratl@tum.de>
2020-05-25 10:30:30 +0200

    pleasing codacity

--- examples/md-flexible/src/parsing/MDFlexParser.cpp
+++ examples/md-flexible/src/parsing/MDFlexParser.cpp
@@ -12,7 +12,7 @@ MDFlexParser::exitCodes MDFlexParser::parseInput(int argc, char **argv, MDFlexCo
   for (int i = 0; i < argc; i++) {
     auto len = std::string(argv[i]).length() + 1;
     argvCopy[i] = new char[len];
-    strncpy(argvCopy[i], argv[i], len);
+    strcpy(argvCopy[i], argv[i]);
   }
   argvCopy[argc] = nullptr;
 
