4c1bc7167e58d100a96fc8179a5e90d07c04f30e
FG-TUM <f.gratl@tum.de>
2020-05-27 18:41:14 +0200

    removed unused return statement

--- src/autopas/utils/TupleUtils.h
+++ src/autopas/utils/TupleUtils.h
@@ -20,7 +20,7 @@ namespace autopas::utils::TupleUtils {
  */
 template <class T, class F>
 void for_each(T &&tuple, F &&f) {
-  return std::apply(
+  std::apply(
       // function that takes all elements of the tuple as variadic argument
       [&](auto &... t) {
         // unpack the tuple using a fold (... op pack) with op as the comma operator to apply a lambda on every element
