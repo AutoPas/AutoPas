ff28fb828de9b1344ac017221a7b4f71b6e01db8
sabrinakrallmann <sabrina.krallmann@tum.de>
2020-07-16 11:31:12 +0200

    outToCsv for Pandas first draft

---
+++ examples/md-flexible/scripts/outToCsvForPandas.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import plotly.graph_objects as go
+import re
+import numpy
+import csv
+
+# THIS SCRIPT NEEDS AT LEAST PYTHON 3.8.
+# However, lesser version will probably fail due to invalid syntax instead of this assertion
+assert sys.version_info >= (3, 8)
+
+# ---------------------------------------------- Input ----------------------------------------------
+
+# help message
+for arg in sys.argv[1:]:
+    if "--help" in arg:
+        print(
+            "Usage: ./plotTuning.py parameter=[number, size, density] [path/To//Output/*.out ...]. Meaning number = number of particles, size = boxSize and density = particle density")
+        print("Please use at least python 3.8.")
+        exit(0)
+
+# take all input files as source for a plot
+if len(sys.argv) > 1:
+    if ".out" in arg:
+        datafiles = sys.argv[1:]
+    else:
+        datadirs = sys.argv[1:]
+        datadirs = list(datadirs)
+        datadirs.sort(reverse=True)
+        datafiles = os.listdir(datadirs[0])
+        datafiles = filter(lambda s: s.endswith('.out'), datafiles)
+        datafiles = map(lambda s: datadirs[0] + "/" + s, datafiles)
+        datafiles = list(datafiles)
+
+
+#    TODO: if nothing is given search for the latest test dir
+
+# -------------------------------------------- Functions --------------------------------------------
+
+def parseConfigToDict(confStr):
+    print("confStr")
+    print(confStr)
+    return {key.strip(): val.strip() for key, val in [pair.split(":") for pair in confStr[1:-1].split(',')]}
+
+
+def appendForm(outerForm, appender):
+    if outerForm is not None:
+        innerForm = outerForm + '-' + appender
+    else:
+        innerForm = appender
+    return innerForm
+
+
+# ---------------------------------------------- Script ---------------------------------------------
+
+# values for plotting in the end
+configs = []
+valuesErrorPlus = []
+valuesErrorMinus = []
+rowList = []
+
+# gather data per file
+for datafile in datafiles:
+    # lists to gather data series
+    values = []
+    thisConfig = None
+    finishedHeader = False
+
+    regexSelectedConf = '.* Selected Configuration +({.*})'
+    regexCellSizeFactor = '.* CellSizeFactor +({.*})'
+    regexIterTook = '.* IteratePairwise took +([0-9]+) .*'
+    regexNumOfParticles = '.*particles-per-dimension*'
+    regexNumOfParticlesAbsolute = '.*numberOfParticles*'
+    regexBoxMin = '.*box-min*'
+    regexBoxMax = '.*box-max*'
+    regexTraversal = '.*traversal*'
+    regexSimulationStarted = 'Starting simulation...*'
+    regexCubeUniform = '.*CubeUniform:*'
+    regexCubeGrid = '.*CubeGrid:*'
+    regexSphere = '.*Sphere:*'
+    regexCubeGauss = '.*CubeGauss:*'
+
+with open(datafile) as f:
+    currentDensity = 0.0
+    foundTraversal = False
+    traversal = "noTraversal"
+    boxSizeListMin = []
+    boxSizeListMax = []
+    numberOfParticles = 0
+    form = None
+    container = None
+    traversal = None
+    cellSizeFactor = None
+    loadEstimator = None
+    dataLayout = None
+    newton3 = None
+
+    counter = 0
+    for line in f.readlines():
+        if not finishedHeader:
+            # parse header
+            if (match := re.search(regexSimulationStarted, line)) is not None:
+                finishedHeader = True
+            elif (match := re.search(regexNumOfParticles, line)) is not None:
+                currentLine = re.findall(r'\[(.*?)\]', line)  # get content inside the brackets
+                arrayOfCurrentLine = currentLine[0].split(',')  # split content inside brackets and show as array
+                numberOfParticles = numpy.prod(
+                    list(map(int, arrayOfCurrentLine)))  # calculate overall number of particles
+            elif (match := re.search(regexNumOfParticlesAbsolute, line)) is not None:
+                currentLine = line.split(':', 1)[1]
+                currentLine.strip()
+                numberOfParticles = currentLine
+            elif (match := re.search(regexBoxMax, line)) is not None:
+                currentLine = re.findall(r'\[(.*?)\]', line)  # get content inside the brackets
+                arrayOfCurrentLine = currentLine[0].split(',')
+                boxSizeListMax = list(map(float, arrayOfCurrentLine))
+            elif (match := re.search(regexBoxMin, line)) is not None:
+                currentLine = re.findall(r'\[(.*?)\]', line)  # get content inside the brackets
+                arrayOfCurrentLine = currentLine[0].split(',')
+                boxSizeListMin = list(map(float, arrayOfCurrentLine))
+            elif (match := re.search(regexCubeUniform, line)) is not None:
+                appendForm(form, 'CubeUniform')
+            elif (match := re.search(regexCubeGrid, line)) is not None:
+                appendForm(form, 'CubeGrid')
+            elif (match := re.search(regexSphere, line)) is not None:
+                appendForm(form, 'Sphere')
+            elif (match := re.search(regexCubeGauss, line)) is not None:
+                appendForm(form, 'CubeGauss')
+        # parse Iterations
+        elif (match := re.search(regexSelectedConf, line)) is not None:
+            thisConfig = parseConfigToDict(match.group(1))
+            container = thisConfig.get("Container")
+            traversal = thisConfig.get("Traversal")
+            cellSizeFactor = thisConfig.get("CellSizeFactor")
+            loadEstimator = thisConfig.get("Load Estimator")
+            dataLayout = thisConfig.get("Data Layout")
+            newton3 = thisConfig.get("Newton 3")
+        elif (match := re.search(regexIterTook, line)) is not None:
+            time = int(match.group(1))
+            x = boxSizeListMax[0] - boxSizeListMin[0]
+            y = boxSizeListMax[1] - boxSizeListMin[1]
+            z = boxSizeListMax[2] - boxSizeListMin[2]
+            rowList.append([traversal, container, newton3, dataLayout, cellSizeFactor, loadEstimator, form, x, y, z,
+                            numberOfParticles, time])
+
+# ---------------------------------------------- Write CSV ---------------------------------------------
+
+with open('mydata.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(
+        ["Traversal", "Container", "Newton3", "Data Layout", "Cellsize Factor", "Load Estimator",
+         "Form", "x", "y", "z", "Number of Particles", "Iteration Time"]) # TODO: "Name of Scenario",
+    writer.writerows(rowList)
