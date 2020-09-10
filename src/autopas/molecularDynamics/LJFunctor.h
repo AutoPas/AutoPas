5b031b8208702a34ca65dfc03147c48a0813acae
Steffen Seckler <steffen.seckler@tum.de>
2020-09-10 10:33:55 +0200

    Revert "Makes _PPLibrary pointer in LJFunctor const"
    This reverts commit 7b039aa53fc518ea5f0a05eb6bbf09efb54d0798.

--- src/autopas/molecularDynamics/LJFunctor.h
+++ src/autopas/molecularDynamics/LJFunctor.h
@@ -102,10 +102,11 @@ class LJFunctor
    * @param particlePropertiesLibrary
    */
   explicit LJFunctor(double cutoff, ParticlePropertiesLibrary<double, size_t> &particlePropertiesLibrary)
-      : LJFunctor(cutoff, nullptr), _PPLibrary{&particlePropertiesLibrary} {
+      : LJFunctor(cutoff, nullptr) {
     static_assert(useMixing,
                   "Not using Mixing but using a ParticlePropertiesLibrary is not allowed! Use a different constructor "
                   "or set mixing to true.");
+    _PPLibrary = &particlePropertiesLibrary;
   }
 
   bool isRelevantForTuning() override { return relevantForTuning; }
@@ -1098,7 +1099,7 @@ class LJFunctor
   // not const because they might be reset through PPL
   double _epsilon24, _sigmasquare, _shift6 = 0;
 
-  ParticlePropertiesLibrary<SoAFloatPrecision, size_t> *const _PPLibrary{nullptr};
+  ParticlePropertiesLibrary<SoAFloatPrecision, size_t> *_PPLibrary = nullptr;
 
   // sum of the potential energy, only calculated if calculateGlobals is true
   double _upotSum;
