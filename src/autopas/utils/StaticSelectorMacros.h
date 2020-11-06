2442b2b6d87513e5a80b1170fdb33f42736398aa
Steffen Seckler <steffen.seckler@tum.de>
2020-11-06 13:30:07 +0100

    makes withStaticBool possible to return things

--- src/autopas/utils/StaticSelectorMacros.h
+++ src/autopas/utils/StaticSelectorMacros.h
@@ -12,15 +12,16 @@ namespace autopas::utils {
  * The static value of the boolean will be passed using the first argument of function.
  * @param theBool The bool that should be used statically.
  * @param func Function to be called, should accept a static bool type, e.g., [&](auto theBool){};
+ * @return the return value of func
  */
 template <typename F>
-void withStaticBool(bool theBool, const F &&func) {
+decltype(auto) withStaticBool(bool theBool, const F &&func) {
   if (theBool) {
     std::true_type t;
-    func(t);
+    return func(t);
   } else {
     std::false_type f;
-    func(f);
+    return func(f);
   }
 }
 }  // namespace autopas::utils
