fb2b6068926af1559335f02b622b547feedbeed3
FG-TUM <f.gratl@tum.de>
2020-11-27 20:54:24 +0100

    fixed rename in test

--- tests/testAutopas/tests/selectors/SmoothingTest.cpp
+++ tests/testAutopas/tests/selectors/SmoothingTest.cpp
@@ -9,19 +9,19 @@
 #include "autopas/selectors/Smoothing.h"
 
 TEST(SmoothingTest, lowessLastPoint) {
-  std::vector<size_t> v_xval{1, 2, 3, 4, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 8, 10, 12};
-  std::vector<size_t> v_yval{18, 2, 15, 6, 10, 4, 16, 11, 7, 3, 14, 17, 20, 12, 9, 13, 1, 8};
+  std::vector<size_t> xvals{1, 2, 3, 4, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 8, 10, 12};
+  std::vector<size_t> yvals{18, 2, 15, 6, 10, 4, 16, 11, 7, 3, 14, 17, 20, 12, 9, 13, 1, 8};
 
-  std::vector<std::pair<size_t, long>> obs;
-  obs.reserve(v_xval.size());
-  for (size_t i = 0; i < v_xval.size(); ++i) {
-    obs.emplace_back(v_xval[i], v_yval[i]);
+  std::vector<std::pair<size_t, size_t>> obs;
+  obs.reserve(xvals.size());
+  for (size_t i = 0; i < xvals.size(); ++i) {
+    obs.emplace_back(xvals[i], yvals[i]);
   }
 
   // YS values with F = .25, NSTEPS = 0, DELTA = 0.0
   std::cout << "THE TEST" << std::endl;
   {
-    auto out = autopas::Smoothing::smoothLastPoint(obs, .25);
+    auto out = autopas::smoothing::smoothLastPoint(obs, .25);
     // out should be an integer (or long)
     EXPECT_EQ(out, 6);
   }
