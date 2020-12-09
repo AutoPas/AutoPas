727aa38551f0d3fa5cb8a0338e815d4a7869ea18
Steffen Seckler <steffen.seckler@tum.de>
2020-12-09 17:19:35 +0100

    doc

--- tests/testAutopas/addNameSpace.sh
+++ tests/testAutopas/addNameSpace.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# This script adds a namespace to the specified file. The name of the namespace is the name of the file without the extension.
+# It will add an opening `namespace namespacename {` after the last include.
+# The namespace is closed by adding a line at the end of the file.
+
 f=$1
 
 namespace=$(basename $f)
@@ -10,5 +14,5 @@ echo "namespace $namespace {" >> tempfile
 let line++
 tail --lines=+$line $f >> tempfile
 echo "" >> tempfile
-echo "} // end namespace $namespace" >> tempfile
+echo "}  // end namespace $namespace" >> tempfile
 mv tempfile $f
