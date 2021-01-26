05c7ea9f62f2f09cf785799e47d6e90d0b27a5f8
FG-TUM <f.gratl@tum.de>
2021-01-26 19:26:51 +0100

    doc

--- src/autopas/utils/logging/IterationLogger.h
+++ src/autopas/utils/logging/IterationLogger.h
@@ -13,11 +13,11 @@
 namespace autopas {
 
 /**
- * Helper to log tuning data of AutoTuner to a csv file for easier analysis.
+ * Helper to log performance data of AutoPas::iteratePairwise() to a csv file for easier analysis.
  *
  * It uses an asynchronous spd logger to write a csv file named "AutoPas_iterationPerformance_<dateStamp>.csv".
  *
- * By default logging the data is disabled. It can be enabled by setting the cmake variable AUTOPAS_Log_Predictions
+ * By default logging the data is disabled. It can be enabled by setting the cmake variable AUTOPAS_Log_Iterations
  * to ON.
  */
 class IterationLogger {
@@ -32,8 +32,21 @@ class IterationLogger {
    */
   ~IterationLogger();
 
+  /**
+   * Log the tuning time to the loggers buffer.
+   * @param timeTuning
+   */
   void logTimeTuning(long timeTuning);
 
+  /**
+   * Log the given arguments and the internal buffer to the csv file.
+   * @param configuration
+   * @param iteration
+   * @param inTuningPhase
+   * @param timeIteratePairwise
+   * @param timeRebuildNeighborLists
+   * @param timeWholeIteration
+   */
   void logIteration(const Configuration &configuration, size_t iteration, bool inTuningPhase, long timeIteratePairwise,
                     long timeRebuildNeighborLists, long timeWholeIteration);
 
