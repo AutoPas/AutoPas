5cd61119d8cc41ba2eb5d17e8dcf491ac66ef1a5
FG-TUM <FG-TUM@users.noreply.github.com>
2021-02-01 14:43:02 +0100

    Update src/autopas/utils/logging/IterationLogger.cpp
    Co-authored-by: Steffen Seckler <38119442+SteffenSeckler@users.noreply.github.com>

--- src/autopas/utils/logging/IterationLogger.cpp
+++ src/autopas/utils/logging/IterationLogger.cpp
@@ -12,7 +12,7 @@ autopas::IterationLogger::IterationLogger(const std::string &outputSuffix) {
 #ifdef AUTOPAS_LOG_ITERATIONS
   auto outputFileName("AutoPas_iterationPerformance_" + outputSuffix + utils::Timer::getDateStamp() + ".csv");
   // Start of workaround: Because we want to use an asynchronous logger we can't quickly switch patterns for the header.
-  // create and register a non-asychronous logger to write the header
+  // Create and register a non-asychronous logger to write the header.
   auto headerLoggerName = loggerName() + "header";
   auto headerLogger = spdlog::basic_logger_mt(headerLoggerName, outputFileName);
   // set the pattern to the message only
