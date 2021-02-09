787cdd6e97f1d581c47d6b362723122d4060f04d
FG-TUM <f.gratl@tum.de>
2021-02-09 14:32:11 +0100

    fix datatype

--- src/autopas/utils/logging/TuningDataLogger.cpp
+++ src/autopas/utils/logging/TuningDataLogger.cpp
@@ -19,7 +19,7 @@ autopas::TuningDataLogger::TuningDataLogger(size_t numSamples, const std::string
   // set the pattern to the message only
   headerLogger->set_pattern("%v");
   std::stringstream samplesHeader;
-  for (int i = 0; i < numSamples; ++i) {
+  for (size_t i = 0; i < numSamples; ++i) {
     samplesHeader << ",sample" << i;
   }
   // print csv header
