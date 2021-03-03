7973e19189535a847d082f446b5a38908b842110
FG-TUM <f.gratl@tum.de>
2021-03-03 21:22:51 +0100

    demo plot scripts

---
+++ examples/md-flexible/scripts/plotTuningData.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sys
+import os.path
+import pandas as pd
+import plotly.express as px
+
+if len(sys.argv) < 2:
+    sys.exit('Usage: ' + os.path.basename(sys.argv[0]) + ' IterationPerformance.csv')
+csvFile = sys.argv[1]
+if not os.path.isfile(csvFile) :
+    sys.exit('File not found: ' + csvFile)
+
+data = pd.read_csv(csvFile)
+
+fig = px.scatter(data,
+        x = 'Iteration',
+        y = 'Smoothed',
+        labels = {'Smoothed' : 'Smoothed iteratePairwise() [ns]'},
+        title = 'Smoothed Tuning Samples',
+        hover_name = 'Traversal',
+        hover_data = ['Data Layout', 'Newton 3', 'Load Estimator'],
+        color = 'Container',
+        )
+fig.update_traces(marker=dict(size=3))
+fig.show()
