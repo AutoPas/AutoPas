581c2c7cbba26f6bb7b4cb046065a8d89e05a550
Johannes Kroll <johannes.kroll@tum.de>
2021-03-24 17:48:26 +0100

    make sure calculateHomogeneity doesn't return inf

--- examples/md-flexible/src/Simulation.cpp
+++ examples/md-flexible/src/Simulation.cpp
@@ -439,7 +439,6 @@ void Simulation::writeVTKFile(autopas::AutoPas<ParticleType> &autopas) {
 }
 
 double Simulation::calculateHomogeneity(autopas::AutoPas<ParticleType> &autopas) const {
-
   size_t numberOfParticles = autopas.getNumberOfParticles();
   // approximately the resolution we want to get.
   size_t numberOfCells = ceil(numberOfParticles / 10.);
