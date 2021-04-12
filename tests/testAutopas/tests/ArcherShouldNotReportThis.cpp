df26169cb264d2fce416e6df46fb7e42444be3d2
Steffen Seckler <steffen.seckler@tum.de>
2021-04-12 16:58:42 +0200

    use atomic + crit

--- tests/testAutopas/tests/ArcherShouldNotReportThis.cpp
+++ tests/testAutopas/tests/ArcherShouldNotReportThis.cpp
@@ -8,22 +8,36 @@
 #include <gtest/gtest.h>
 #include <omp.h>
 
+#include <atomic>
 #include <numeric>
 
 class Container {
-  std::vector<int> vec{0, 0, 0};
-  bool generated = false;
+  enum class ValidityState : unsigned char {
+    invalid = 0,
+    valid = 1,
+  };
+
+  std::vector<int> vec{};
+  std::atomic<ValidityState> _isValid{ValidityState::invalid};
 
   auto generate() {
     vec = {1, 2, 3};
-    std::iota(vec.begin(), vec.end(), 1);
-    generated = true;
+    _isValid = ValidityState::valid;
   }
 
  public:
-  auto begin() {
+  auto begin_critical() {
+#pragma omp critical
+    if (_isValid == ValidityState::invalid) {
+      generate();
+    }
+
+    return vec.begin() + omp_get_thread_num();
+  }
+
+  auto begin_single() {
 #pragma omp single
-    if (not generated) {
+    if (_isValid == ValidityState::invalid) {
       generate();
     }
 
@@ -37,13 +51,30 @@ TEST(ArcherShouldNotReportThis, testGenerateUsingSingleInBegin) {
   Container c;
   int res{0};
 #pragma omp parallel reduction(+ : res)
-  for (auto iter = c.begin(); iter < c.end(); iter += omp_get_num_threads()) {
+  for (auto iter = c.begin_single(); iter < c.end(); iter += omp_get_num_threads()) {
     *iter = *iter * 2;
     res += *iter;
   }
 
   int reference_res{0};
-  for (auto iter = c.begin(); iter < c.end(); iter += 1) {
+  for (auto iter = c.begin_single(); iter < c.end(); iter += 1) {
+    reference_res += *iter;
+  }
+  EXPECT_GT(res, 0);
+  EXPECT_EQ(res, reference_res);
+}
+
+TEST(ArcherShouldNotReportThis, testGenerateUsingCriticalInBegin) {
+  Container c;
+  int res{0};
+#pragma omp parallel reduction(+ : res)
+  for (auto iter = c.begin_critical(); iter < c.end(); iter += omp_get_num_threads()) {
+    *iter = *iter * 2;
+    res += *iter;
+  }
+
+  int reference_res{0};
+  for (auto iter = c.begin_critical(); iter < c.end(); iter += 1) {
     reference_res += *iter;
   }
   EXPECT_GT(res, 0);
