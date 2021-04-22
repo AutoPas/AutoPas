7811030578765696f1adc1d6b6a6453fdc79e396
J0llyver <j0llyver.grain@gmail.com>
2021-04-22 17:30:06 +0200

    Included documentation for new classes

--- examples/md-flexible/src/simulations/MDFlexSimulation.h
+++ examples/md-flexible/src/simulations/MDFlexSimulation.h
@@ -8,17 +8,35 @@
 #include "../Simulation.h"
 
 /**
- * Provides base functionality for MD-Flexible simulations.
- * Maybe add unique identifier for each class or an MDFlex factory of some kind
+ * Handles minimal initialization requriements for MD-Flexible simulations.
+ * Derivce this class to create custom simulations.
  */
 class MDFlexSimulation {
  public:
-  MDFlexSimulation(int argc, char **argv);
+	/** 
+	 * Initializes the simulation according to the arguments passed to the main function.
+	 * @param argc The number of arguments passed in argv.
+	 * @param argv The arguments passed to the program.
+	 */
+  MDFlexSimulation(int argc, char** argv);
+
   ~MDFlexSimulation();
 
+	/**
+	 * Runs the simulation
+	 */
   virtual void run() = 0;
 
  protected:
+  /**
+	 * Stores the configuration used for the simulation.
+	 * The configuration is defined by the .yaml file passed to the application  with the '--yaml-file' argument.
+	 */
   MDFlexConfig *_configuration;
+
+	/**
+	 * The simulation, which will be started using the run function.
+	 * @todo: Create a simulation interface to allow users to provide their own simulation class
+	 */
   Simulation *_simulation;
 };
--- examples/md-flexible/src/simulations/MDFlexSingleNode.h
+++ examples/md-flexible/src/simulations/MDFlexSingleNode.h
@@ -7,14 +7,28 @@
 
 #include "MDFlexSimulation.h"
 
+/**
+ * Runs the MD-Flex simulation on a single node.
+ * This is the default demonstration of AutoPas.  
+ */
 class MDFlexSingleNode : MDFlexSimulation {
  public:
   MDFlexSingleNode(int argc, char **argv);
   ~MDFlexSingleNode();
 
+	/**
+	 * Runs the simulation
+	 */
   void run() override;
 
  private:
+ 	/**
+	 * Stores the argument count passed to the constructor for later reuse.
+	 */
   int _argc;
+
+	/**
+	 * Stores the arguments passed to the constructor for later reuse.
+	 */
   char **_argv;
 };
