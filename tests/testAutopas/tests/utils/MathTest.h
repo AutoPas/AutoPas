9ac6444c2aa9a81133a0b116f939fe1d209fead8
FG-TUM <f.gratl@tum.de>
2021-04-23 09:55:17 +0200

    add functions for + - * that do not overflow

---
+++ tests/testAutopas/tests/utils/MathTest.h
@@ -0,0 +1,14 @@
+/**
+ * @file MathTest.h
+ * @author F. Gratl
+ * @date 23.04.21
+ */
+
+#pragma once
+
+#include <AutoPasTestBase.h>
+
+template<class NumberType>
+class MathTest : public AutoPasTestBase {
+
+};
