358f6b68d979db62c49c1f66af0ab1b24513af0c
HobbyProgrammer <--replace-all>
2021-06-04 12:49:15 +0200

    Add file comments

--- examples/md-flexible/src/templateInstantiations/iteratePairwiseLJFunctorAVX.cpp
+++ examples/md-flexible/src/templateInstantiations/iteratePairwiseLJFunctorAVX.cpp
@@ -1,3 +1,10 @@
+/**
+ * @file iteratePairwiseLJFunctorAVX.cpp
+ *
+ * Contains a explicit template instantiation for the iteratePairwise() method with the LJFunctorAVX of the main
+ * AutoPas class and the particle type used by md-flexible. This is linked into the md-flexible executable to enable the
+ * other compilation units to only declare, but not instantiate this template.
+ */
 
 #include "autopas/AutoPasImpl.h"
 #include "autopas/molecularDynamics/LJFunctorAVX.h"
