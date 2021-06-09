41fcbdac57d661dc3a794cfd2467df3e21263ac2
J0llyver <j0llyver.grain@gmail.com>
2021-06-09 10:48:56 +0200

    Requirements for MPI debugging

--- examples/md-flexible/src/main.cpp
+++ examples/md-flexible/src/main.cpp
@@ -6,6 +6,11 @@
 
 #include "mpi.h"
 
+#include <chrono>
+#include <thread>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "simulations/MDFlexMPI.h"
 #include "simulations/MDFlexSingleRank.h"
 
@@ -16,6 +21,17 @@
  * @return
  */
 int main(int argc, char **argv) {
+  int gdbBlocker = 0;
+
+  pid_t pid = getpid();
+  std::cout << "ProcessId: " << pid << std::endl;
+
+  using namespace std::chrono_literals;
+
+  while (gdbBlocker == 0) {
+    std::this_thread::sleep_for(5000ms);
+  }
+
   MPI_Init(&argc, &argv);
   MDFlexMPI simulation(3, argc, argv);
   simulation.run();
