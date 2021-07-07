f5b3e615ddfb0d379403fec6be9422463a9100bb
J0llyver <j0llyver.grain@gmail.com>
2021-07-07 09:38:19 +0200

    Created getTerminalWidth function

--- examples/md-flexible/src/Simulation.cpp
+++ examples/md-flexible/src/Simulation.cpp
@@ -34,6 +34,41 @@ extern template bool autopas::AutoPas<ParticleType>::iteratePairwise(autopas::Fl
 #include "configuration/MDFlexConfig.h"
 #include "src/ParticleSerializationTools.h"
 
+namespace {
+/**
+ * Tries to identify the width of the terminal where the simulation is running.
+ * If no width can be identified, the function defaults to 80.
+ * @return width of the terminal.
+ */
+size_t getTerminalWidth(){
+  size_t terminalWidth = 0;
+  // test all std pipes to get the current terminal width
+  for (auto fd : {STDOUT_FILENO, STDIN_FILENO, STDERR_FILENO}) {
+    if (isatty(fd)) {
+      struct winsize w {};
+      ioctl(fd, TIOCGWINSZ, &w);
+      terminalWidth = w.ws_col;
+      break;
+    }
+  }
+
+  // if width is still zero try the environment variable COLUMNS
+  if (terminalWidth == 0) {
+    if (auto *teminalWidthCharArr = std::getenv("COLUMNS")) {
+      terminalWidth = atoi(teminalWidthCharArr);
+    }
+  }
+
+  // if all of the above fail fall back to a fixed width
+  if (terminalWidth == 0) {
+    // this seems to be the default width in most terminal windows
+    terminalWidth = 80;
+  }
+
+  return terminalWidth;
+}
+}
+
 // @todo: create configuration variable to set output folder for the vtk writer. Currently it is "output".
 Simulation::Simulation(const MDFlexConfig &configuration, RegularGridDecomposition &domainDecomposition)
     : _configuration(configuration),
@@ -184,29 +219,8 @@ void Simulation::printProgress(size_t iterationProgress, size_t maxIterations, b
   std::stringstream progressbar;
   progressbar << "[";
   // get current terminal width
-  size_t terminalWidth = 0;
-  // test all std pipes to get the current terminal width
-  for (auto fd : {STDOUT_FILENO, STDIN_FILENO, STDERR_FILENO}) {
-    if (isatty(fd)) {
-      struct winsize w {};
-      ioctl(fd, TIOCGWINSZ, &w);
-      terminalWidth = w.ws_col;
-      break;
-    }
-  }
+  size_t terminalWidth = getTerminalWidth();
 
-  // if width is still zero try the environment variable COLUMNS
-  if (terminalWidth == 0) {
-    if (auto *teminalWidthCharArr = std::getenv("COLUMNS")) {
-      terminalWidth = atoi(teminalWidthCharArr);
-    }
-  }
-
-  // if all of the above fail fall back to a fixed width
-  if (terminalWidth == 0) {
-    // this seems to be the default width in most terminal windows
-    terminalWidth = 80;
-  }
   // the bar should fill the terminal window so subtract everything else (-2 for "] ")
   size_t maxBarWidth = terminalWidth - info.str().size() - progressbar.str().size() - 2;
   // sanity check for underflow
