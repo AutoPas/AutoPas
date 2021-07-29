58bb5bb3ea87b0377b33af7d019533887489507f
J0llyver <j0llyver.grain@gmail.com>
2021-07-29 07:47:17 +0200

    Uncommented run of simulation

--- examples/md-flexible/src/main.cpp
+++ examples/md-flexible/src/main.cpp
@@ -37,7 +37,7 @@ int main(int argc, char **argv) {
   }
 
   Simulation simulation(configuration, domainDecomposition);
-  //  simulation.run();
+  simulation.run();
 
   if (domainDecomposition.getDomainIndex() == 0) {
     std::cout << std::endl << "Using " << autopas::autopas_get_max_threads() << " Threads" << std::endl;
