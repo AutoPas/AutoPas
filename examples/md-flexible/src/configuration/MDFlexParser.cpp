227d1a23cd2e2d865d5e913515e05ee5edad0c6a
J0llyver <j0llyver.grain@gmail.com>
2021-09-02 11:44:43 +0200

    Another attempt to fix tests

--- examples/md-flexible/src/configuration/MDFlexParser.cpp
+++ examples/md-flexible/src/configuration/MDFlexParser.cpp
@@ -7,6 +7,15 @@
 #include "MDFlexParser.h"
 
 MDFlexParser::exitCodes MDFlexParser::parseInput(int argc, char **argv, MDFlexConfig &config) {
+  // we need to copy argv because the call to getOpt in _cliParser.inputFilesPresent reorders it...
+  auto *argvCopy = new char *[argc + 1];
+  for (int i = 0; i < argc; i++) {
+    auto len = std::string(argv[i]).length() + 1;
+    argvCopy[i] = new char[len];
+    strcpy(argvCopy[i], argv[i]);
+  }
+  argvCopy[argc] = nullptr;
+
   CLIParser::inputFilesPresent(argc, argv, config);
 
   if (not config.yamlFilename.value.empty()) {
@@ -15,7 +24,12 @@ MDFlexParser::exitCodes MDFlexParser::parseInput(int argc, char **argv, MDFlexCo
     }
   }
 
-  auto cliParseExitCode = CLIParser::parseInput(argc, argv, config);
+  auto cliParseExitCode = CLIParser::parseInput(argc, argvCopy, config);
+
+  for (int i = 0; i < argc + 1; i++) {
+    delete[] argvCopy[i];
+  }
+  delete[] argvCopy;
 
   return cliParseExitCode;
 }
