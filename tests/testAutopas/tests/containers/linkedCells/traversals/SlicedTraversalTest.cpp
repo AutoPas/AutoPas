609358e9788ca9a4801b15a6591578f2a8cc716f
Jonas Schulz <js97.sw@gmail.com>
2021-09-06 16:59:19 +0200

    Changed the distance of the grid generated particles to 0.99 to avoid tolerable numeric errors leading to failing the test. Changed the MFunctor to a FlopCounterFunctor and the test to equality of the functor's kernel calls and 54. Details in the comments.

--- tests/testAutopas/tests/containers/linkedCells/traversals/SlicedTraversalTest.cpp
+++ tests/testAutopas/tests/containers/linkedCells/traversals/SlicedTraversalTest.cpp
@@ -10,29 +10,70 @@
 #include "autopasTools/generators/GridGenerator.h"
 #include "testingHelpers/NumThreadGuard.h"
 #include "testingHelpers/commonTypedefs.h"
+#include "autopas/pairwiseFunctors/FlopCounterFunctor.h"
 
 using ::testing::_;
 
 void testSlicedTraversal(const std::array<size_t, 3> &edgeLength) {
-  MFunctor functor;
+
+  // MFunctor functor;
+  autopas::FlopCounterFunctor<autopas::Particle> f(1);
   std::vector<FPCell> cells;
   cells.resize(edgeLength[0] * edgeLength[1] * edgeLength[2]);
 
-  autopasTools::generators::GridGenerator::fillWithParticles(cells, edgeLength, edgeLength);
+  autopasTools::generators::GridGenerator::fillWithParticles(cells, edgeLength, edgeLength,
+                                                             autopas::utils::ParticleTypeTrait<FPCell>::value(),
+                                                             {0.99, 0.99, 0.99}, {0.5, 0.5, 0.5}, {1., 1., 1.});
 
-  NumThreadGuard numThreadGuard(4);
+  //NumThreadGuard numThreadGuard(4);
 
-  autopas::LCSlicedTraversal<FPCell, MFunctor, autopas::DataLayoutOption::aos, true> slicedTraversal(
-      edgeLength, &functor, 1., {1., 1., 1.});
+  autopas::LCSlicedTraversal<FPCell, autopas::FlopCounterFunctor<autopas::Particle>, autopas::DataLayoutOption::aos, true> slicedTraversal(
+      edgeLength, &f, 1., {1., 1., 1.});
 
-  // every particle interacts with 13 others. Last layer of each dim is covered
-  // by previous interactions
   EXPECT_TRUE(slicedTraversal.isApplicable());
-  EXPECT_CALL(functor, AoSFunctor(_, _, true))
-      .Times((edgeLength[0] - 1) * (edgeLength[1] - 1) * (edgeLength[2] - 1) * 13);
   slicedTraversal.setCellsToTraverse(cells);
   slicedTraversal.initTraversal();
   slicedTraversal.traverseParticlePairs();
+
+
+//    EXPECT_CALL(f, FlopCounterFunctor::AoSFunctor(_, _, true))
+//        .Times((edgeLength[0] - 1) * (edgeLength[1] - 1) * (edgeLength[2] - 1) * 13);
+  // all interactions: (edgeLength[0] - 1) * (edgeLength[1] - 1) * (edgeLength[2] - 1) * 13;
+
+  /* center point interactions:
+   *
+   *           x         |         x         |        x         |                   |
+   *           |  x      |         |  x      |        |  x      |            x      |
+   *      x----o----x    |    x----o----x    |        o----x    |         o----x    |
+   *        x  |         |         |         |        |         |         |         |
+   *           x         |         x         |        x         |         x         |
+   *                     |                   |                  |                   |
+   *        center       |       face        |      edge        |      vertex       |
+   *
+   *  3x3x3 grid is a cube with 27 = 1 + 6 + 12 + 8 particles, referring to
+   *  1: one center point
+   *  6: six faces
+   *  12: twelve edges
+   *  8: eight vertices
+   *  Center point interacts with 6 particles (s. visualization above)
+   *  "Face points" interact with 5 particles each (s. vis/ above wo/ particle a)
+   *  "Edge points" interact with 4 particles each (s. vis/ above wo/ particles a and b)
+   *  "Vertex points" interact with 3 particles each (s. vis/ above wo/ a, b, c)
+   *  This leads to (1 * 6) + (6 * 5) + (12 * 4) + (8 * 3) = 108 interactions, since
+   *  particles are in interaction length iff their position difference is a unit vector (see vis/ above).
+   *  Newton3 is enabled, so there should be 108/2 = 54 kernel calls.
+   */
+  auto expectedKernelCalls = 54;
+  ASSERT_EQ(expectedKernelCalls, f.getKernelCalls());
+
+  /*
+   * @todo: include boundary cells in LCSlicedTraversal
+   * Currently, exactly those pair interactions are calculated where one of both particles is not at the "boundary" cells
+   * (i.e., (0.5, 1.49, 1.49) and (0.5, 2.48, 1.49) interact, but not (0.5, 2.48, 1.49) and (0.5, 2.48, 2.48).
+   * For a visualization of all interactions that currently are actually done, see https://www.geogebra.org/3d/cnuekxyk).
+   * (Remark: all actual interactions were drawn there, the visualization is 100% representing the interactions.
+   * The particles were drawn at the lower integer, i.e. 0.5 -> 0, 1.49 -> 1, 2.48 -> 2)
+   */
 }
 
 /**
