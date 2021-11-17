540bbfb794fd448ce812ca1eba28fa404d441b21
lgaertner <ludwig.gaertner@tum.de>
2021-11-17 13:04:11 +0100

    fully compiling without cuda support

--- tests/testAutopas/allTests.cpp
+++ tests/testAutopas/allTests.cpp
@@ -9,14 +9,16 @@
 #include <Kokkos_Core.hpp>
 
 int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  // set the gtest death test style to threadsafe
-  testing::FLAGS_gtest_death_test_style = "threadsafe";
-
-  // damit nur meine Tests durchgelaufen werden
-  Kokkos::initialize(argc, argv);
   int result;
-  { result = RUN_ALL_TESTS(); }
+  Kokkos::initialize(argc, argv);
+  {
+    testing::InitGoogleTest(&argc, argv);
+    // set the gtest death test style to threadsafe
+    testing::FLAGS_gtest_death_test_style = "threadsafe";
+
+    // damit nur meine Tests durchgelaufen werden
+    result = RUN_ALL_TESTS();
+  }
   Kokkos::finalize();
 
   return result;
