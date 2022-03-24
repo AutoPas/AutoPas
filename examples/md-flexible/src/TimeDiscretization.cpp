5186fbeca00913da4868614eba72b9e67715bd09
SamNewcome <snewc3@gmail.com>
2022-03-24 17:47:13 +0100

    Added const to variables in TimeDiscretization.cpp

--- examples/md-flexible/src/TimeDiscretization.cpp
+++ examples/md-flexible/src/TimeDiscretization.cpp
@@ -1,6 +1,6 @@
 /**
  * @file TimeDiscretization.cpp
- * @author N. Fottner
+ * @author N. Fottner & S. Newcome
  * @date 13/05/19
  */
 #include "TimeDiscretization.h"
@@ -25,14 +25,14 @@ void calculatePositions(autopas::AutoPas<ParticleClass> &autoPasContainer,
 #pragma omp parallel
 #endif
   for (auto iter = autoPasContainer.begin(autopas::IteratorBehavior::owned); iter.isValid(); ++iter) {
-    auto v = iter->getV();
-    auto m = particlePropertiesLibrary.getMass(iter->getTypeId());
-    auto f = iter->getF();
+    const auto v = iter->getV();
+    const auto m = particlePropertiesLibrary.getMass(iter->getTypeId());
+    const auto f = iter->getF();
     iter->setOldF(f);
-    iter->setF(globalForce);
-    v = mulScalar(v, deltaT);
-    f = mulScalar(f, (deltaT * deltaT / (2 * m)));
-    auto newR = add(v, f);
+    iter->setF(globalForce); // reset force to globalForce (not relevant to calculating positions, but needs to be done at this stage of the simulation)
+    const auto velocityUpdateComponent = mulScalar(v, deltaT);
+    const auto accelerationUpdateComponent = mulScalar(f, (deltaT * deltaT / (2 * m)));
+    const auto newR = add(velocityUpdateComponent, accelerationUpdateComponent);
     iter->addR(newR);
   }
 }
@@ -69,23 +69,23 @@ template<> void calculateQuaternions<MulticenteredMoleculeLJ>(autopas::AutoPas<M
 #pragma omp parallel
 #endif
   for (auto iter = autoPasContainer.begin(autopas::IteratorBehavior::owned); iter.isValid(); ++iter) {
-    auto q = iter->getQ();
-    auto angVelW = iter->getAngularVel(); // angular velocity in world frame
-    auto angVelM = rotatePositionBackwards(q,angVelW); // angular velocity in molecular frame  (equivalent to (17))
-    auto torqueW = iter->getT();
-    auto torqueM = rotatePositionBackwards(q,torqueW); // (18)
+    const auto q = iter->getQ();
+    const auto angVelW = iter->getAngularVel(); // angular velocity in world frame
+    const auto angVelM = rotatePositionBackwards(q,angVelW); // angular velocity in molecular frame  (equivalent to (17))
+    const auto torqueW = iter->getT();
+    const auto torqueM = rotatePositionBackwards(q,torqueW); // (18)
 
-    auto I = particlePropertiesLibrary.getMomentOfInteria(iter->getTypeId()); // moment of inertia
+    const auto I = particlePropertiesLibrary.getMomentOfInteria(iter->getTypeId()); // moment of inertia
 
-    auto angMomentumM = mul(I,angVelM); // equivalent to (19)
-    auto derivativeAngMomentumM = sub(torqueM, cross(angVelM,angMomentumM)); // (20)
-    auto angMomentumMHalfStep = add(angMomentumM, mulScalar(derivativeAngMomentumM, halfDeltaT)); // (21)
+    const auto angMomentumM = mul(I,angVelM); // equivalent to (19)
+    const auto derivativeAngMomentumM = sub(torqueM, cross(angVelM,angMomentumM)); // (20)
+    const auto angMomentumMHalfStep = add(angMomentumM, mulScalar(derivativeAngMomentumM, halfDeltaT)); // (21)
 
-    auto derivativeQHalfStep = mulScalar(qMul(q, div(angMomentumMHalfStep, I)), 0.5); // (22)
+    const auto derivativeQHalfStep = mulScalar(qMul(q, div(angMomentumMHalfStep, I)), 0.5); // (22)
 
     auto qHalfStep = normalize(add(q, mulScalar(derivativeQHalfStep,halfDeltaT))); // (23)
 
-    auto angVelWHalfStep = add(angVelW, mulScalar(div(torqueW, I), halfDeltaT)); // equivalent to (24)
+    const auto angVelWHalfStep = add(angVelW, mulScalar(div(torqueW, I), halfDeltaT)); // equivalent to (24)
 
     // (25) start
     // initialise qHalfStepOld to be outside tolerable distance from qHalfStep to satisfy while statement
@@ -100,7 +100,7 @@ template<> void calculateQuaternions<MulticenteredMoleculeLJ>(autopas::AutoPas<M
     }
     // (25) end
 
-    auto qFullStep = normalize(add(q, mulScalar(derivativeQHalfStep, deltaT)));
+    const auto qFullStep = normalize(add(q, mulScalar(derivativeQHalfStep, deltaT)));
 
     iter->setQ(qFullStep);
     iter->setAngularVel(angVelWHalfStep) // save angular velocity half step, to be used by calculateAngularVelocities
@@ -118,10 +118,10 @@ void calculateVelocities(autopas::AutoPas<ParticleClass> &autoPasContainer,
 #pragma omp parallel
 #endif
   for (auto iter = autoPasContainer.begin(autopas::IteratorBehavior::owned); iter.isValid(); ++iter) {
-    auto m = particlePropertiesLibrary.getMass(iter->getTypeId());
-    auto force = iter->getF();
-    auto oldForce = iter->getOldF();
-    auto newV = mulScalar((add(force, oldForce)), deltaT / (2 * m));
+    const auto m = particlePropertiesLibrary.getMass(iter->getTypeId());
+    const auto force = iter->getF();
+    const auto oldForce = iter->getOldF();
+    const auto newV = mulScalar((add(force, oldForce)), deltaT / (2 * m));
     iter->addV(newV);
   }
 }
@@ -141,8 +141,8 @@ template<> void calculateAngularVelocities<MulticenteredMoleculeLJ>(autopas::Aut
 #pragma omp parallel
 #endif
   for (auto iter = autoPasContainer.begin(autopas::IteratorBehavior::owned); iter.isValid(); ++iter) {
-    auto torque = iter->getTorque();
-    auto I = particlePropertiesLibrary.getMomentOfInteria(iter->getTypeId()); // moment of inertia
+    const auto torque = iter->getTorque();
+    const auto I = particlePropertiesLibrary.getMomentOfInteria(iter->getTypeId()); // moment of inertia
     iter->addAngularVel(mulScalar(div(torque, I), 0.5*deltaT)); // (28)
   }
 }
