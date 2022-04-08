6c1b815d482930ce3b802fa68127a1a681203f06
SamNewcome <snewc3@gmail.com>
2022-04-08 13:35:44 +0200

    TimeDiscretization.cpp moved into TimeDiscretization.h

--- examples/md-flexible/src/TimeDiscretization.cpp
+++ examples/md-flexible/src/TimeDiscretization.cpp
@@ -12,46 +12,46 @@
 /**
  * Functions for updating velocities and positions as simulation time progresses.
  */
-namespace TimeDiscretization {
-template <class ParticleClass>
-void calculatePositions(autopas::AutoPas<ParticleClass> &autoPasContainer,
-                        const ParticlePropertiesLibraryType &particlePropertiesLibrary, const double &deltaT,
-                        const std::array<double, 3> &globalForce) {
-  using autopas::utils::ArrayMath::add;
-  using autopas::utils::ArrayMath::mulScalar;
-
-#ifdef AUTOPAS_OPENMP
-#pragma omp parallel
-#endif
-  for (auto iter = autoPasContainer.begin(autopas::IteratorBehavior::owned); iter.isValid(); ++iter) {
-    auto v = iter->getV();
-    auto m = particlePropertiesLibrary.getMass(iter->getTypeId());
-    auto f = iter->getF();
-    iter->setOldF(f);
-    iter->setF(globalForce);
-    v = mulScalar(v, deltaT);
-    f = mulScalar(f, (deltaT * deltaT / (2 * m)));
-    auto newR = add(v, f);
-    iter->addR(newR);
-  }
-}
-
-template <class ParticleClass>
-void calculateVelocities(autopas::AutoPas<ParticleClass> &autoPasContainer,
-                         const ParticlePropertiesLibraryType &particlePropertiesLibrary, const double &deltaT) {
-  // helper declarations for operations with vector
-  using autopas::utils::ArrayMath::add;
-  using autopas::utils::ArrayMath::mulScalar;
-
-#ifdef AUTOPAS_OPENMP
-#pragma omp parallel
-#endif
-  for (auto iter = autoPasContainer.begin(autopas::IteratorBehavior::owned); iter.isValid(); ++iter) {
-    auto m = particlePropertiesLibrary.getMass(iter->getTypeId());
-    auto force = iter->getF();
-    auto oldForce = iter->getOldF();
-    auto newV = mulScalar((add(force, oldForce)), deltaT / (2 * m));
-    iter->addV(newV);
-  }
-}
-}  // namespace TimeDiscretization
+//namespace TimeDiscretization {
+//template <class ParticleClass>
+//void calculatePositions(autopas::AutoPas<ParticleClass> &autoPasContainer,
+//                        const ParticlePropertiesLibraryType &particlePropertiesLibrary, const double &deltaT,
+//                        const std::array<double, 3> &globalForce) {
+//  using autopas::utils::ArrayMath::add;
+//  using autopas::utils::ArrayMath::mulScalar;
+//
+//#ifdef AUTOPAS_OPENMP
+//#pragma omp parallel
+//#endif
+//  for (auto iter = autoPasContainer.begin(autopas::IteratorBehavior::owned); iter.isValid(); ++iter) {
+//    auto v = iter->getV();
+//    auto m = particlePropertiesLibrary.getMass(iter->getTypeId());
+//    auto f = iter->getF();
+//    iter->setOldF(f);
+//    iter->setF(globalForce);
+//    v = mulScalar(v, deltaT);
+//    f = mulScalar(f, (deltaT * deltaT / (2 * m)));
+//    auto newR = add(v, f);
+//    iter->addR(newR);
+//  }
+//}
+//
+//template <class ParticleClass>
+//void calculateVelocities(autopas::AutoPas<ParticleClass> &autoPasContainer,
+//                         const ParticlePropertiesLibraryType &particlePropertiesLibrary, const double &deltaT) {
+//  // helper declarations for operations with vector
+//  using autopas::utils::ArrayMath::add;
+//  using autopas::utils::ArrayMath::mulScalar;
+//
+//#ifdef AUTOPAS_OPENMP
+//#pragma omp parallel
+//#endif
+//  for (auto iter = autoPasContainer.begin(autopas::IteratorBehavior::owned); iter.isValid(); ++iter) {
+//    auto m = particlePropertiesLibrary.getMass(iter->getTypeId());
+//    auto force = iter->getF();
+//    auto oldForce = iter->getOldF();
+//    auto newV = mulScalar((add(force, oldForce)), deltaT / (2 * m));
+//    iter->addV(newV);
+//  }
+//}
+//}  // namespace TimeDiscretization
