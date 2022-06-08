5f38286f6719ce03154111a95517e3b082dac984
L. Laumeyer <leon.laumeyer@tum.de>
2022-06-08 03:06:12 +0200

    added two more tests

--- tests/testAutopas/tests/selectors/tuningStrategy/ReinforcementLearningTest.cpp
+++ tests/testAutopas/tests/selectors/tuningStrategy/ReinforcementLearningTest.cpp
@@ -86,16 +86,111 @@ TEST_F(ReinforcementLearningTest, testTuneDoubleRun) {
   EXPECT_EQ(optimalConfig, reinforcementLearning.getCurrentConfiguration());
 
   //  Second Run
+  reinforcementLearning.addEvidence(20000, 1);
+  reinforcementLearning.tune();
+
   optimalConfig = reinforcementLearning.getCurrentConfiguration();
-  reinforcementLearning.addEvidence(5, 0);
+  reinforcementLearning.addEvidence(1, 1);
+  reinforcementLearning.tune();
+
+  reinforcementLearning.addEvidence(20000, 1);
+
+  EXPECT_THAT(allConfigs, testing::UnorderedElementsAreArray(testedConfigs));
+  EXPECT_THAT(false, reinforcementLearning.tune());
+  EXPECT_EQ(optimalConfig, reinforcementLearning.getCurrentConfiguration());
+}
+
+TEST_F(ReinforcementLearningTest, testBools) {
+  autopas::ReinforcementLearning reinforcementLearning(
+      {autopas::ContainerOption::linkedCells}, {1.},
+      {autopas::TraversalOption::lc_c08, autopas::TraversalOption::lc_c01, autopas::TraversalOption::lc_sliced},
+      {autopas::LoadEstimatorOption::none}, {autopas::DataLayoutOption::soa}, {autopas::Newton3Option::disabled});
+
+  std::vector<autopas::Configuration> testedConfigs;
+  std::vector<autopas::Configuration> allConfigs{
+      autopas::Configuration(autopas::ContainerOption::linkedCells, 1., autopas::TraversalOption::lc_sliced,
+                             autopas::LoadEstimatorOption::none, autopas::DataLayoutOption::soa,
+                             autopas::Newton3Option::disabled),
+      autopas::Configuration(autopas::ContainerOption::linkedCells, 1., autopas::TraversalOption::lc_c01,
+                             autopas::LoadEstimatorOption::none, autopas::DataLayoutOption::soa,
+                             autopas::Newton3Option::disabled),
+      autopas::Configuration(autopas::ContainerOption::linkedCells, 1., autopas::TraversalOption::lc_c08,
+                             autopas::LoadEstimatorOption::none, autopas::DataLayoutOption::soa,
+                             autopas::Newton3Option::disabled)};
+
+  testedConfigs.emplace_back(reinforcementLearning.getCurrentConfiguration());
+
+  EXPECT_THAT(true, reinforcementLearning.get_firstRun());
+  reinforcementLearning.addEvidence(2, 0);
 
   reinforcementLearning.tune();
-  reinforcementLearning.addEvidence(100, 0);
+  testedConfigs.emplace_back(reinforcementLearning.getCurrentConfiguration());
+  autopas::Configuration optimalConfig = reinforcementLearning.getCurrentConfiguration();
+  reinforcementLearning.addEvidence(1, 0);
 
   reinforcementLearning.tune();
-  reinforcementLearning.addEvidence(4, 0);
+  testedConfigs.emplace_back(reinforcementLearning.getCurrentConfiguration());
+  reinforcementLearning.addEvidence(20000, 0);
 
   EXPECT_THAT(allConfigs, testing::UnorderedElementsAreArray(testedConfigs));
   reinforcementLearning.tune();
   EXPECT_EQ(optimalConfig, reinforcementLearning.getCurrentConfiguration());
+
+  //  Second Run
+  EXPECT_THAT(false, reinforcementLearning.get_firstRun());
+  EXPECT_THAT(true, reinforcementLearning.get_firstTune());
+  optimalConfig = reinforcementLearning.getCurrentConfiguration();
+  reinforcementLearning.addEvidence(5, 1);
+
+  reinforcementLearning.tune();
+  EXPECT_THAT(false, reinforcementLearning.get_firstTune());
+  reinforcementLearning.addEvidence(100, 1);
+
+  reinforcementLearning.tune();
+  reinforcementLearning.addEvidence(4, 1);
+
+  reinforcementLearning.tune();
+}
+
+TEST_F(ReinforcementLearningTest, testAddEvidence) {
+  autopas::ReinforcementLearning reinforcementLearning(
+      {autopas::ContainerOption::linkedCells}, {1.},
+      {autopas::TraversalOption::lc_c08, autopas::TraversalOption::lc_c01, autopas::TraversalOption::lc_sliced},
+      {autopas::LoadEstimatorOption::none}, {autopas::DataLayoutOption::soa}, {autopas::Newton3Option::disabled});
+
+  std::vector<autopas::Configuration> allConfigs{
+      autopas::Configuration(autopas::ContainerOption::linkedCells, 1., autopas::TraversalOption::lc_sliced,
+                             autopas::LoadEstimatorOption::none, autopas::DataLayoutOption::soa,
+                             autopas::Newton3Option::disabled),
+      autopas::Configuration(autopas::ContainerOption::linkedCells, 1., autopas::TraversalOption::lc_c01,
+                             autopas::LoadEstimatorOption::none, autopas::DataLayoutOption::soa,
+                             autopas::Newton3Option::disabled),
+      autopas::Configuration(autopas::ContainerOption::linkedCells, 1., autopas::TraversalOption::lc_c08,
+                             autopas::LoadEstimatorOption::none, autopas::DataLayoutOption::soa,
+                             autopas::Newton3Option::disabled)};
+
+  reinforcementLearning.addEvidence(10, 0);
+  EXPECT_THAT(-10, reinforcementLearning.getState(reinforcementLearning.getCurrentConfiguration()));
+  reinforcementLearning.tune();
+
+  autopas::Configuration optimalConfig = reinforcementLearning.getCurrentConfiguration();
+  reinforcementLearning.addEvidence(15, 0);
+  EXPECT_THAT(-15, reinforcementLearning.getState(reinforcementLearning.getCurrentConfiguration()));
+  reinforcementLearning.tune();
+
+  reinforcementLearning.addEvidence(20, 0);
+  EXPECT_THAT(-20, reinforcementLearning.getState(reinforcementLearning.getCurrentConfiguration()));
+  reinforcementLearning.tune();
+
+  //  Second Run
+  EXPECT_THAT(false, reinforcementLearning.get_firstRun());
+  reinforcementLearning.addEvidence(5, 0);
+  reinforcementLearning.tune();
+
+  reinforcementLearning.addEvidence(5, 0);
+  reinforcementLearning.tune();
+
+  reinforcementLearning.addEvidence(5, 0);
+  reinforcementLearning.tune();
+  EXPECT_THAT(-5.5, reinforcementLearning.getState(reinforcementLearning.getCurrentConfiguration()));
 }
