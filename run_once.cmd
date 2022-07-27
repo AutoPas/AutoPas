609769f7e54a34d0663fee6a7646eb053ed67089
L. Laumeyer <leon.laumeyer@tum.de>
2022-07-27 18:05:33 +0200

    run just once cmd

---
+++ run_once.cmd
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH -J Alpha_Gamma
+#SBATCH -o ./%x%j.%N.out
+#SBATCH -D .
+#SBATCH --clusters=serial
+#SBATCH --partition=serial_std
+
+a='0'
+g='0'
+export "ALPHA=${a}"
+export "GAMMA=${g}"
+strategy="ReinforcementLearning"
+simulation="fallingDrop"
+yaml_file="./build/examples/md-flexible/${simulation}ReinforcementLearning.yaml"
+txt_file="alpha_${a}_gamma_${g}.txt"
+./build/examples/md-flexible/md-flexible "--yaml-filename" ${yaml_file}
+unset "ALPHA"
+unset "GAMMA"
