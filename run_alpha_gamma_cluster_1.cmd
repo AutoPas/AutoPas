29c0166d94b0a358f4dd9dcaebdad1208531e1a4
L. Laumeyer <leon.laumeyer@tum.de>
2022-07-27 16:43:16 +0200

    modified for test run

--- run_alpha_gamma_cluster_1.cmd
+++ run_alpha_gamma_cluster_1.cmd
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #SBATCH -J Alpha_Gamma
-#SBATCH -o ./%x%j.%N.txt
+#SBATCH -o ./%x%j.%N.out
 #SBATCH -D .
 #SBATCH --clusters=serial
 #SBATCH --partition=serial_std
 
-value_array_gamma='0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1'
-value_array_alpha='0.65 0.7 0.75 0.8 0.85 0.9 0.95 1'
+value_array_gamma='0'
+value_array_alpha='0'
 
 for a in ${value_array_alpha}
 do
@@ -18,10 +18,11 @@ do
             export "ALPHA=${a}"
             export "GAMMA=${g}"
             strategy='ReinforcementLearning'
-            yaml-file='fallingDropReinforcementLearningNoProgress.yaml'
+            yaml-file='examples/md-flexible/input/fallingDropReinforcementLearningNoProgress.yaml'
             txt_file=alpha_${a}_gamma_${g}.txt
-            ./build/example/md-flexible/md-flexible "--yaml-filename" ${yaml_file} | tee ${txt_file}
+            ./build/examples/md-flexible/md-flexible "--yaml-filename" ${yaml_file} | tee ${txt_file}
             unset "ALPHA"
+            unset "GAMMA"
         )
     done
     )
