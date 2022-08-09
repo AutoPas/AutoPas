b71931515e40bcdf9cea35fb493e8292b913d8c2
L. Laumeyer <leon.laumeyer@tum.de>
2022-08-09 15:11:18 +0200

    changed cluster and num threads

--- run_alpha_gamma_cluster_2.cmd
+++ run_alpha_gamma_cluster_2.cmd
@@ -3,13 +3,13 @@
 #SBATCH -J Alpha_Gamma_2
 #SBATCH -o ./%x%j.%N.out
 #SBATCH -D .
-#SBATCH --clusters=serial
-#SBATCH --partition=serial_std
+#SBATCH --clusters=cm2_tiny
+#SBATCH --partition=cm2_tiny
 
 value_array_gamma='0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1'
 value_array_alpha='0.1 0.15'
 
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=28
 
 for a in ${value_array_alpha}
 do
