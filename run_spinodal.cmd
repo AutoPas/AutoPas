bd56c2d055414c7e196fb06df999ea5d77b4759b
L. Laumeyer <leon.laumeyer@tum.de>
2022-08-09 02:54:47 +0200

    batch job for spinodal with mpi

--- run_spinodal.cmd
+++ run_spinodal.cmd
@@ -3,7 +3,9 @@
 #SBATCH -J Spinodal_Checkpoint
 #SBATCH -o ./%x%j.%N.out
 #SBATCH -D .
-#SBATCH --clusters=serial
-#SBATCH --partition=serial_std
+#SBATCH --clusters=cm2_tiny
+#SBATCH --partition=cm2_tiny
 
-./build/examples/md-flexible/md-flexible --yaml-filename ./build/examples/md-flexible/SpinodalDecomposition_equilibration.yaml
+export OMP_NUM_THREADS=28
+
+./build_mp/examples/md-flexible/md-flexible --yaml-filename ./build_mp/examples/md-flexible/SpinodalDecomposition_equilibration.yaml
