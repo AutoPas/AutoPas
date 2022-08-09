1f84a56da911fc5c800658d2fd72373073a03486
L. Laumeyer <leon.laumeyer@tum.de>
2022-08-09 15:03:46 +0200

    reverted syntax change

--- start_cluster.sh
+++ start_cluster.sh
@@ -3,7 +3,8 @@ export STRATEGY=ReinforcementLearning
 
 mkdir -p results
 
-for i in 1 2 3 4 5 6 7 8 9 10 11; do
+for i in 1 2 3 4 5 6 7 8 9 10 11
+do
   (
     sbatch "run_alpha_gamma_cluster_${i}.cmd"
   )
