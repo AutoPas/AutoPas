f472b1192eadb8ceace4070b52d1d3a5ad820e2e
L. Laumeyer <leon.laumeyer@tum.de>
2022-08-09 22:00:54 +0200

    run once for mp

--- run_once.cmd
+++ run_once.cmd
@@ -4,20 +4,20 @@
 #SBATCH -o ./%x%j.%N.out
 #SBATCH -e ./%x%j.%N_error.out
 #SBATCH -D .
-#SBATCH --clusters=serial
-#SBATCH --partition=serial_std
+#SBATCH --clusters=cm2_tiny
+#SBATCH --partition=cm2_tiny
 #SBATCH --get-user-env
 #SBATCH --export=ALPHA,Gamma,OMP_NUM_THREADS
 
 a='0'
 g='0'
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=28
 export ALPHA=${a}
 export GAMMA=${g}
-strategy="ReinforcementLearning"
+strategy="FullSearch"
 simulation="fallingDrop"
-yaml_file="./build/examples/md-flexible/${simulation}ReinforcementLearningNoProgress.yaml"
+yaml_file="./build_mp/examples/md-flexible/${simulation}${strategy}NoProgress.yaml"
 txt_file="alpha_${a}_gamma_${g}.txt"
-./build/examples/md-flexible/md-flexible "--yaml-filename" ${yaml_file}
+./build_mp/examples/md-flexible/md-flexible "--yaml-filename" ${yaml_file}
 unset ALPHA
 unset GAMMA
