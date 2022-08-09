0c4a8281e625e2c40a655ea8314f96973ed9f07a
L. Laumeyer <leon.laumeyer@tum.de>
2022-08-09 03:52:24 +0200

    added get user env

--- run_spinodal.cmd
+++ run_spinodal.cmd
@@ -3,9 +3,11 @@
 #SBATCH -J Spinodal_Checkpoint
 #SBATCH -o ./%x%j.%N.out
 #SBATCH -D .
+#SBATCH --get-user-env
 #SBATCH --clusters=cm2_tiny
 #SBATCH --partition=cm2_tiny
 
 export OMP_NUM_THREADS=28
+
 
 ./build_mp/examples/md-flexible/md-flexible --yaml-filename ./build_mp/examples/md-flexible/SpinodalDecomposition_equilibration.yaml
