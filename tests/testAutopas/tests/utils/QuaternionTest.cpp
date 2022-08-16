66fcd483073cfd0d6fd52183217b84c234358530
Samuel Newcome <samuel.newcome@tum.de>
2022-08-16 09:45:43 +0200

    Beginning on QuaternionTest.cpp

---
+++ tests/testAutopas/tests/utils/QuaternionTest.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file QuaternionTest.cpp
+ * @author S. Newcome
+ * @date 16/08/2022
+ */
+
+#include <gtest/gtest.h>
+
+#include "autopas/utils/Quaternion.h"
+
+using namespace autopas;
+
+std::array<double, 4> returnQuaternion(std::array<double, 3> r, double theta) {
+  const auto cos_theta_div_2
+}
+
+/**
+ * Tests rotation by defining directionvector to be rotated about and angle of rotation.
+ * From this a quaternion is definined and used to rotate vector.
+ */
+TEST(QuaternionTest, testRotatePosition) {
+  st
+}
