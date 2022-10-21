46bd3c4a6e607cb61a61acd4d985c77e893fdb67
Christina <101648049+christinanb@users.noreply.github.com>
2022-10-21 10:31:13 +0200

    Update tests/testAutopas/tests/molecularDynamics/LJFunctorSVETest.h
    Co-authored-by: SamNewcome <97889602+SamNewcome@users.noreply.github.com>

--- tests/testAutopas/tests/molecularDynamics/LJFunctorSVETest.h
+++ tests/testAutopas/tests/molecularDynamics/LJFunctorSVETest.h
@@ -92,7 +92,7 @@ class LJFunctorSVETest : public AutoPasTestBase, public ::testing::WithParamInte
   constexpr static double _cutoff{6.};
   constexpr static double _skinPerTimestep{0.1};
   constexpr static unsigned int _rebuildFrequency{20.};
-  constexpr static double _interactionLengthSquare{(_cutoff + _skin) * (_cutoff + _skinPerTimestep)};
+  constexpr static double _interactionLengthSquare{(_cutoff + _skinPerTimestep * _rebuildFrequency) * (_cutoff + _skinPerTimestep * _rebuildFrequency)};
   constexpr static double _epsilon{1.};
   constexpr static double _sigma{1.};
   const std::array<double, 3> _lowCorner{0., 0., 0.};
