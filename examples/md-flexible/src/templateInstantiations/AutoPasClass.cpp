1f183659d1a505e208cd556dde775d08360c2470
Samuel Newcome <samuel.newcome@tum.de>
2022-11-28 17:39:52 +0100

    Cleaning up merge changes for main

--- examples/md-flexible/src/templateInstantiations/AutoPasClass.cpp
+++ examples/md-flexible/src/templateInstantiations/AutoPasClass.cpp
@@ -1,7 +1,7 @@
 /**
  * @file AutoPasClass.cpp
  *
- * Contains a explicit template instantiation for the main AutoPas class and the particle type used by md-flexible. This
+ * Contains a explicit template instantiation for the main AutoPas class and the particle types used by md-flexible. This
  * is linked into the md-flexible executable to enable the other compilation units to only declare, but not instantiate
  * this template.
  */
@@ -9,4 +9,5 @@
 #include "autopas/AutoPasImpl.h"
 #include "src/TypeDefinitions.h"
 
-template class autopas::AutoPas<ParticleType>;
+template class autopas::AutoPas<SingleSiteMolecule>;
+template class autopas::AutoPas<MultiSiteMolecule>;
