67b0a7a0da97232da05b0d1b7c5d4673bae1d72c
Samuel Newcome <samuel.newcome@tum.de>
2022-12-07 15:00:10 +0100

    Some very early work on switching TimeDiscretization to the CMake switching

--- examples/md-flexible/src/TimeDiscretization.h
+++ examples/md-flexible/src/TimeDiscretization.h
@@ -10,14 +10,14 @@
 #include "src/configuration/MDFlexConfig.h"
 #include "autopas/utils/Quaternion.h"
 
-#include "TimeDiscretizationImpl.h"
+#include "TimeDiscretization.cpp"
 
 /**
  * Functions for updating velocities and positions as simulation time progresses.
  */
 namespace TimeDiscretization {
 /**
- * Calculate and update the position for every particle using the Velocity-Verlet Algorithm.
+ * Calculate and update the position for every particle using the Störmer-Verlet Algorithm.
  * In addition, pushes the force stored in the force vector to the old force vector and sets the force vector to the
  * global force in preparation for the calculate forces stage.
  *
@@ -32,157 +32,26 @@ namespace TimeDiscretization {
  * @param fastParticlesThrow When true throws an exception if particles moved too far for verlet technique
  * (>skin/2/rebuildFrequency).
  */
-template <class ParticleClass>
-void calculatePositionsAndUpdateForces(autopas::AutoPas<ParticleClass> &autoPasContainer,
-                        const ParticlePropertiesLibraryType &particlePropertiesLibrary, const double &deltaT,
-                        const std::array<double, 3> &globalForce, bool fastParticlesThrow) {
-  using autopas::utils::ArrayUtils::operator<<;
-  using autopas::utils::ArrayMath::add;
-  using autopas::utils::ArrayMath::dot;
-  using autopas::utils::ArrayMath::mulScalar;
-
-  const auto maxAllowedDistanceMoved =
-      autoPasContainer.getVerletSkin() / (2 * autoPasContainer.getVerletRebuildFrequency());
-  const auto maxAllowedDistanceMovedSquared = maxAllowedDistanceMoved * maxAllowedDistanceMoved;
-
-  bool throwException = false;
-
-#ifdef AUTOPAS_OPENMP
-#pragma omp parallel reduction(|| : throwException)
-#endif
-  for (auto iter = autoPasContainer.begin(autopas::IteratorBehavior::owned); iter.isValid(); ++iter) {
-    const auto m = particlePropertiesLibrary.getMolMass(iter->getTypeId());
-    auto v = iter->getV();
-    auto f = iter->getF();
-    iter->setOldF(f);
-    iter->setF(globalForce);
-    v = mulScalar(v, deltaT);
-    f = mulScalar(f, (deltaT * deltaT / (2 * m)));
-    const auto displacement = add(v, f);
-    // sanity check that particles are not too fast for the Verlet skin technique.
-    // If this condition is violated once this is not necessarily an error. Only if the total distance traveled over
-    // the whole rebuild frequency is farther than the skin we lose interactions.
-    const auto distanceMovedSquared = dot(displacement, displacement);
-    if (distanceMovedSquared > maxAllowedDistanceMoved) {
-#pragma omp critical
-      std::cerr << "A particle moved farther than skin/2/rebuildFrequency: " << std::sqrt(distanceMovedSquared) << " > "
-                << autoPasContainer.getVerletSkin() << "/2/" << autoPasContainer.getVerletRebuildFrequency() << " = "
-                << maxAllowedDistanceMoved << "\n"
-                << *iter << "\nNew Position: " << add(iter->getR(), displacement) << std::endl;
-      if (fastParticlesThrow) {
-        throwException = true;
-      }
-    }
-    iter->addR(displacement);
-  }
-
-  if (throwException) {
-    throw std::runtime_error("At least one particle was too fast!");
-  }
-}
+void calculatePositionsAndUpdateForces(autopas::AutoPas<ParticleType> &autoPasContainer,
+                                       const ParticlePropertiesLibraryType &particlePropertiesLibrary, const double &deltaT,
+                                       const std::array<double, 3> &globalForce, bool fastParticlesThrow)
 
 /**
  * Calculate and update the quaternion for every particle. Uses the rotational velocity-verlet algorithm as described by
  * Rozmanov, 2010, Robust rotational-velocity-Verlet integration methods (method A); with slight adaptations to account
  * for md-flexible primarily using (angular) velocities rather than (angular) momentums. Code lines are commented with
- * references to corresponding equations within the paper. Throws error unless ParticleClass is specialised to a
-* rotational molecule, i.e. MultisiteMoleculeLJ.
- * @tparam ParticleClass
+ * references to corresponding equations within the paper.
+ *
+ * @note Throws error if md-flexible is compiled without multi-site support.
+ *
  * @param autoPasContainer
  * @param particlePropertiesLibrary
  * @param deltaT
  * @param globalForce
  */
-template <class ParticleClass>
-inline void calculateQuaternions(autopas::AutoPas<ParticleClass> &autoPasContainer,
+inline void calculateQuaternions(autopas::AutoPas<ParticleType> &autoPasContainer,
                           const ParticlePropertiesLibraryType &particlePropertiesLibrary, const double &deltaT,
-                          const std::array<double, 3> &globalForce) {
-  autopas::utils::ExceptionHandler::exception("calculateQuaternion should not be run with a non-rotational molecule type!");
-}
-
-template<> inline void calculateQuaternions<autopas::MultisiteMoleculeLJ>(autopas::AutoPas<autopas::MultisiteMoleculeLJ> &autoPasContainer,
-                                                               const ParticlePropertiesLibraryType &particlePropertiesLibrary, const double &deltaT,
-                                                               const std::array<double, 3> &globalForce) {
-  using autopas::utils::ArrayMath::add;
-  using autopas::utils::ArrayMath::addScalar;
-  using autopas::utils::ArrayMath::sub;
-  using autopas::utils::ArrayMath::mul;
-  using autopas::utils::ArrayMath::mulScalar;
-  using autopas::utils::ArrayMath::div;
-  using autopas::utils::ArrayMath::cross;
-  using autopas::utils::ArrayMath::L2Norm;
-  using autopas::utils::ArrayMath::normalize;
-  using autopas::utils::quaternion::qMul;
-  using autopas::utils::quaternion::rotatePosition;
-  using autopas::utils::quaternion::rotatePositionBackwards;
-
-  // const auto halfDeltaT = 0.5 * deltaT;
-  //
-  // const double tol = 1e-13; // tolerance given in paper
-  //
-  // // todo sort out how to handle global forces.
-  //
-  // std::cout << "here0";
-  // bool flag = true;
-  //
-  //#ifdef AUTOPAS_OPENMP
-  //#pragma omp parallel
-  //#endif
-  // for (auto iter = autoPasContainer.begin(autopas::IteratorBehavior::owned); iter.isValid(); ++iter) {
-  //   if (flag == true) {
-  //     std::cout << "here1";
-  //   }
-  //   const auto q = iter->getQ();
-  //   const auto angVelW = iter->getAngularVel(); // angular velocity in world frame
-  //   const auto angVelM = rotatePositionBackwards(q,angVelW); // angular velocity in molecular frame  (equivalent to (17))
-  //   const auto torqueW = iter->getTorque();
-  //   const auto torqueM = rotatePositionBackwards(q,torqueW); // (18)
-  //
-  //   const auto I = particlePropertiesLibrary.getMomentOfInertia(iter->getTypeId()); // moment of inertia
-  //
-  //   const auto angMomentumM = mul(I,angVelM); // equivalent to (19)
-  //   const auto derivativeAngMomentumM = sub(torqueM, cross(angVelM,angMomentumM)); // (20)
-  //   const auto angMomentumMHalfStep = add(angMomentumM, mulScalar(derivativeAngMomentumM, halfDeltaT)); // (21)
-  //
-  //   auto derivativeQHalfStep = mulScalar(qMul(q, div(angMomentumMHalfStep, I)), 0.5); // (22)
-  //
-  //   auto qHalfStep = normalize(add(q, mulScalar(derivativeQHalfStep,halfDeltaT))); // (23)
-  //
-  //   const auto angVelWHalfStep = add(angVelW, mulScalar(rotatePosition(q,div(torqueM, I)), halfDeltaT)); // equivalent to (24)
-  //
-  //   // (25) start
-  //   // initialise qHalfStepOld to be outside tolerable distance from qHalfStep to satisfy while statement
-  //   auto qHalfStepOld = qHalfStep;
-  //   qHalfStepOld[0] += 2 * tol;
-  //
-  //   if (flag == true) {
-  //     std::cout << "here3";
-  //   }
-  //
-  //   int i = 0;
-  //   while (L2Norm(sub(qHalfStep,qHalfStepOld))>tol) {
-  //     qHalfStepOld = qHalfStep;
-  //     auto angVelMHalfStep = rotatePositionBackwards(qHalfStepOld,angVelWHalfStep); // equivalent to first two lines of (25)
-  //     derivativeQHalfStep = mulScalar(qMul(qHalfStepOld,angVelMHalfStep),0.5);
-  //     qHalfStep = normalize(add(q, mulScalar(derivativeQHalfStep, halfDeltaT)));
-  //     if (i > 30) {
-  //       std::cout << i << ":";
-  //     }
-  //   }
-  //   // (25) end
-  //
-  //   const auto qFullStep = normalize(add(q, mulScalar(derivativeQHalfStep, deltaT))); // (26)
-  //
-  //   if (flag == true) {
-  //     std::cout << "here4";
-  //   }
-  //
-  //   iter->setQ(qFullStep);
-  //   iter->setAngularVel(angVelWHalfStep); // save angular velocity half step, to be used by calculateAngularVelocities
-  //
-  //   flag = false;
-  // }
-}
+                          const std::array<double, 3> &globalForce) {}
 
 /**
  * Calculate and update the velocity for every particle using the the Störmer-Verlet Algorithm.
