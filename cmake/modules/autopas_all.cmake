d288cc9fb770b1995815338257697501abc83aae
FG-TUM <f.gratl@tum.de>
2023-03-21 12:00:50 +0100

    fix wrong include of ALL code

--- cmake/modules/autopas_all.cmake
+++ cmake/modules/autopas_all.cmake
@@ -9,7 +9,7 @@ if (MD_FLEXIBLE_ENABLE_ALLLBL)
   include(FetchContent)
   FetchContent_Declare(
     allfetch
-    URL ${CMAKE_CURRENT_SOURCE_DIR}/ALL_20210930.zip
+    URL ${AUTOPAS_SOURCE_DIR}/libs/ALL_20210930.zip
     URL_HASH MD5=841b87748f82da1666bdc26f2038a3a1
   )
   
