bd05c7f5a8878cd78fcbecbdb88f56899dc81fb5
Daniel Asch <spiele.e.da@gmail.com>
2023-03-30 12:28:44 +0200

    code clean-up

--- examples/md-flexible/tests/domainDecomposition/MixedBoundaryConditionTest.cpp
+++ examples/md-flexible/tests/domainDecomposition/MixedBoundaryConditionTest.cpp
@@ -22,8 +22,6 @@ auto MixedBoundaryConditionTest::setUpExpectations(
   std::vector<std::array<double, 3>> expectedForces;
   expectedForces.resize(particlePositions.size());
 
-
-
   auto forceFromReflection = [&](const std::array<double, 3> position, const int dimensionOfBoundary,
                                  const bool isUpper) {
     const auto distanceToBoundary = isUpper ? boxMax[dimensionOfBoundary] - position[dimensionOfBoundary]
