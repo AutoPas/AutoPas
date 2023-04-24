84b82471c129a0ec2dcfc7063c51f61ab14e5f46
FG-TUM <f.gratl@tum.de>
2023-04-24 15:46:49 +0200

    add info for fcc

--- src/autopas/utils/CompileInfo.cpp
+++ src/autopas/utils/CompileInfo.cpp
@@ -8,24 +8,34 @@
 
 std::string utils::CompileInfo::getCompilerInfo() {
   std::stringstream ss;
-#if defined(__GNUC__) and not(defined(__clang__) or defined(__INTEL_COMPILER))
+  // prefix if not included in VERSION
+#if defined(__FUJITSU) or defined(__CLANG_FUJITSU)
+  ss << "Fujitsu ";
+#elif defined(__GNUC__) and not(defined(__clang__) or defined(__INTEL_COMPILER))
   ss << "GCC ";
 #endif
-  // defined for:
+
+  // Main version info. Defined for:
   //   - icpc (without version)
   //   - icpx
   //   - gcc (without name)
   //   - clang
+  //   - fcc (weird version number)
 #if defined(__VERSION__)
   ss << __VERSION__;
 #endif
+
+  // suffix for compilers where version is not clear yet
   // icpc does not specify the version in __VERSION__
 #if defined(__INTEL_COMPILER)
   ss << " (icpc version: " << __INTEL_COMPILER << "." << __INTEL_COMPILER_UPDATE << ")";
+#elif defined(__FCC_version__)
+  ss << " (FCC version: " << __FCC_version__ << ")";
 #endif
   // Base case if nothing could be found
   if (ss.str().empty()) {
     ss << "Unknown";
   }
+
   return ss.str();
 }
