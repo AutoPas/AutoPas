faa4c589d76a0f6b76640641c99885fcaf512242
Samuel Newcome <samuel.newcome@tum.de>
2023-06-15 13:09:18 +0200

    Quaternion function inputs use const

--- src/autopas/utils/Quaternion.h
+++ src/autopas/utils/Quaternion.h
@@ -22,8 +22,8 @@ namespace autopas::utils::quaternion {
  * @return rotated positions
  * @note we could instead pre-compute the rotation matrix -
  */
-std::vector<std::array<double, 3>> rotateVectorOfPositions(std::array<double, 4> &q,
-                                                           std::vector<std::array<double, 3>> &positionVector);
+std::vector<std::array<double, 3>> rotateVectorOfPositions(const std::array<double, 4> &q,
+                                                           const std::vector<std::array<double, 3>> &positionVector);
 
 /**
  * Rotates a single 3D position
@@ -31,7 +31,7 @@ std::vector<std::array<double, 3>> rotateVectorOfPositions(std::array<double, 4>
  * @param pos array of 3 doubles, defining position
  * @return rotated position
  */
-std::array<double, 3> rotatePosition(std::array<double, 4> &q, std::array<double, 3> &pos);
+std::array<double, 3> rotatePosition(const std::array<double, 4> &q, const std::array<double, 3> &pos);
 
 /**
  * Rotates a single 3D position backwards
@@ -39,7 +39,7 @@ std::array<double, 3> rotatePosition(std::array<double, 4> &q, std::array<double
  * @param pos array of 3 doubles, defining position
  * @return rotated position
  */
-std::array<double, 3> rotatePositionBackwards(std::array<double, 4> &q, std::array<double, 3> &pos);
+std::array<double, 3> rotatePositionBackwards(const std::array<double, 4> &q, const std::array<double, 3> &pos);
 
 /**
  * Quaternion multiplication. See Hamiltonian Product: https://en.wikipedia.org/wiki/Quaternion#Hamilton_product.
@@ -47,7 +47,7 @@ std::array<double, 3> rotatePositionBackwards(std::array<double, 4> &q, std::arr
  * @param q2 quaternion 2
  * @return q1 times q2
  */
-std::array<double, 4> qMul(std::array<double, 4> &q1, std::array<double, 4> &q2);
+std::array<double, 4> qMul(const std::array<double, 4> &q1, const std::array<double, 4> &q2);
 
 /**
  * Quaternion multiplication (converting v into a quaternion (0,v))
@@ -55,7 +55,7 @@ std::array<double, 4> qMul(std::array<double, 4> &q1, std::array<double, 4> &q2)
  * @param v 3D-vector
  * @return q times (0,v)
  */
-std::array<double, 4> qMul(std::array<double, 4> &q, std::array<double, 3> &v);
+std::array<double, 4> qMul(const std::array<double, 4> &q, const std::array<double, 3> &v);
 
 /**
  * Quaternion multiplication (converting v into a quaternion (0,v))
@@ -63,21 +63,21 @@ std::array<double, 4> qMul(std::array<double, 4> &q, std::array<double, 3> &v);
  * @param q quaternion
  * @return (0,v) times q
  */
-std::array<double, 4> qMul(std::array<double, 3> &v, std::array<double, 4> &q);
+std::array<double, 4> qMul(const std::array<double, 3> &v, const std::array<double, 4> &q);
 
 /**
  * Quaternion conjugation.
  * @param q quaternion
  * @return conjugated quaternion
  */
-std::array<double, 4> qConjugate(std::array<double, 4> &q);
+std::array<double, 4> qConjugate(const std::array<double, 4> &q);
 
 /**
  * Convert quaternion to 3d-vec
  * @param q quaternion
  * @return Quaternion without scalar part (i.e. 0th element)
  */
-std::array<double, 3> convertQuaternionTo3DVec(std::array<double, 4> &q);
+std::array<double, 3> convertQuaternionTo3DVec(const std::array<double, 4> &q);
 
 /**
  * Calculate the quaternion representing the same rotation, but mirrored.
@@ -85,5 +85,5 @@ std::array<double, 3> convertQuaternionTo3DVec(std::array<double, 4> &q);
  * @param dimensionNormalToMirror dimension normal to mirror plane
  * @return mirrored quaternion.
  */
-std::array<double, 4> qMirror(std::array<double, 4> &q, int &dimensionNormalToMirror);
+std::array<double, 4> qMirror(const std::array<double, 4> &q, const int &dimensionNormalToMirror);
 }  // namespace autopas::utils::quaternion
