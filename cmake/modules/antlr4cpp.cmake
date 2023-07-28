7d55cb94bff2c56f08051f094420d11725dd4bbd
FG-TUM <f.gratl@tum.de>
2023-07-28 19:54:24 +0200

    Fix building antlr
      - The install location should always be in the AutoPas directory
        especially when AutoPas is just a subproject
      - Don't build the dynamic library as it is not used

--- cmake/modules/antlr4cpp.cmake
+++ cmake/modules/antlr4cpp.cmake
@@ -2,13 +2,23 @@ message(STATUS "antlr4cpp - using bundled version")
 
 include(ExternalProject)
 
+# install prefix for antlr
+set(prefix ${CMAKE_CURRENT_BINARY_DIR}/antlr4cppPrefix)
+# location where antlr will build its static library
+set(staticLibInstallLocation ${prefix}/src/antlr4cpp_bundled/dist/libantlr4-runtime.a)
+
 ExternalProject_ADD(
         antlr4cpp_bundled
-        PREFIX      antlr4cppPrefix
+        PREFIX      ${prefix}
         URL         ${PROJECT_SOURCE_DIR}/libs/antlr4-cpp-runtime-4.9.3-source.zip
         URL_HASH    MD5=eafa4fef583e12e963062882773461be
-        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/antlr4cppPrefix/install/lib/libantlr4-runtime.a
-        CMAKE_ARGS  -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/antlr4cppPrefix/install -DCMAKE_CXX_FLAGS=-w
+        BUILD_BYPRODUCTS ${staticLibInstallLocation}
+        CMAKE_ARGS  -DCMAKE_INSTALL_PREFIX=${prefix}/install -DCMAKE_CXX_FLAGS=-w
+        # only build the static library. Unfortunately there is no CMake switch in Antlr for this
+        BUILD_COMMAND cmake --build . --parallel ${CMAKE_BUILD_PARALLEL_LEVEL} --target antlr4_static
+        # A full install would trigger the building of the shared lib so only install the headers
+        # We leave the static lib where it is and just mark the location in the target
+        INSTALL_COMMAND cmake --install . --component dev
 )
 
 # create dummy target that contains all information to easily link against
@@ -23,7 +33,7 @@ add_dependencies(antlr4cpp antlr4cpp_bundled)
 ExternalProject_Get_Property(antlr4cpp_bundled install_dir)
 set_target_properties(
         antlr4cpp
-        PROPERTIES "IMPORTED_LOCATION" "${install_dir}/install/lib/libantlr4-runtime.a"
+        PROPERTIES "IMPORTED_LOCATION" "${staticLibInstallLocation}"
 )
 
 # create directory otherwise cmake will complain during generate step since this is only generated during make
