cc19dc64549d1ba9226826da62c1bee05a0ee6a9
FG-TUM <f.gratl@tum.de>
2023-07-28 22:14:27 +0200

    Instead of messing with build and install commands patch away the shared target via sed

--- cmake/modules/antlr4cpp.cmake
+++ cmake/modules/antlr4cpp.cmake
@@ -4,8 +4,8 @@ include(ExternalProject)
 
 # install prefix for antlr
 set(prefix ${CMAKE_CURRENT_BINARY_DIR}/antlr4cppPrefix)
-# location where antlr will build its static library
-set(staticLibInstallLocation ${prefix}/src/antlr4cpp_bundled/dist/libantlr4-runtime.a)
+# location where antlr will install its static library
+set(staticLibInstallLocation ${prefix}/install/lib/libantlr4-runtime.a)
 
 ExternalProject_ADD(
         antlr4cpp_bundled
@@ -14,11 +14,10 @@ ExternalProject_ADD(
         URL_HASH    MD5=eafa4fef583e12e963062882773461be
         BUILD_BYPRODUCTS ${staticLibInstallLocation}
         CMAKE_ARGS  -DCMAKE_INSTALL_PREFIX=${prefix}/install -DCMAKE_CXX_FLAGS=-w
-        # only build the static library. Unfortunately there is no CMake switch in Antlr for this
-        BUILD_COMMAND cmake --build . --parallel ${CMAKE_BUILD_PARALLEL_LEVEL} --target antlr4_static
-        # A full install would trigger the building of the shared lib so only install the headers
-        # We leave the static lib where it is and just mark the location in the target
-        INSTALL_COMMAND cmake --install . --component dev
+        # Patch away the shared library target
+        PATCH_COMMAND sed --in-place -e /install.*shared/,+2d runtime/CMakeLists.txt                &&
+                      sed --in-place -e /set_target_properties.*shared/,+9d runtime/CMakeLists.txt  &&
+                      sed --in-place -e /shared/d runtime/CMakeLists.txt
 )
 
 # create dummy target that contains all information to easily link against
