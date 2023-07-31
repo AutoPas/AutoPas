8dbac7036ab7bcb4f1c9729d04e14d7c8553cccb
RobinQ <robin.quedzuweit@tum.de>
2023-07-31 18:50:07 +0200

    To prevent a NaN error, implemented a section of xsimd wrapper to be hardcoded for AVX and 512bit SVE and AVX512

--- src/autopas/molecularDynamics/LJFunctorXSIMD.h
+++ src/autopas/molecularDynamics/LJFunctorXSIMD.h
@@ -233,6 +233,7 @@ class LJFunctorXSIMD
            SoAKernel<true, false>(j, ownedStateI, reinterpret_cast<const int64_t *>(ownedStatePtr), x1, y1, z1, xptr,
                                   yptr, zptr, fxptr, fyptr, fzptr, &typeIDptr[i], typeIDptr, fxacc, fyacc, fzacc,
                                   &virialSumX, &virialSumY, &virialSumZ, &upotSum, 0);
+
          }
          // If b is a power of 2 the following holds:
          // a & (b -1) == a mod b
@@ -403,11 +404,69 @@ class LJFunctorXSIMD
                            double *const __restrict fz2ptr, const size_t *const typeID1ptr, const size_t *const typeID2ptr,
                            xsimd::batch<double> &fxacc, xsimd::batch<double> &fyacc, xsimd::batch<double> &fzacc, xsimd::batch<double> *virialSumX, xsimd::batch<double> *virialSumY,
                            xsimd::batch<double> *virialSumZ, xsimd::batch<double> *upotSum, const unsigned int rest = 0) {
-       xsimd::batch<double> epsilon24s;
-       xsimd::batch<double> sigmaSquares;
-       xsimd::batch<double> shift6s;
-         if(useMixing) {
-             const auto mixingDataPtr = _PPLibrary->getMixingDataPtr(*typeID1ptr, 0);
+       xsimd::batch<double> epsilon24s = _epsilon24;
+       xsimd::batch<double> sigmaSquares = _sigmaSquare;
+       xsimd::batch<double> shift6s = _shift6;
+
+       if(useMixing) {
+            #if defined(__AVX__)
+           epsilon24s = {
+                   _PPLibrary->mixing24Epsilon(*typeID1ptr, *(typeID2ptr + 0)),
+                   not remainderIsMasked or rest > 1 ? _PPLibrary->mixing24Epsilon(*typeID1ptr, *(typeID2ptr + 1)) : 0,
+                   not remainderIsMasked or rest > 2 ? _PPLibrary->mixing24Epsilon(*typeID1ptr, *(typeID2ptr + 2)) : 0,
+                   not remainderIsMasked or rest > 3 ? _PPLibrary->mixing24Epsilon(*typeID1ptr, *(typeID2ptr + 3)) : 0};
+           sigmaSquares = {
+                   _PPLibrary->mixingSigmaSquare(*typeID1ptr, *(typeID2ptr + 0)),
+                   not remainderIsMasked or rest > 1 ? _PPLibrary->mixingSigmaSquare(*typeID1ptr, *(typeID2ptr + 1)) : 0,
+                   not remainderIsMasked or rest > 2 ? _PPLibrary->mixingSigmaSquare(*typeID1ptr, *(typeID2ptr + 2)) : 0,
+                   not remainderIsMasked or rest > 3 ? _PPLibrary->mixingSigmaSquare(*typeID1ptr, *(typeID2ptr + 3)) : 0};
+           if constexpr (applyShift) {
+               shift6s = {
+                       _PPLibrary->mixingShift6(*typeID1ptr, *(typeID2ptr + 0)),
+                       (not remainderIsMasked or rest > 1) ? _PPLibrary->mixingShift6(*typeID1ptr, *(typeID2ptr + 1)) : 0,
+                       (not remainderIsMasked or rest > 2) ? _PPLibrary->mixingShift6(*typeID1ptr, *(typeID2ptr + 2)) : 0,
+                       (not remainderIsMasked or rest > 3) ? _PPLibrary->mixingShift6(*typeID1ptr, *(typeID2ptr + 3)) : 0};
+           }
+            // compile with 512bit registers available
+            #elif defined(__ARM_FEATURE_SVE) || defined(XSIMD_WITH_AVX512BW)
+           epsilon24s = {
+                   _PPLibrary->mixing24Epsilon(*typeID1ptr, *(typeID2ptr + 0)),
+                   not remainderIsMasked or rest > 1 ? _PPLibrary->mixing24Epsilon(*typeID1ptr, *(typeID2ptr + 1)) : 0,
+                   not remainderIsMasked or rest > 2 ? _PPLibrary->mixing24Epsilon(*typeID1ptr, *(typeID2ptr + 2)) : 0,
+                   not remainderIsMasked or rest > 3 ? _PPLibrary->mixing24Epsilon(*typeID1ptr, *(typeID2ptr + 3)) : 0,
+                   not remainderIsMasked or rest > 4 ? _PPLibrary->mixing24Epsilon(*typeID1ptr, *(typeID2ptr + 4)) : 0,
+                   not remainderIsMasked or rest > 5 ? _PPLibrary->mixing24Epsilon(*typeID1ptr, *(typeID2ptr + 5)) : 0,
+                   not remainderIsMasked or rest > 6 ? _PPLibrary->mixing24Epsilon(*typeID1ptr, *(typeID2ptr + 6)) : 0,
+                   not remainderIsMasked or rest > 7 ? _PPLibrary->mixing24Epsilon(*typeID1ptr, *(typeID2ptr + 7)) : 0};
+           sigmaSquares = {
+                   _PPLibrary->mixingSigmaSquare(*typeID1ptr, *(typeID2ptr + 0)),
+                   not remainderIsMasked or rest > 1 ? _PPLibrary->mixingSigmaSquare(*typeID1ptr, *(typeID2ptr + 1)) : 0,
+                   not remainderIsMasked or rest > 2 ? _PPLibrary->mixingSigmaSquare(*typeID1ptr, *(typeID2ptr + 2)) : 0,
+                   not remainderIsMasked or rest > 3 ? _PPLibrary->mixingSigmaSquare(*typeID1ptr, *(typeID2ptr + 3)) : 0,
+                   not remainderIsMasked or rest > 4 ? _PPLibrary->mixingSigmaSquare(*typeID1ptr, *(typeID2ptr + 4)) : 0,
+                   not remainderIsMasked or rest > 5 ? _PPLibrary->mixingSigmaSquare(*typeID1ptr, *(typeID2ptr + 5)) : 0,
+                   not remainderIsMasked or rest > 6 ? _PPLibrary->mixingSigmaSquare(*typeID1ptr, *(typeID2ptr + 6)) : 0,
+                   not remainderIsMasked or rest > 7 ? _PPLibrary->mixingSigmaSquare(*typeID1ptr, *(typeID2ptr + 7)) : 0};
+           if constexpr (applyShift) {
+               shift6s = {
+                       _PPLibrary->mixingShift6(*typeID1ptr, *(typeID2ptr + 0)),
+                       (not remainderIsMasked or rest > 1) ? _PPLibrary->mixingShift6(*typeID1ptr, *(typeID2ptr + 1)) : 0,
+                       (not remainderIsMasked or rest > 2) ? _PPLibrary->mixingShift6(*typeID1ptr, *(typeID2ptr + 2)) : 0,
+                       (not remainderIsMasked or rest > 3) ? _PPLibrary->mixingShift6(*typeID1ptr, *(typeID2ptr + 3)) : 0,
+                       (not remainderIsMasked or rest > 4) ? _PPLibrary->mixingShift6(*typeID1ptr, *(typeID2ptr + 4)) : 0,
+                       (not remainderIsMasked or rest > 5) ? _PPLibrary->mixingShift6(*typeID1ptr, *(typeID2ptr + 5)) : 0,
+                       (not remainderIsMasked or rest > 6) ? _PPLibrary->mixingShift6(*typeID1ptr, *(typeID2ptr + 6)) : 0,
+                       (not remainderIsMasked or rest > 7) ? _PPLibrary->mixingShift6(*typeID1ptr, *(typeID2ptr + 7)) : 0};
+           }
+
+            #endif
+
+       }
+
+
+         /* likely to cause a NaN-Error, so fixed vector size is implemented
+          if(useMixing) {
+             const auto mixingDataPtr = _PPLibrary->getMixingDataPtr(*typeID1ptr, *typeID2ptr);
              double epsilon24s_a[vecLength] = {0};
              double sigmaSquares_a[vecLength] = {0};
              double shift6s_a[vecLength] = {0};
@@ -421,34 +480,63 @@ class LJFunctorXSIMD
              sigmaSquares = xsimd::load_unaligned(sigmaSquares_a);
              shift6s = xsimd::load_unaligned(shift6s_a);
 
-         } else {
-             epsilon24s = _epsilon24;
-             sigmaSquares = _sigmaSquare;
-             shift6s = _shift6;
-         }
+         }*/
        xsimd::batch<double> x2;
        xsimd::batch<double> y2;
        xsimd::batch<double> z2;
        // load only masked values
-       if(remainderIsMasked) {
-           double x2_a[vecLength] = {0};
-           double y2_a[vecLength] = {0};
-           double z2_a[vecLength] = {0};
+         if(remainderIsMasked) {
+             double x2_a[vecLength] = {0};
+             double y2_a[vecLength] = {0};
+             double z2_a[vecLength] = {0};
 
-           for (int i = 0; _masks[rest-1].get(i); ++i) {
-               x2_a[i] = x2ptr[j + i];
-               y2_a[i] = y2ptr[j + i];
-               z2_a[i] = z2ptr[j + i];
-           }
-           x2 = xsimd::load_unaligned(x2_a);
-           y2 = xsimd::load_unaligned(y2_a);
-           z2 = xsimd::load_unaligned(z2_a);
+             for (int i = 0; _masks[rest-1].get(i); ++i) {
+                 x2_a[i] = x2ptr[j + i];
+                 y2_a[i] = y2ptr[j + i];
+                 z2_a[i] = z2ptr[j + i];
+             }
+             x2 = xsimd::load_unaligned(x2_a);
+             y2 = xsimd::load_unaligned(y2_a);
+             z2 = xsimd::load_unaligned(z2_a);
+            /* hard-coded section in case of NaN-error
+             #elif defined(__ARM_FEATURE_SVE)
+             x2 = {
+                     _masks[rest-1].get(0) ? x2ptr[j] : 0,
+                     _masks[rest-1].get(1) ? x2ptr[j + 1] : 0,
+                     _masks[rest-1].get(2) ? x2ptr[j + 2] : 0,
+                     _masks[rest-1].get(3) ? x2ptr[j + 3] : 0,
+                     _masks[rest-1].get(4) ? x2ptr[j + 4] : 0,
+                     _masks[rest-1].get(5) ? x2ptr[j + 5] : 0,
+                     _masks[rest-1].get(6) ? x2ptr[j + 6] : 0,
+                     _masks[rest-1].get(7) ? x2ptr[j + 7] : 0,
+             };
+             y2 = {
+                     _masks[rest-1].get(0) ? y2ptr[j] : 0,
+                     _masks[rest-1].get(1) ? y2ptr[j + 1] : 0,
+                     _masks[rest-1].get(2) ? y2ptr[j + 2] : 0,
+                     _masks[rest-1].get(3) ? y2ptr[j + 3] : 0,
+                     _masks[rest-1].get(4) ? y2ptr[j + 4] : 0,
+                     _masks[rest-1].get(5) ? y2ptr[j + 5] : 0,
+                     _masks[rest-1].get(6) ? y2ptr[j + 6] : 0,
+                     _masks[rest-1].get(7) ? y2ptr[j + 7] : 0,
+             };
+             z2 = {
+                     _masks[rest-1].get(0) ? z2ptr[j] : 0,
+                     _masks[rest-1].get(1) ? z2ptr[j + 1] : 0,
+                     _masks[rest-1].get(2) ? z2ptr[j + 2] : 0,
+                     _masks[rest-1].get(3) ? z2ptr[j + 3] : 0,
+                     _masks[rest-1].get(4) ? z2ptr[j + 4] : 0,
+                     _masks[rest-1].get(5) ? z2ptr[j + 5] : 0,
+                     _masks[rest-1].get(6) ? z2ptr[j + 6] : 0,
+                     _masks[rest-1].get(7) ? z2ptr[j + 7] : 0,
+             };
 
-       } else {
-           x2 = xsimd::load_unaligned(&x2ptr[j]);
-           y2 = xsimd::load_unaligned(&y2ptr[j]);
-           z2 = xsimd::load_unaligned(&z2ptr[j]);
-       }
+            #endif*/
+         } else {
+             x2 = xsimd::load_unaligned(&x2ptr[j]);
+             y2 = xsimd::load_unaligned(&y2ptr[j]);
+             z2 = xsimd::load_unaligned(&z2ptr[j]);
+         }
        const xsimd::batch<double> drx = xsimd::sub(x1,x2);
        const xsimd::batch<double> dry = xsimd::sub(y1,y2);
        const xsimd::batch<double> drz = xsimd::sub(z1,z2);
@@ -503,6 +591,9 @@ class LJFunctorXSIMD
        fyacc = xsimd::add(fyacc, fy);
        fzacc = xsimd::add(fzacc, fz);
 
+
+
+
        // if newton 3 is used subtract fD from particle j
         if(newton3) {
             xsimd::batch<double> fx2;
@@ -522,7 +613,58 @@ class LJFunctorXSIMD
                 fx2 = xsimd::load_unaligned(fx2_a);
                 fy2 = xsimd::load_unaligned(fy2_a);
                 fz2 = xsimd::load_unaligned(fz2_a);
-
+/* hard-coded section in case of NaN-error:
+ #if defined(__AVX__)
+                fx2 = {
+                        _masks[rest-1].get(0) ? fx2ptr[j] : 0,
+                        _masks[rest-1].get(1) ? fx2ptr[j + 1] : 0,
+                        _masks[rest-1].get(2) ? fx2ptr[j + 2] : 0,
+                        _masks[rest-1].get(3) ? fx2ptr[j + 3] : 0,
+                };
+                fy2 = {
+                        _masks[rest-1].get(0) ? fy2ptr[j] : 0,
+                        _masks[rest-1].get(1) ? fy2ptr[j + 1] : 0,
+                        _masks[rest-1].get(2) ? fy2ptr[j + 2] : 0,
+                        _masks[rest-1].get(3) ? fy2ptr[j + 3] : 0,
+                };
+                fz2 = {
+                        _masks[rest-1].get(0) ? fz2ptr[j] : 0,
+                        _masks[rest-1].get(1) ? fz2ptr[j + 1] : 0,
+                        _masks[rest-1].get(2) ? fz2ptr[j + 2] : 0,
+                        _masks[rest-1].get(3) ? fz2ptr[j + 3] : 0,
+                };
+#elif defined(__ARM_FEATURE_SVE)
+                fx2 = {
+                     _masks[rest-1].get(0) ? fx2ptr[j] : 0,
+                     _masks[rest-1].get(1) ? fx2ptr[j + 1] : 0,
+                     _masks[rest-1].get(2) ? fx2ptr[j + 2] : 0,
+                     _masks[rest-1].get(3) ? fx2ptr[j + 3] : 0,
+                     _masks[rest-1].get(4) ? fx2ptr[j + 4] : 0,
+                     _masks[rest-1].get(5) ? fx2ptr[j + 5] : 0,
+                     _masks[rest-1].get(6) ? fx2ptr[j + 6] : 0,
+                     _masks[rest-1].get(7) ? fx2ptr[j + 7] : 0,
+             };
+             fy2 = {
+                     _masks[rest-1].get(0) ? fy2ptr[j] : 0,
+                     _masks[rest-1].get(1) ? fy2ptr[j + 1] : 0,
+                     _masks[rest-1].get(2) ? fy2ptr[j + 2] : 0,
+                     _masks[rest-1].get(3) ? fy2ptr[j + 3] : 0,
+                     _masks[rest-1].get(4) ? fy2ptr[j + 4] : 0,
+                     _masks[rest-1].get(5) ? fy2ptr[j + 5] : 0,
+                     _masks[rest-1].get(6) ? fy2ptr[j + 6] : 0,
+                     _masks[rest-1].get(7) ? fy2ptr[j + 7] : 0,
+             };
+             fz2 = {
+                     _masks[rest-1].get(0) ? fz2ptr[j] : 0,
+                     _masks[rest-1].get(1) ? fz2ptr[j + 1] : 0,
+                     _masks[rest-1].get(2) ? fz2ptr[j + 2] : 0,
+                     _masks[rest-1].get(3) ? fz2ptr[j + 3] : 0,
+                     _masks[rest-1].get(4) ? fz2ptr[j + 4] : 0,
+                     _masks[rest-1].get(5) ? fz2ptr[j + 5] : 0,
+                     _masks[rest-1].get(6) ? fz2ptr[j + 6] : 0,
+                     _masks[rest-1].get(7) ? fz2ptr[j + 7] : 0,
+             };
+#endif*/
             } else {
                 fx2 = xsimd::load_unaligned(&fx2ptr[j]);
                 fy2 = xsimd::load_unaligned(&fy2ptr[j]);
