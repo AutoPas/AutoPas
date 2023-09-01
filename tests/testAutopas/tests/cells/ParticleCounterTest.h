f6ebd65504f180234bd249c7443dcce15ad0001e
David Martin <david.martin@in.tum.de>
2023-09-01 14:08:52 +0200

    first particle counter tests

---
+++ tests/testAutopas/tests/cells/ParticleCounterTest.h
@@ -0,0 +1,21 @@
+/**
+ * @file ParticleCounterTest.h
+ * @author D. Martin
+ * @date 01.09.23
+ */
+
+#pragma once
+
+#include "AutoPasTestBase.h"
+#include "testingHelpers/commonTypedefs.h"
+#include "autopas/cells/SortedCellView.h"
+#include "autopas/containers/verletClusterLists/ClusterTower.h"
+
+class ParticleCounterTest : public AutoPasTestBase {
+ public:
+  ParticleCounterTest() = default;
+
+  ~ParticleCounterTest() override = default;
+
+ private:
+};
