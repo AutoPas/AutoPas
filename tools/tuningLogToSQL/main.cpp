52387d93239ed0c08827750e272f987e1414902e
FG-TUM <f.gratl@tum.de>
2023-09-12 12:17:43 +0200

    formatting

--- tools/tuningLogToSQL/main.cpp
+++ tools/tuningLogToSQL/main.cpp
@@ -1,24 +1,24 @@
 #include "LogToSQLiteWriter.h"
 
 /**
-* This program converts files written by the TuningStrategyLoggerWrapper into a SQLite database file `out.db` for easy data analysis. It already creates some interesting views on the data.
-* See README.md.
-*/
-int main(int argc, char** argv) {
+ * This program converts files written by the TuningStrategyLoggerWrapper into a SQLite database file `out.db` for easy
+ * data analysis. It already creates some interesting views on the data. See README.md.
+ */
+int main(int argc, char **argv) {
   autopas::Logger::create();
 
-  if(argc <= 1) {
+  if (argc <= 1) {
     std::cerr << "Please provide the data files as arguments" << std::endl;
     exit(1);
   }
 
-  if(not getenv("DISABLE_DEBUG_LOG")) {
+  if (not getenv("DISABLE_DEBUG_LOG")) {
     autopas::Logger::get()->set_level(spdlog::level::info);
   }
 
   LogToSQLiteWriter writer{"out.db"};
 
-  for(int i = 1; i < argc; i++) {
+  for (int i = 1; i < argc; i++) {
     auto filename = argv[i];
     AutoPasLog(INFO, "Writing file {}: {}", i, filename);
 
