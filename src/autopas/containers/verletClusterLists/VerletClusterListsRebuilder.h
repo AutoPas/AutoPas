1f36960999dcc3fb9b2b2908d1bf798ad231e58d
FG-TUM <FG-TUM@users.noreply.github.com>
2023-09-23 23:41:49 +0200

    Apply suggestions from code review
    Co-authored-by: Samuel Newcome <97889602+SamNewcome@users.noreply.github.com>

--- src/autopas/containers/verletClusterLists/VerletClusterListsRebuilder.h
+++ src/autopas/containers/verletClusterLists/VerletClusterListsRebuilder.h
@@ -165,7 +165,7 @@ class VerletClusterListsRebuilder {
    * false)
    */
   void rebuildNeighborListsAndFillClusters(bool useNewton3) {
-    clearNeighborListsAndmoveDummiesIntoClusters();
+    clearNeighborListsAndMoveDummiesIntoClusters();
     updateNeighborLists(useNewton3);
 
     double dummyParticleDistance = _interactionLength * 2;
@@ -216,7 +216,7 @@ class VerletClusterListsRebuilder {
    * Clears previously saved neighbors from clusters and sets the 3D positions of the dummy particles to inside of the
    * cluster to avoid all dummies being in one place and potentially trigger cluster-cluster distance evaluations.
    */
-  void clearNeighborListsAndmoveDummiesIntoClusters() {
+  void clearNeighborListsAndMoveDummiesIntoClusters() {
     for (auto &tower : _towers) {
       tower.setDummyParticlesToLastActualParticle();
       for (auto &cluster : tower.getClusters()) {
