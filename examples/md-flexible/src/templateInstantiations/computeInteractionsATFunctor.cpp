cff95c260484a23b94ecc679b861f909804362dc
Markus Muehlhaeusser <markus.muehlhaeusser@tum.de>
2023-10-25 18:16:48 +0200

    Remove unused CMake option

--- examples/md-flexible/src/templateInstantiations/computeInteractionsATFunctor.cpp
+++ examples/md-flexible/src/templateInstantiations/computeInteractionsATFunctor.cpp
@@ -2,7 +2,7 @@
  * @file computeInteractionsATFunctor.cpp
  *
  * Contains an explicit template instantiation for the computeInteractions() method with the appropriate auto-vectorized
- * Lennard-Jones Functor and Particle Type, as determined by whether md-flexible is compiled with or without Multi-Site
+ * Axilrod-Teller Functor and Particle Type, as determined by whether md-flexible is compiled with or without Multi-Site
  * support. This is linked into the md-flexible executable to enable the other compilation units to only declare, but
  * not instantiate this template.
  */
