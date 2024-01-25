6f7c56a266cd1819fa072b1942897125ee0f84a2
Nick Deng <nanxingnick.deng@tum.de>
2024-01-25 11:31:05 +0100

    print particles per cell

--- src/autopas/containers/linkedCells/traversals/LCC08Traversal3B.h
+++ src/autopas/containers/linkedCells/traversals/LCC08Traversal3B.h
@@ -73,6 +73,10 @@ private:
 template <class ParticleCell, class Functor, DataLayoutOption::Value dataLayout, bool useNewton3>
 inline void LCC08Traversal3B<ParticleCell, Functor, dataLayout, useNewton3>::traverseParticleTriplets() {
  auto &cells = *(this->_cells);
+ std::cout << "Cells : " << cells.size() << std::endl;
+ for (int i = 0; i < cells.size(); ++i) {
+   std::cout << cells[i].size();
+ }
  this->c08Traversal([&](unsigned long x, unsigned long y, unsigned long z) {
    unsigned long baseIndex = utils::ThreeDimensionalMapping::threeToOneD(x, y, z, this->_cellsPerDimension);
    _cellHandler.processBaseCell(cells, baseIndex);
