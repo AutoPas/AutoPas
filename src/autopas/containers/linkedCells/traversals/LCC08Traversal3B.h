0781d64fca399716d100d36d25906a5dc4cb0d21
Nick Deng <nanxingnick.deng@tum.de>
2024-01-27 00:11:16 +0100

    print particles per cell

--- src/autopas/containers/linkedCells/traversals/LCC08Traversal3B.h
+++ src/autopas/containers/linkedCells/traversals/LCC08Traversal3B.h
@@ -89,7 +89,7 @@ inline void LCC08Traversal3B<ParticleCell, Functor, dataLayout, useNewton3>::tra
    }
 
  }
- double particlespercell = particles/cells.size;
+ double particlespercell = particles/cells.size();
  std::cout << "Particles per cell : " << particlespercell << std::endl;
  this->c08Traversal([&](unsigned long x, unsigned long y, unsigned long z) {
    unsigned long baseIndex = utils::ThreeDimensionalMapping::threeToOneD(x, y, z, this->_cellsPerDimension);
