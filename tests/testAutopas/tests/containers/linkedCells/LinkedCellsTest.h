faa2a380fa9fb725b8003e93ebc402e8cee91efd
FG-TUM <f.gratl@tum.de>
2024-02-07 16:01:51 +0100

    const, pass by ref, and renaming

--- tests/testAutopas/tests/containers/linkedCells/LinkedCellsTest.h
+++ tests/testAutopas/tests/containers/linkedCells/LinkedCellsTest.h
@@ -23,33 +23,32 @@ class LinkedCellsTest : public AutoPasTestBase {
   /// using keepListsValid = std::tuple_element_t<1, TestingType>;
   using LinkedCellsType = typename TestingType::first_t;
   using KeepListsValid = typename TestingType::second_t;
-  LinkedCellsTest() {}
 
  protected:
   bool _keepListsValid{KeepListsValid()};
 
   void checkParticleIDsInCells(
       LinkedCellsType &linkedCells,
-      std::map<unsigned long /*cellID*/, std::vector<std::tuple<int, autopas::OwnershipState>> /*particleIDs*/>
-          expectedParticleIDsInCells,
+      const std::map<unsigned long /*cellID*/, std::vector<std::tuple<int, autopas::OwnershipState>> /*particleIDs*/>
+          &expectedParticleIDsInCells,
       bool ordered, int line);
 };
 
 template <class TestingType>
 void LinkedCellsTest<TestingType>::checkParticleIDsInCells(
     LinkedCellsType &linkedCells,
-    std::map<unsigned long, std::vector<std::tuple<int, autopas::OwnershipState>>> expectedParticleIDsInCells,
+    const std::map<unsigned long, std::vector<std::tuple<int, autopas::OwnershipState>>> &expectedParticleIDsInCells,
     bool ordered, int line) {
   // check particles are where we expect them to be (and nothing else)
-  for (size_t i = 0; i < linkedCells.getCells().size(); ++i) {
-    if (auto iter = expectedParticleIDsInCells.find(i); iter != expectedParticleIDsInCells.end()) {
-      auto expectedParticles = iter->second;
-      auto expectedNumParticles = expectedParticles.size();
-      ASSERT_EQ(linkedCells.getCells()[i].size(), expectedNumParticles) << "called from line: " << line;
+  for (size_t cellId = 0; cellId < linkedCells.getCells().size(); ++cellId) {
+    if (const auto iter = expectedParticleIDsInCells.find(cellId); iter != expectedParticleIDsInCells.end()) {
+      const auto &expectedParticles = iter->second;
+      const auto expectedNumParticles = expectedParticles.size();
+      EXPECT_EQ(linkedCells.getCells()[cellId].size(), expectedNumParticles) << "called from line: " << line;
 
       std::vector<std::tuple<int, autopas::OwnershipState>> foundParticles;
       {
-        auto pIter = linkedCells.getCells()[i].begin();
+        auto pIter = linkedCells.getCells()[cellId].begin();
         for (int j = 0; j < expectedNumParticles; ++j, ++pIter) {
           foundParticles.push_back({pIter->getID(), pIter->getOwnershipState()});
         }
@@ -61,11 +60,11 @@ void LinkedCellsTest<TestingType>::checkParticleIDsInCells(
             << "called from line: " << line;
       }
     } else {
-      bool isEmpty = linkedCells.getCells()[i].isEmpty();
+      const bool isEmpty = linkedCells.getCells()[cellId].isEmpty();
       if (not isEmpty) {
         // If the cell isn't empty, all particles should be dummy particles!
-        for (auto &p : linkedCells.getCells()[i]) {
-          EXPECT_TRUE(p.isDummy()) << "Cell: " << i << " Particle ID: " << p.getID() << std::endl
+        for (const auto &p : linkedCells.getCells()[cellId]) {
+          EXPECT_TRUE(p.isDummy()) << "Cell: " << cellId << " Particle ID: " << p.getID() << std::endl
                                    << "called from line: " << line;
         }
       }
