7c3b05b29ffee8ccd97db9e4a335beeb65b5fe55
Manish Kumar Mishra <manish6016mishra@gmail.com>
2024-03-22 15:16:18 +0100

    removed getNumberOfParticles()

--- src/autopas/containers/verletListsCellBased/verletLists/VerletLists.h
+++ src/autopas/containers/verletListsCellBased/verletLists/VerletLists.h
@@ -195,16 +195,6 @@ class VerletLists : public VerletListsLinkedBase<Particle> {
     _soaListIsValid = true;
   }
 
-  /**
-   * Shows if the SoA neighbor list is currently valid.
-   */
-  bool _soaListIsValid{false};
-
-  /**
-   * Specifies for what data layout the verlet lists are build.
-   */
-  BuildVerletListType _buildVerletListType;
-
  private:
   /**
    * Neighbor Lists: Map of particle pointers to vector of particle pointers.
@@ -222,6 +212,16 @@ class VerletLists : public VerletListsLinkedBase<Particle> {
    * For every Particle, identified via the _particlePtr2indexMap, a vector of its neighbor indices is stored.
    */
   std::vector<std::vector<size_t, autopas::AlignedAllocator<size_t>>> _soaNeighborLists;
+
+  /**
+   * Shows if the SoA neighbor list is currently valid.
+   */
+  bool _soaListIsValid{false};
+
+  /**
+   * Specifies for what data layout the verlet lists are build.
+   */
+  BuildVerletListType _buildVerletListType;
 };
 
 }  // namespace autopas
--- src/autopas/containers/verletListsCellBased/verletListsCells/neighborLists/VLCCellPairNeighborList.h
+++ src/autopas/containers/verletListsCellBased/verletListsCells/neighborLists/VLCCellPairNeighborList.h
@@ -58,11 +58,6 @@ class VLCCellPairNeighborList : public VLCNeighborListInterface<Particle> {
   }
 
   /**
-   * @copydoc VLCNeighborListInterface::getNumberOfParticles()
-   */
-  size_t getNumberOfParticles() const override { return _aosNeighborList.size(); }
-
-  /**
    * Returns the neighbor list in AoS layout.
    * @return Neighbor list in AoS layout.
    */
