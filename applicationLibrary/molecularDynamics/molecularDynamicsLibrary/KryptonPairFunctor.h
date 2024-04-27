d38e96d0ec4605fc13e3949a079000b48e62a03a
Markus Muehlhaeusser <markus.muehlhaeusser@tum.de>
2024-04-27 22:37:19 +0200

    Initialize empty krypton pair functor

---
+++ applicationLibrary/molecularDynamics/molecularDynamicsLibrary/KryptonPairFunctor.h
@@ -0,0 +1,314 @@
+/**
+ * @file KryptonFunctor.h
+ *
+ * @date 27.04.2024
+ * @author muehlhaeusser
+ */
+
+#pragma once
+
+#include "autopas/pairwiseFunctors/Functor.h"
+#include "autopas/particles/OwnershipState.h"
+#include "autopas/utils/AlignedAllocator.h"
+#include "autopas/utils/ArrayMath.h"
+#include "autopas/utils/ExceptionHandler.h"
+#include "autopas/utils/SoA.h"
+#include "autopas/utils/StaticBoolSelector.h"
+#include "autopas/utils/WrapOpenMP.h"
+#include "autopas/utils/inBox.h"
+
+namespace mdLib {
+
+/**
+ * A functor to handle interactions between two krypton atoms.
+ * This functor assumes that duplicated calculations are always happening, which is characteristic for a Full-Shell
+ * scheme.
+ * @tparam Particle The type of particle.
+ * @tparam useNewton3 Switch for the functor to support newton3 on, off or both. See FunctorN3Modes for possible values.
+ * @tparam calculateGlobals Defines whether the global values are to be calculated (energy, virial).
+ * @tparam relevantForTuning Whether or not the auto-tuner should consider this functor.
+ */
+template <class Particle,
+          autopas::FunctorN3Modes useNewton3 = autopas::FunctorN3Modes::Both, bool calculateGlobals = false,
+          bool relevantForTuning = true>
+class KryptonPairFunctor
+    : public autopas::Functor<
+          Particle, KryptonPairFunctor<Particle, useNewton3, calculateGlobals, relevantForTuning>> {
+  /**
+   * Structure of the SoAs defined by the particle.
+   */
+  using SoAArraysType = typename Particle::SoAArraysType;
+
+  /**
+   * Precision of SoA entries.
+   */
+  using SoAFloatPrecision = typename Particle::ParticleSoAFloatPrecision;
+
+ public:
+  /**
+   * Deleted default constructor
+   */
+  KryptonPairFunctor() = delete;
+
+ private:
+  /**
+   * Internal, actual constructor.
+   * @param cutoff
+   * @note param dummy is unused, only there to make the signature different from the public constructor.
+   */
+  explicit KryptonPairFunctor(double cutoff, void * /*dummy*/)
+      : autopas::Functor<Particle,
+                         KryptonPairFunctor<Particle, useNewton3, calculateGlobals, relevantForTuning>>(
+            cutoff),
+        _cutoffSquared{cutoff * cutoff},
+        _potentialEnergySum{0.},
+        _virialSum{0., 0., 0.},
+        _aosThreadData(),
+        _postProcessed{false} {
+    if constexpr (calculateGlobals) {
+      _aosThreadData.resize(autopas::autopas_get_max_threads());
+    }
+  }
+
+ public:
+  /**
+   * Constructor for Functor with mixing disabled. When using this functor it is necessary to call
+   * setParticleProperties() to set internal constants because it does not use a particle properties library.
+   *
+   * @note Only to be used with mixing == false.
+   *
+   * @param cutoff
+   */
+  explicit KryptonPairFunctor(double cutoff) : KryptonPairFunctor(cutoff, nullptr) {}
+
+  bool isRelevantForTuning() final { return relevantForTuning; }
+
+  bool allowsNewton3() final {
+    return useNewton3 == autopas::FunctorN3Modes::Newton3Only or useNewton3 == autopas::FunctorN3Modes::Both;
+  }
+
+  bool allowsNonNewton3() final {
+    return useNewton3 == autopas::FunctorN3Modes::Newton3Off or useNewton3 == autopas::FunctorN3Modes::Both;
+  }
+
+  void AoSFunctor(Particle &i, Particle &j, bool newton3) final {
+    using namespace autopas::utils::ArrayMath::literals;
+
+    if (i.isDummy() or j.isDummy()) {
+      return;
+    }
+
+    auto dr = i.getR() - j.getR();
+    double dr2 = autopas::utils::ArrayMath::dot(dr, dr);
+
+    if (dr2 > _cutoffSquared) {
+      return;
+    }
+
+    auto f = 0.0;
+    i.addF(f);
+    if (newton3) {
+      // only if we use newton 3 here, we want to
+      j.subF(f);
+    }
+    if (calculateGlobals) {
+      auto virial = dr * f;
+      double potentialEnergy = 0.0;
+
+      const int threadnum = autopas::autopas_get_thread_num();
+      if (i.isOwned()) {
+        _aosThreadData[threadnum].potentialEnergySum += potentialEnergy;
+        _aosThreadData[threadnum].virialSum += virial;
+      }
+      // for non-newton3 the second particle will be considered in a separate calculation
+      if (newton3 and j.isOwned()) {
+        _aosThreadData[threadnum].potentialEnergySum += potentialEnergy;
+        _aosThreadData[threadnum].virialSum += virial;
+      }
+    }
+  }
+
+  /**
+   * @copydoc autopas::Functor::SoAFunctorSingle()
+   * This functor will always use a newton3 like traversal of the soa.
+   * However, it still needs to know about newton3 to correctly add up the global values.
+   */
+  void SoAFunctorSingle(autopas::SoAView<SoAArraysType> soa, bool newton3) final {}
+
+  /**
+   * @copydoc autopas::Functor::SoAFunctorPair()
+   */
+  void SoAFunctorPair(autopas::SoAView<SoAArraysType> soa1, autopas::SoAView<SoAArraysType> soa2,
+                      const bool newton3) final {}
+
+ public:
+  // clang-format off
+  /**
+   * @copydoc autopas::Functor::SoAFunctorVerlet()
+   * @note If you want to parallelize this by openmp, please ensure that there
+   * are no dependencies, i.e. introduce colors!
+   */
+  // clang-format on
+  void SoAFunctorVerlet(autopas::SoAView<SoAArraysType> soa, const size_t indexFirst,
+                        const std::vector<size_t, autopas::AlignedAllocator<size_t>> &neighborList,
+                        bool newton3) final {
+    if (soa.size() == 0 or neighborList.empty()) return;
+  }
+
+  /**
+   * @copydoc autopas::Functor::getNeededAttr()
+   */
+  constexpr static auto getNeededAttr() {
+    return std::array<typename Particle::AttributeNames, 9>{
+        Particle::AttributeNames::id,     Particle::AttributeNames::posX,   Particle::AttributeNames::posY,
+        Particle::AttributeNames::posZ,   Particle::AttributeNames::forceX, Particle::AttributeNames::forceY,
+        Particle::AttributeNames::forceZ, Particle::AttributeNames::typeId, Particle::AttributeNames::ownershipState};
+  }
+
+  /**
+   * @copydoc autopas::Functor::getNeededAttr(std::false_type)
+   */
+  constexpr static auto getNeededAttr(std::false_type) {
+    return std::array<typename Particle::AttributeNames, 6>{
+        Particle::AttributeNames::id,   Particle::AttributeNames::posX,   Particle::AttributeNames::posY,
+        Particle::AttributeNames::posZ, Particle::AttributeNames::typeId, Particle::AttributeNames::ownershipState};
+  }
+
+  /**
+   * @copydoc autopas::Functor::getComputedAttr()
+   */
+  constexpr static auto getComputedAttr() {
+    return std::array<typename Particle::AttributeNames, 3>{
+        Particle::AttributeNames::forceX, Particle::AttributeNames::forceY, Particle::AttributeNames::forceZ};
+  }
+
+  /**
+   * Get the number of flops used per kernel call for a given particle pair. This should count the
+   * floating point operations needed for two particles that lie within a cutoff radius, having already calculated the
+   * distance.
+   * @param molAType molecule A's type id
+   * @param molBType molecule B's type id
+   * @param newton3 is newton3 applied.
+   * @note molAType and molBType should always be the same for KryptonPairFunctor (i.e. Krypton atoms), but the method is kept to have a consistent interface with other
+   * functors.
+   * @return the number of floating point operations
+   */
+  static unsigned long getNumFlopsPerKernelCall(size_t molAType, size_t molBType, bool newton3) {
+    // Kernel:
+    // Total =
+    return newton3 ? 0ul : 0ul;
+  }
+
+  /**
+   * Reset the global values.
+   * Will set the global values to zero to prepare for the next iteration.
+   */
+  void initTraversal() final {
+    _potentialEnergySum = 0.;
+    _virialSum = {0., 0., 0.};
+    _postProcessed = false;
+    for (size_t i = 0; i < _aosThreadData.size(); ++i) {
+      _aosThreadData[i].setZero();
+    }
+  }
+
+  /**
+   * Accumulates global values, e.g. potential energy and virial.
+   * @param newton3
+   */
+  void endTraversal(bool newton3) final {
+    using namespace autopas::utils::ArrayMath::literals;
+
+    if (_postProcessed) {
+      throw autopas::utils::ExceptionHandler::AutoPasException(
+          "Already postprocessed, endTraversal(bool newton3) was called twice without calling initTraversal().");
+    }
+    if (calculateGlobals) {
+      for (size_t i = 0; i < _aosThreadData.size(); ++i) {
+        _virialSum += _aosThreadData[i].potentialEnergySum;
+        _potentialEnergySum += _aosThreadData[i].potentialEnergySum;
+      }
+
+      _postProcessed = true;
+
+      AutoPasLog(TRACE, "Final potential energy {}", _potentialEnergySum);
+      AutoPasLog(TRACE, "Final virial           {}", _virialSum[0] + _virialSum[1] + _virialSum[2]);
+    }
+  }
+
+  /**
+   * Get the potential Energy.
+   * @return the potential Energy
+   */
+  double getPotentialEnergy() {
+    if (not calculateGlobals) {
+      throw autopas::utils::ExceptionHandler::AutoPasException(
+          "Trying to get potential energy even though calculateGlobals is false. If you want this functor to calculate "
+          "global "
+          "values, please specify calculateGlobals to be true.");
+    }
+    if (not _postProcessed) {
+      throw autopas::utils::ExceptionHandler::AutoPasException(
+          "Cannot get potential energy, because endTraversal was not called.");
+    }
+    return _potentialEnergySum;
+  }
+
+  /**
+   * Get the virial.
+   * @return
+   */
+  double getVirial() {
+    if (not calculateGlobals) {
+      throw autopas::utils::ExceptionHandler::AutoPasException(
+          "Trying to get virial even though calculateGlobals is false. If you want this functor to calculate global "
+          "values, please specify calculateGlobals to be true.");
+    }
+    if (not _postProcessed) {
+      throw autopas::utils::ExceptionHandler::AutoPasException(
+          "Cannot get virial, because endTraversal was not called.");
+    }
+    return _virialSum[0] + _virialSum[1] + _virialSum[2];
+  }
+
+ private:
+  /**
+   * This class stores internal data of each thread, make sure that this data has proper size, i.e. k*64 Bytes!
+   */
+  class AoSThreadData {
+   public:
+    AoSThreadData()
+        : virialSum{0., 0., 0.},
+          potentialEnergySum{0.},
+          __remainingTo64{} {}
+    void setZero() {
+      virialSum = {0., 0., 0.};
+      potentialEnergySum = 0.;
+    }
+
+    // variables
+    std::array<double, 3> virialSum;
+    double potentialEnergySum;
+
+   private:
+    // dummy parameter to get the right size (64 bytes)
+    double __remainingTo64[(64 - 8 * sizeof(double)) / sizeof(double)];
+  };
+  // make sure of the size of AoSThreadData
+  static_assert(sizeof(AoSThreadData) % 64 == 0, "AoSThreadData has wrong size");
+
+  const double _cutoffSquared;
+
+  // sum of the potential energy, only calculated if calculateGlobals is true
+  double _potentialEnergySum;
+
+  // sum of the virial, only calculated if calculateGlobals is true
+  std::array<double, 3> _virialSum;
+
+  // thread buffer for aos
+  std::vector<AoSThreadData> _aosThreadData;
+
+  // defines whether or whether not the global values are already preprocessed
+  bool _postProcessed;
+};
+}  // namespace mdLib
