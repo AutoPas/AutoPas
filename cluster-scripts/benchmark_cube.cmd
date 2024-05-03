291298f85c1eab1b9f2a712f5404c85688cc46b4
Luis Gall <luis.gall@tum.de>
2024-05-03 12:59:52 +0200

    Added first script for benchmarking at linux cluster

---
+++ cluster-scripts/benchmark_cube.cmd
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J autopas_highway
+#SBATCH -o ./output/%x.%j.%N.out
+#SBATCH -D ./
+#SBATCH --get-user-env
+#SBATCH --clusters=cm2_tiny
+#SBATCH --partition=cm2_tiny
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mail-type=end
+#SBATCH --mail-user=luis.gall@tum.de
+#SBATCH --export=NONE
+#SBATCH --time=00:02:00
+module load slurm_setup
+module load gcc
+module load cmake
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+./build/examples/md-flexible/md-flexible --yaml-filename Cube_20_hwy.yaml
