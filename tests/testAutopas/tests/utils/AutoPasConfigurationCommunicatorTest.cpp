593896f88b46c649fdb4a59b4bb3ac24b2fd4ae7
Markus Muehlhaeusser <markus.muehlhaeusser@tum.de>
2024-05-17 00:11:26 +0200

    Add AutoPasConfigurationCommunicatorTest

---
+++ tests/testAutopas/tests/utils/AutoPasConfigurationCommunicatorTest.cpp
@@ -0,0 +1,52 @@
+/**
+ * @file AutoPasConfigurationCommunicatorTest.cpp
+ * @author muehlhaeusser
+ * @date 16.05.24
+ */
+#include <gmock/gmock-matchers.h>
+
+#include "AutoPasConfigurationCommunicatorTest.h"
+
+#include "autopas/utils/AutoPasConfigurationCommunicator.h"
+#include "autopas/tuning/utils/SearchSpaceGenerators.h"
+
+/**
+ * Takes a set of configurations, serializes them and deserializes them back.
+ * Then checks if the deserialized configs are the same as before.
+ * @param configs
+ */
+void AutoPasConfigurationCommunicatorTest::testConfigsCommunication(std::set<autopas::Configuration> &configs) {
+
+  auto configsVector = std::vector<autopas::Configuration>(configs.begin(), configs.end());
+
+  // Serialize
+  auto byteVector = autopas::utils::AutoPasConfigurationCommunicator::serializeConfigurations(configsVector);
+  // Deserialize
+  auto configsDeserialized = autopas::utils::AutoPasConfigurationCommunicator::deserializeConfigurations(byteVector);
+
+  EXPECT_THAT(configsVector, ::testing::ContainerEq(configsDeserialized));
+}
+
+/**
+ * Creates a searchSpace (set of configurations) and tests if the configurations are the same after serializing and deserializing them.
+ */
+TEST_F(AutoPasConfigurationCommunicatorTest, SerializationTest) {
+
+  auto pairwiseSearchSpace = autopas::SearchSpaceGenerators::cartesianProduct(containerOptions,pairwiseTraversalOptions,
+                                                                      loadEstimatorOptions, dataLayoutOptions,
+                                                                      newton3Options, &cellSizeFactors,
+                                                                      autopas::InteractionTypeOption::pairwise);
+  auto maxSearchSpaceSize = autopas::utils::AutoPasConfigurationCommunicator::getSearchSpaceSize(containerOptions, cellSizeFactors, pairwiseTraversalOptions, loadEstimatorOptions, dataLayoutOptions, newton3Options, autopas::InteractionTypeOption::pairwise);
+  EXPECT_GE(maxSearchSpaceSize, pairwiseSearchSpace.size());
+
+  testConfigsCommunication(pairwiseSearchSpace);
+
+  auto triwiseSearchSpace = autopas::SearchSpaceGenerators::cartesianProduct(containerOptions,triwiseTraversalOptions,
+                                                                              loadEstimatorOptions, dataLayoutOptions,
+                                                                              newton3Options, &cellSizeFactors,
+                                                                              autopas::InteractionTypeOption::triwise);
+  maxSearchSpaceSize = autopas::utils::AutoPasConfigurationCommunicator::getSearchSpaceSize(containerOptions, cellSizeFactors, triwiseTraversalOptions, loadEstimatorOptions, dataLayoutOptions, newton3Options, autopas::InteractionTypeOption::triwise);
+
+  EXPECT_GE(maxSearchSpaceSize, triwiseSearchSpace.size());
+  testConfigsCommunication(triwiseSearchSpace);
+}
---
+++ tests/testAutopas/tests/utils/AutoPasConfigurationCommunicatorTest.h
@@ -0,0 +1,36 @@
+/**
+ * @file AutoPasConfigurationCommunicatorTest.h
+ * @author muehlhaeusser
+ * @date 16.05.24
+ */
+
+#pragma once
+
+#include <set>
+#include <gtest/gtest.h>
+
+#include "AutoPasTestBase.h"
+
+#include "autopas/tuning/Configuration.h"
+#include "autopas/options/ContainerOption.h"
+#include "autopas/options/LoadEstimatorOption.h"
+#include "autopas/options/DataLayoutOption.h"
+#include "autopas/options/Newton3Option.h"
+#include "autopas/options/SelectorStrategyOption.h"
+#include "autopas/options/TraversalOption.h"
+#include "autopas/utils/NumberSetFinite.h"
+
+class AutoPasConfigurationCommunicatorTest : public AutoPasTestBase {
+
+ public:
+
+  void testConfigsCommunication(std::set<autopas::Configuration> &configs);
+
+  std::set<autopas::ContainerOption> containerOptions = autopas::ContainerOption::getMostOptions();
+  std::set<autopas::LoadEstimatorOption> loadEstimatorOptions = autopas::LoadEstimatorOption::getMostOptions();
+  std::set<autopas::DataLayoutOption> dataLayoutOptions = autopas::DataLayoutOption::getMostOptions();
+  std::set<autopas::Newton3Option> newton3Options = autopas::Newton3Option::getMostOptions();
+  autopas::NumberSetFinite<double> cellSizeFactors = {0.5, 1.0, 1.5};
+  std::set<autopas::TraversalOption> pairwiseTraversalOptions = autopas::TraversalOption::getMostPairwiseOptions();
+  std::set<autopas::TraversalOption> triwiseTraversalOptions = autopas::TraversalOption::getMostTriwiseOptions();
+};
