a437aa6efd26bd01220f39df55b6cd1b400b165c
FG-TUM <f.gratl@tum.de>
2024-06-01 09:34:33 +0200

    First attempt at rebuild function without functor.
      - Contains compile time switch to use old style
      - Contains workaround for all other traversals to use the old style
      - Seems to be not yet completely bug free (see TraversalComparison test with shifts)

--- src/autopas/containers/verletListsCellBased/verletListsCells/VerletListsCells.h
+++ src/autopas/containers/verletListsCellBased/verletListsCells/VerletListsCells.h
@@ -118,9 +118,168 @@ class VerletListsCells : public VerletListsLinkedBase<Particle> {
   void rebuildNeighborLists(TraversalInterface *traversal) override {
     this->_verletBuiltNewton3 = traversal->getUseNewton3();
 
-    _neighborList.buildAoSNeighborList(this->_linkedCells, this->_verletBuiltNewton3, this->getCutoff(),
-                                       this->getVerletSkin(), this->getInteractionLength(),
-                                       traversal->getTraversalType(), _dataLayoutDuringListRebuild);
+    // VLP needs constexpr special case because the types and thus interfaces are slightly different
+    if constexpr (std::is_same_v<NeighborList, VLCCellPairNeighborList<Particle>>) {
+      _neighborList.buildAoSNeighborList(this->_linkedCells, this->_verletBuiltNewton3, this->getCutoff(),
+                                         this->getVerletSkin(), this->getInteractionLength(),
+                                         traversal->getTraversalType(), _dataLayoutDuringListRebuild);
+
+    } else {
+// Switch to test two implementations for vlc_c08 list generation
+#define BUILD_WITHOUT_FUNCTOR
+#ifdef BUILD_WITHOUT_FUNCTOR
+      if (traversal->getTraversalType() == TraversalOption::vlc_c08) {
+        const auto &cellsPerDim = utils::ArrayUtils::static_cast_copy_array<int>(
+            this->_linkedCells.getCellBlock().getCellsPerDimensionWithHalo());
+
+        // list of cell interaction pairs relative to the base cell
+        const auto offsets = [&]() {
+          std::vector<std::pair<int, int>> offsets;
+          offsets.reserve(27);
+          // This is currently guaranteed by the constructor
+          const int interactionCellsPerDim = 1;
+          for (int z = -interactionCellsPerDim; z <= interactionCellsPerDim; ++z) {
+            for (int y = -interactionCellsPerDim; y <= interactionCellsPerDim; ++y) {
+              for (int x = -interactionCellsPerDim; x <= interactionCellsPerDim; ++x) {
+                int baseCell = 0;
+                int partnerCell = utils::ThreeDimensionalMapping::threeToOneD(x, y, z, cellsPerDim);
+                if (x < 0) {
+                  baseCell -= x;
+                  partnerCell -= x;
+                }
+                if (y < 0) {
+                  baseCell -= y * cellsPerDim[0];
+                  partnerCell -= y * cellsPerDim[0];
+                }
+                if (z < 0) {
+                  baseCell -= z * cellsPerDim[0] * cellsPerDim[1];
+                  partnerCell -= z * cellsPerDim[0] * cellsPerDim[1];
+                }
+                const auto &[smallerIndex, biggerIndex] = std::minmax(baseCell, partnerCell);
+                if (std::find(offsets.begin(), offsets.end(), std::make_pair(smallerIndex, biggerIndex)) ==
+                    offsets.end()) {
+                  offsets.emplace_back(smallerIndex, biggerIndex);
+                }
+              }
+            }
+          }
+          // sort offsets to group processing of the same cells (-> better cache re-usage)
+          std::sort(offsets.begin(), offsets.end(), [](const auto &pair1, const auto &pair2) {
+            const auto &[a1, b1] = pair1;
+            const auto &[a2, b2] = pair2;
+
+            if (a1 == a2) {
+              return b1 < b2;
+            } else {
+              return a1 < a2;
+            }
+          });
+          return offsets;
+        }();
+
+        using namespace utils::ArrayMath::literals;
+        // Reset lists and define some aliases
+        auto &neighborLists = _neighborList.getAoSNeighborList();
+        neighborLists.clear();
+        _neighborList.setLinkedCells(&this->_linkedCells);
+        auto &cells = this->_linkedCells.getCells();
+        const auto interactionLength = this->getInteractionLength();
+        const auto interactionLengthSquared = interactionLength * interactionLength;
+        const auto boxSizeWithHalo =
+            this->_linkedCells.getBoxMax() - this->_linkedCells.getBoxMin() +
+            std::array<double, 3>{interactionLength, interactionLength, interactionLength} * 2.;
+
+        const auto listLengthEstimate = VerletListsCellsHelpers::estimateListLength(
+            this->_linkedCells.getNumberOfParticles(IteratorBehavior::ownedOrHalo), boxSizeWithHalo, interactionLength,
+            1.3);
+
+        neighborLists.resize(cells.size());
+
+        // Helper function to insert a pointer into a list of the base cell.
+        // It considers the cases that neither particle is in the base cell and in that case
+        // finds or creates the appropriate list
+        auto insert = [&](auto &p1, auto i, auto &p2, auto cellIndex1, auto cellIndexBase, auto &currentCellsLists) {
+          // Easy case: cell1 is the base cell
+          if (cellIndexBase == cellIndex1) {
+            currentCellsLists[i].second.push_back(&p2);
+          } else {
+            // Otherwise, check if the base cell already has a list for p1
+            auto iter = std::find_if(currentCellsLists.begin(), currentCellsLists.end(), [&](const auto &pair) {
+              const auto &[particlePtr, list] = pair;
+              return particlePtr == &p1;
+            });
+            // If yes, append p2 to it.
+            if (iter != currentCellsLists.end()) {
+              iter->second.push_back(&p2);
+            } else {
+              // If no, create one, reserve space and emplace p2
+              currentCellsLists.emplace_back(&p1, std::vector<Particle *>{});
+              currentCellsLists.back().second.reserve(listLengthEstimate);
+              currentCellsLists.back().second.push_back(&p2);
+            }
+          }
+        };
+
+        // Go over all cells except the very last layer
+      AUTOPAS_OPENMP(parallel for collapse(3))
+      for (int z = 0; z < cellsPerDim[2] - 1; ++z) {
+        for (int y = 0; y < cellsPerDim[1] - 1; ++y) {
+          for (int x = 0; x < cellsPerDim[0] - 1; ++x) {
+            const auto cellIndexBase = utils::ThreeDimensionalMapping::threeToOneD(x, y, z, cellsPerDim);
+            auto &baseCell = cells[cellIndexBase];
+            auto &baseCellsLists = neighborLists[cellIndexBase];
+            // Allocate space for ptr-list pairs for this cell
+            baseCellsLists.reserve(
+                std::ceil(static_cast<double>(baseCell.size()) * 1.2));  // FIXME heuristic for number of lists??
+            for (size_t i = 0; i < baseCell.size(); ++i) {
+              baseCellsLists.emplace_back(&baseCell[i], std::vector<Particle *>{});
+              baseCellsLists.back().second.reserve(listLengthEstimate);
+            }
+
+            // Build c08 lists according to predefined cell pairs
+            for (const auto &[offset1, offset2] : offsets) {
+              const auto cellIndex1 = cellIndexBase + offset1;
+              const auto cellIndex2 = cellIndexBase + offset2;
+
+              // Skip if both cells only contain halos
+              if (not(cells[cellIndex1].getPossibleParticleOwnerships() == OwnershipState::owned) and
+                  not(cells[cellIndex2].getPossibleParticleOwnerships() == OwnershipState::owned)) {
+                continue;
+              }
+
+              // Go over all particle pairs in the two cells
+              for (size_t i = 0; i < cells[cellIndex1].size(); ++i) {
+                auto &p1 = cells[cellIndex1][i];
+                for (size_t j = (cellIndex1 == cellIndex2) ? i + 1 : 0; j < cells[cellIndex2].size(); ++j) {
+                  auto &p2 = cells[cellIndex2][j];
+                  if (&p1 == &p2 or p1.isDummy() or p2.isDummy()) {
+                    continue;
+                  }
+
+                  const auto distVec = p2.getR() - p1.getR();
+                  const auto distSquared = utils::ArrayMath::dot(distVec, distVec);
+                  if (distSquared < interactionLengthSquared) {
+                    insert(p1, i, p2, cellIndex1, cellIndexBase, baseCellsLists);
+
+                    if (not this->_verletBuiltNewton3) {
+                      insert(p2, j, p1, cellIndex2, cellIndexBase, baseCellsLists);
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      } else {
+#endif
+        _neighborList.buildAoSNeighborList(this->_linkedCells, this->_verletBuiltNewton3, this->getCutoff(),
+                                           this->getVerletSkin(), this->getInteractionLength(),
+                                           traversal->getTraversalType(), _dataLayoutDuringListRebuild);
+#ifdef BUILD_WITHOUT_FUNCTOR
+      }
+#endif
+    }
 
     if (traversal->getDataLayout() == DataLayoutOption::soa) {
       _neighborList.generateSoAFromAoS(this->_linkedCells);
