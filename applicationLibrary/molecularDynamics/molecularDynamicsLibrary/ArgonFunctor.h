31a007cf11ae173d271187c645e1087cc2ba831b
Irene Angelucci <irene.angelucci@tum.de>
2024-06-11 12:31:38 +0200

    Argon Functor skeleton

---
+++ applicationLibrary/molecularDynamics/molecularDynamicsLibrary/ArgonFunctor.h
@@ -0,0 +1,257 @@
+/**
+* @file ArgonFunctor.h
+* @author I. Angelucci
+* @date 11/06/24
+*/
+
+#pragma once
+
+#include "ParticlePropertiesLibrary.h"
+#include "autopas/baseFunctors/TriwiseFunctor.h"
+#include "autopas/utils/ArrayMath.h"
+#include "autopas/utils/SoA.h"
+#include "autopas/utils/WrapOpenMP.h"
+
+namespace mdLib {
+
+template <class Particle, bool useMixing = false, autopas::FunctorN3Modes useNewton3 = autopas::FunctorN3Modes::Both,
+          bool calculateGlobals = false>
+class ArgonFunctor
+    : public autopas::TriwiseFunctor<Particle,
+                                     ArgonFunctor<Particle, useMixing, useNewton3, calculateGlobals>> {
+
+  using SoAArraysType = typename Particle::SoAArraysType;
+  using SoAFloatPrecision = typename Particle::ParticleSoAFloatPrecision;
+
+ public:
+  /**
+   * Deleted default constructor
+   */
+  ArgonFunctor() = delete;
+
+  /**
+   * Constructor for Functor with mixing disabled. When using this functor it is necessary to call
+   * setParticleProperties() to set internal constants because it does not use a particle properties library.
+   *
+   * @note Only to be used with mixing == false.
+   *
+   * @param cutoff
+   */
+  explicit ArgonFunctor(double cutoff) : ArgonFunctor(cutoff, nullptr) {
+    static_assert(not useMixing,
+                  "Mixing without a ParticlePropertiesLibrary is not possible! Use a different constructor or set "
+                  "mixing to false.");
+  }
+
+  /**
+   * Constructor for Functor with mixing active. This functor takes a ParticlePropertiesLibrary to look up (mixed)
+   * properties like nu.
+   * @param cutoff
+   * @param particlePropertiesLibrary
+   */
+  explicit ArgonFunctor(double cutoff, ParticlePropertiesLibrary<double, size_t> &particlePropertiesLibrary)
+      : ArgonFunctor(cutoff, nullptr) {
+    static_assert(useMixing,
+                  "Not using Mixing but using a ParticlePropertiesLibrary is not allowed! Use a different constructor "
+                  "or set mixing to true.");
+    _PPLibrary = &particlePropertiesLibrary;
+  }
+
+  std::string getName() final { return "ArgonFunctorAutoVec"; }
+
+  bool isRelevantForTuning() final { return true; }
+
+  bool allowsNewton3() final {
+    return useNewton3 == autopas::FunctorN3Modes::Newton3Only or useNewton3 == autopas::FunctorN3Modes::Both;
+  }
+
+  bool allowsNonNewton3() final {
+    return useNewton3 == autopas::FunctorN3Modes::Newton3Off or useNewton3 == autopas::FunctorN3Modes::Both;
+  }
+
+  void AoSFunctor(Particle &i, Particle &j, Particle &k, bool newton3) {}
+
+  /**
+   * @copydoc autopas::Functor::getNeededAttr()
+   */
+  constexpr static auto getNeededAttr() {
+    return std::array<typename Particle::AttributeNames, 9>{
+        Particle::AttributeNames::id,     Particle::AttributeNames::posX,   Particle::AttributeNames::posY,
+        Particle::AttributeNames::posZ,   Particle::AttributeNames::forceX, Particle::AttributeNames::forceY,
+        Particle::AttributeNames::forceZ, Particle::AttributeNames::typeId, Particle::AttributeNames::ownershipState};
+  }
+
+  /**
+   * @copydoc autopas::Functor::getNeededAttr(std::false_type)
+   */
+  constexpr static auto getNeededAttr(std::false_type) {
+    return std::array<typename Particle::AttributeNames, 6>{
+        Particle::AttributeNames::id,   Particle::AttributeNames::posX,   Particle::AttributeNames::posY,
+        Particle::AttributeNames::posZ, Particle::AttributeNames::typeId, Particle::AttributeNames::ownershipState};
+  }
+
+  /**
+   * @copydoc autopas::Functor::getComputedAttr()
+   */
+  constexpr static auto getComputedAttr() {
+    return std::array<typename Particle::AttributeNames, 3>{
+        Particle::AttributeNames::forceX, Particle::AttributeNames::forceY, Particle::AttributeNames::forceZ};
+  }
+
+  /**
+   * @return useMixing
+   */
+  constexpr static bool getMixing() { return useMixing; }
+
+  /**
+   * Get the number of flops used per kernel call for a given particle pair. This should count the
+   * floating point operations needed for two particles that lie within a cutoff radius, having already calculated the
+   * distance.
+   * @param molAType molecule A's type id
+   * @param molBType molecule B's type id
+   * @param molCType molecule C's type id
+   * @param newton3 is newton3 applied.
+   * @note The molecule types make no difference for ArgonFunctor, but are kept to have a consistent interface
+   * for other functors where they may.
+   * @return the number of floating point operations
+   */
+   //TODO @ireneangelucci compute number of flops needed per kernel call, once Functor implementation is completed
+  static unsigned long getNumFlopsPerKernelCall(size_t molAType, size_t molBType, size_t molCType, bool newton3) {
+    return 0;
+  }
+
+  /**
+   * Reset the global values.
+   * Will set the global values to zero to prepare for the next iteration.
+   */
+  void initTraversal() final {
+    _potentialEnergySum = 0.;
+    _virialSum = {0., 0., 0.};
+    _postProcessed = false;
+    for (size_t i = 0; i < _aosThreadData.size(); ++i) {
+      _aosThreadData[i].setZero();
+    }
+  }
+
+  /**
+   * Accumulates global values, e.g. potential energy and virial.
+   * @param newton3
+   */
+  void endTraversal(bool newton3) final {
+    using namespace autopas::utils::ArrayMath::literals;
+
+    if (_postProcessed) {
+      throw autopas::utils::ExceptionHandler::AutoPasException(
+          "Already postprocessed, endTraversal(bool newton3) was called twice without calling initTraversal().");
+    }
+    if (calculateGlobals) {
+      // Accumulate potential energy and virial values.
+      for (size_t i = 0; i < _aosThreadData.size(); ++i) {
+        _potentialEnergySum += _aosThreadData[i].potentialEnergySum;
+        _virialSum += _aosThreadData[i].virialSum;
+      }
+
+      _postProcessed = true;
+
+      AutoPasLog(TRACE, "Final potential energy {}", _potentialEnergySum);
+      AutoPasLog(TRACE, "Final virial           {}", _virialSum[0] + _virialSum[1] + _virialSum[2]);
+    }
+  }
+
+  /**
+   * Get the potential Energy.
+   * @return the potential Energy
+   */
+  double getPotentialEnergy() {
+    if (not calculateGlobals) {
+      throw autopas::utils::ExceptionHandler::AutoPasException(
+          "Trying to get potential energy even though calculateGlobals is false. If you want this functor to calculate "
+          "global "
+          "values, please specify calculateGlobals to be true.");
+    }
+    if (not _postProcessed) {
+      throw autopas::utils::ExceptionHandler::AutoPasException(
+          "Cannot get potential energy, because endTraversal was not called.");
+    }
+    return _potentialEnergySum;
+  }
+
+  /**
+   * Get the virial.
+   * @return
+   */
+  double getVirial() {
+    if (not calculateGlobals) {
+      throw autopas::utils::ExceptionHandler::AutoPasException(
+          "Trying to get virial even though calculateGlobals is false. If you want this functor to calculate global "
+          "values, please specify calculateGlobals to be true.");
+    }
+    if (not _postProcessed) {
+      throw autopas::utils::ExceptionHandler::AutoPasException(
+          "Cannot get virial, because endTraversal was not called.");
+    }
+    return _virialSum[0] + _virialSum[1] + _virialSum[2];
+  }
+
+ private:
+  /**
+   * Internal, actual constructor.
+   * @param cutoff
+   * @note param dummy is unused, only there to make the signature different from the public constructor.
+   */
+  explicit ArgonFunctor(double cutoff, void * /*dummy*/)
+      : autopas::TriwiseFunctor<Particle, ArgonFunctor<Particle, useMixing, useNewton3, calculateGlobals>>(
+            cutoff),
+        _cutoffSquared{cutoff * cutoff},
+        _potentialEnergySum{0.},
+        _virialSum{0., 0., 0.},
+        _aosThreadData(),
+        _postProcessed{false} {
+    if constexpr (calculateGlobals) {
+      _aosThreadData.resize(autopas::autopas_get_max_threads());
+    }
+  }
+
+  template <bool newton3>
+  void SoAFunctorVerletImpl(autopas::SoAView<SoAArraysType> soa, const size_t indexFirst,
+                            const std::vector<size_t, autopas::AlignedAllocator<size_t>> &neighborList) {
+    autopas::utils::ExceptionHandler::exception("ArgonFunctor::SoAFunctorVerletImpl() is not yet implemented.");
+  }
+
+  /**
+   * This class stores internal data of each thread, make sure that this data has proper size, i.e. k*64 Bytes!
+   */
+   //TODO @ireneangelucci Why do we want it to be k*64 Bytes by using __remainingTo64? And not just checking it has k*32 Bytes without storing __remainingTo64?
+  class AoSThreadData {
+   public:
+    AoSThreadData() : virialSum{0., 0., 0.}, potentialEnergySum{0.}, __remainingTo64{} {}
+    void setZero() {
+      virialSum = {0., 0., 0.};
+      potentialEnergySum = 0.;
+    }
+
+    std::array<double, 3> virialSum;
+    double potentialEnergySum;
+
+   private:
+    double __remainingTo64[(64 - 4 * sizeof(double)) / sizeof(double)];
+  };
+  static_assert(sizeof(AoSThreadData) % 64 == 0, "AoSThreadData has wrong size");
+
+  const double _cutoffSquared;
+
+  ParticlePropertiesLibrary<SoAFloatPrecision, size_t> *_PPLibrary = nullptr;
+
+  // sum of the potential energy, only calculated if calculateGlobals is true
+  double _potentialEnergySum;
+
+  // sum of the virial, only calculated if calculateGlobals is true
+  std::array<double, 3> _virialSum;
+
+  // thread buffer for aos
+  std::vector<AoSThreadData> _aosThreadData;
+
+  // defines whether or whether not the global values are already preprocessed
+  bool _postProcessed;
+};
+}  // namespace mdLib
