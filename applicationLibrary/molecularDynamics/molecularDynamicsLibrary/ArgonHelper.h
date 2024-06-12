e27fb07b27bc14f53bd7dd2b02967b6dcd36d860
Irene Angelucci <irene.angelucci@tum.de>
2024-06-12 18:36:59 +0200

    implemented permutation function

--- applicationLibrary/molecularDynamics/molecularDynamicsLibrary/ArgonHelper.h
+++ applicationLibrary/molecularDynamics/molecularDynamicsLibrary/ArgonHelper.h
@@ -13,13 +13,13 @@ namespace autopas::utils::ArrayMath::ArgonMath {
 
 /**
  *
- * @tparam order order of the Legendre polynomial
  * @tparam T floating point type
  * @param x variable
+ * @param order order of the Legendre polynomial
  * @return Lagrange Polynomial of given order
  */
-template <size_t order, class T>
-[[nodiscard]] constexpr T Legendre(const T &x) {
+template <class T>
+[[nodiscard]] constexpr T Legendre(const T &x, const size_t order) {
   if (order > 6) {
     throw ExceptionHandler::AutoPasException(
         "Argon Simulation: asked to compute Legendre polynomial of order higher than 6.");
@@ -37,16 +37,16 @@ template <size_t order, class T>
 
 /**
  *
- * @tparam order order of the Legendre polynomial
  * @tparam T floating point type
  * @param x variable
+ * @param order order of the Legendre polynomial
  * @return derivative w.r.t. x of the Legendre polynomial
  */
-template <size_t order, class T>
-[[nodiscard]] constexpr T derivativeLegendre(const T &x) {
-  auto Legendre_order_minus_1 = (order == 0) ? 0 : Legendre<order - 1>(x);
-  auto num{Legendre_order_minus_1 - Math::safeMul(x, Legendre<order>(x))};
-  return Math::safeMul(order, num) / (1 - Math::pow<2>(x));
+template <class T>
+[[nodiscard]] constexpr T derivativeLegendre(const T& x, size_t order) {
+  auto Legendre_order_minus_1 = (order == 0) ? 0 : Legendre(x, order - 1);
+  auto num{Legendre_order_minus_1 - x * Legendre(x, order)};
+  return order * num / (1 - Math::pow<2>(x));
 }
 
 class Displacement {
@@ -114,5 +114,19 @@ class Cosine {
   double cos_;
   size_t id_;
 };
+
+template <size_t wrt>
+[[nodiscard]] std::array<double, 3> P(const size_t A, const size_t B, const size_t C, const Cosine& cosI, const Cosine& cosJ, const Cosine& cosK) {
+  auto firstTerm = derivativeLegendre(cosI.getCos(), A) * cosI.derive_wrt<wrt>() * Legendre(cosJ.getCos(), B) * Legendre(cosK.getCos(), C);
+  auto secondTerm = Legendre(cosI.getCos(), A) * derivativeLegendre(cosJ.getCos(), B) * cosJ.derive_wrt<wrt>() * Legendre(cosK.getCos(), C);
+  auto thirdTerm = Legendre(cosI.getCos(), A) * Legendre(cosJ.getCos(), B) * derivativeLegendre(cosK.getCos(), C) * cosK.derive_wrt<wrt>();
+  return firstTerm + secondTerm + thirdTerm;
+}
+
+template <size_t wrt>
+[[nodiscard]] std::array<double, 3> Permutation_deriv_wrt(const size_t A, const size_t B, const size_t C, const Cosine& cosI, const Cosine& cosJ, const Cosine& cosK) {
+  return P<wrt>(A, B, C, cosI, cosJ, cosK) + P<wrt>(A, C, B, cosI, cosJ, cosK)  + P<wrt>(B, A, C, cosI, cosJ, cosK)
+      + P<wrt>(B, C, A, cosI, cosJ, cosK) + P<wrt>(C, A, B, cosI, cosJ, cosK) + P<wrt>(C, B, A, cosI, cosJ, cosK);
+}
 
 } // namespace autopas::utils::ArgonMath
