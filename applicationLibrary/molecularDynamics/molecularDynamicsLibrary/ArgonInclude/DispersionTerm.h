80db8039b1ada84d0fec82e3b40853c74281349e
Irene Angelucci <irene.angelucci@tum.de>
2024-07-02 12:15:26 +0200

    implemented dispersion term

---
+++ applicationLibrary/molecularDynamics/molecularDynamicsLibrary/ArgonInclude/DispersionTerm.h
@@ -0,0 +1,66 @@
+/**
+* @file ArgonFunctor.h
+* @author I. Angelucci
+* @date 02/07/24
+*/
+
+#pragma once
+
+#include <cmath>
+
+#include "AngularFunctions.h"
+#include "DampingTerm.h"
+#include "Parameters.h"
+
+namespace autopas::utils::ArrayMath::Argon {
+
+template <size_t a, size_t b, size_t c, size_t ID>
+[[nodiscard]] nabla F_dispersive_abc(const std::array<double, 5> &Z, const std::array<double, 5> &beta,
+                                     DisplacementHandle displacementIJ, DisplacementHandle displacementJK,
+                                     DisplacementHandle displacementKI) {
+
+  const auto Z_abc = Z[mdLib::Argon::index<mdLib::Argon::param::Z>(a, b, c)];
+  const auto beta_abc = beta[mdLib::Argon::index<mdLib::Argon::param::beta>(a, b, c)];
+
+  const auto cosineI = CosineHandle(displacementIJ, displacementKI.getInv());
+  const auto cosineJ = CosineHandle(displacementIJ.getInv(), displacementJK);
+  const auto cosineK = CosineHandle(displacementKI, displacementJK.getInv());
+
+  const auto n1 = a + b + 1;
+  const auto n2 = b + c + 1;
+  const auto n3 = c + a + 1;
+
+  const auto D1 = DampingTerm(beta_abc, displacementIJ, n1);
+  const auto D2 = DampingTerm(beta_abc, displacementJK, n2);
+  const auto D3 = DampingTerm(beta_abc, displacementKI, n3);
+
+  const auto nabla_D1 = DampingTerm_deriv_wrt<ID>(beta_abc, displacementIJ, n1);
+  const auto nabla_D2 = DampingTerm_deriv_wrt<ID>(beta_abc, displacementJK, n2);
+  const auto nabla_D3 = DampingTerm_deriv_wrt<ID>(beta_abc, displacementKI, n3);
+
+  const auto W = AngularTerm<a, b, c>(displacementIJ, displacementJK, displacementKI, cosineI, cosineJ, cosineK);
+
+  const auto nabla_W = AngularTerm_derive_wrt<a, b, c, ID>(displacementIJ, displacementJK, displacementKI, cosineI, cosineJ, cosineK);
+
+  return Z_abc * (W * (nabla_D1 * D2 * D3 + D1 * nabla_D2 * D3 + D1 * D2 * nabla_D3) + D1 * D2 * D3 * nabla_W);
+}
+
+template <size_t ID>
+[[nodiscard]] nabla F_dispersive(const std::array<double, 5> &Z, const std::array<double, 5> &beta,
+                                 DisplacementHandle displacementIJ, DisplacementHandle displacementJK,
+                                 DisplacementHandle displacementKI) {
+  const auto F = F_dispersive_abc<1, 1, 1, ID>(Z, beta, displacementIJ, displacementJK, displacementKI) +
+                 F_dispersive_abc<1, 1, 2, ID>(Z, beta, displacementIJ, displacementJK, displacementKI) +
+                 F_dispersive_abc<1, 2, 1, ID>(Z, beta, displacementIJ, displacementJK, displacementKI) +
+                 F_dispersive_abc<2, 1, 1, ID>(Z, beta, displacementIJ, displacementJK, displacementKI) +
+                 F_dispersive_abc<1, 2, 2, ID>(Z, beta, displacementIJ, displacementJK, displacementKI) +
+                 F_dispersive_abc<2, 1, 2, ID>(Z, beta, displacementIJ, displacementJK, displacementKI) +
+                 F_dispersive_abc<2, 2, 1, ID>(Z, beta, displacementIJ, displacementJK, displacementKI) +
+                 F_dispersive_abc<2, 2, 2, ID>(Z, beta, displacementIJ, displacementJK, displacementKI) +
+                 F_dispersive_abc<1, 1, 3, ID>(Z, beta, displacementIJ, displacementJK, displacementKI) +
+                 F_dispersive_abc<1, 3, 1, ID>(Z, beta, displacementIJ, displacementJK, displacementKI) +
+                 F_dispersive_abc<3, 1, 1, ID>(Z, beta, displacementIJ, displacementJK, displacementKI);
+  return F;
+}
+
+} //  autopas::utils::ArrayMath::Argon
