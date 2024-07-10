40589a76d85b3f69426255b3c0df755211a1823e
Irene Angelucci <irene.angelucci@tum.de>
2024-07-10 14:47:31 +0200

    moved all template implementations in header files

--- applicationLibrary/molecularDynamics/molecularDynamicsLibrary/ArgonInclude/CosineHandle.h
+++ applicationLibrary/molecularDynamics/molecularDynamicsLibrary/ArgonInclude/CosineHandle.h
@@ -45,4 +45,35 @@ class CosineHandle {
   size_t id_;
 };
 
+CosineHandle::CosineHandle(const DisplacementHandle &displacementAB, const DisplacementHandle &displacementAC) {
+  if (displacementAB.getIdStartVertex() != displacementAC.getIdStartVertex()) {
+    throw autopas::utils::ExceptionHandler::AutoPasException("cannot build cosine");
+  }
+  displacementAB_ = displacementAB;
+  displacementAC_ = displacementAC;
+  AB_ = displacementAB_.getDisplacement();
+  AC_ = displacementAC_.getDisplacement();
+  cos_ = ArrayMath::dot(AB_, AC_) / (ArrayMath::L2Norm(AB_) * ArrayMath::L2Norm(AC_));
+  id_ = displacementAB.getIdStartVertex();
+}
+
+template <size_t ID>
+[[nodiscard]] nabla CosineHandle::derive_wrt() const {
+  if (id_ != ID && displacementAB_.getIdEndVertex() != ID && displacementAC_.getIdEndVertex() != ID) {
+    return std::array<double, 3>{{0, 0, 0}};
+  } else if (ID == id_) {
+    auto firstTerm{cos_ / ArrayMath::dot(AB_, AB_) - 1. / ArrayMath::dot(AB_, AC_)};
+    auto secondTerm{cos_ / ArrayMath::dot(AC_, AC_) - 1. / ArrayMath::dot(AB_, AC_)};
+    return AB_ * firstTerm + AC_ * secondTerm;
+  } else if (ID == displacementAB_.getIdEndVertex()) {
+    auto firstTerm{-cos_ / ArrayMath::dot(AB_, AB_)};
+    auto secondTerm{1. / ArrayMath::dot(AB_, AC_)};
+    return AB_ * firstTerm + AC_ * secondTerm;
+  } else if (ID == displacementAC_.getIdEndVertex()) {
+    auto firstTerm{-cos_ / ArrayMath::dot(AC_, AC_)};
+    auto secondTerm{1. / ArrayMath::dot(AB_, AC_)};
+    return AC_ * firstTerm + AB_ * secondTerm;
+  }
+}
+
 }  // namespace autopas::utils::ArrayMath::Argon
