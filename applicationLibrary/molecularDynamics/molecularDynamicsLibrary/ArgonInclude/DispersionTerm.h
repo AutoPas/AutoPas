5b8d3ad2971657ccfca15306a978d1c6314f3b78
Irene Angelucci <irene.angelucci@tum.de>
2024-07-16 18:15:23 +0200

    consistency with main pt 2

--- applicationLibrary/molecularDynamics/molecularDynamicsLibrary/ArgonInclude/DispersionTerm.h
+++ applicationLibrary/molecularDynamics/molecularDynamicsLibrary/ArgonInclude/DispersionTerm.h
@@ -55,7 +55,7 @@ template <size_t a, size_t b, size_t c, size_t ID>
   const auto nabla_W =
       AngularTerm_derive_wrt<a, b, c, ID>(displacementIJ, displacementJK, displacementKI, cosineI, cosineJ, cosineK);
 
-  return ((nabla_D1 * D2 * D3 + nabla_D2 * D1 * D3 + nabla_D3 * D1 * D2) * W + nabla_W * D1 * D2 * D3) * Z_abc;
+  return Z_abc * (W * (nabla_D1 * D2 * D3 + D1 * nabla_D2 * D3 + D1 * D2 * nabla_D3) + D1 * D2 * D3 * nabla_W);
 }
 
 template <size_t a, size_t b, size_t c>
