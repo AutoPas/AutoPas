3e73eeaa21a91a8afb2b52b22f2c902b8c24ff4d
Luis Gall <luis.gall@tum.de>
2024-08-11 10:34:19 +0200

    Added MPI experiment, adjusted benchmarking script

--- examples/md-flexible/input/vectorizationBenchmarking/patternTests/benchmarks.sh
+++ examples/md-flexible/input/vectorizationBenchmarking/patternTests/benchmarks.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-for i in {1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9}
+for i in {0.25,0.50,0.75,1.0,1.25,1.50,1.75,2.0,2.25,2.50,2.75,3.00,3.25,3.50}
 do
-    sed -i "13s/.*/      particle-spacing           :  ${i}/" ./Cubes_hwy_A.yaml
+    sed -i "13s/.*/      particle-spacing           :  ${i}/" ./Cubes_hwy.yaml
     echo "Running Cube experiment with ${i} spacing"
-    OMP_NUM_THREADS=1 ./md-flexible --yaml-filename Cubes_hwy_A.yaml --vectorizationPattern 1xVectorLength --no-progress-bar | tee ./output/benchmark_Cube_${i}_1xVec.txt
-    OMP_NUM_THREADS=1 ./md-flexible --yaml-filename Cubes_hwy_A.yaml --vectorizationPattern 2xVectorLengthDiv2 --no-progress-bar | tee ./output/benchmark_Cube_${i}_2xVecDiv2.txt
-    OMP_NUM_THREADS=1 ./md-flexible --yaml-filename Cubes_hwy_A.yaml --vectorizationPattern VectorLengthDiv2x2 --no-progress-bar | tee ./output/benchmark_Cube_${i}_VecDiv2x2.txt
-    OMP_NUM_THREADS=1 ./md-flexible --yaml-filename Cubes_hwy_A.yaml --vectorizationPattern VectorLengthx1 --no-progress-bar | tee ./output/benchmark_Cube_${i}_Vecx1.txt
+    OMP_NUM_THREADS=1 ./md-flexible --yaml-filename Cubes_hwy.yaml --no-progress-bar | tee ./output/benchmark_Cube_${i}.txt
 done
