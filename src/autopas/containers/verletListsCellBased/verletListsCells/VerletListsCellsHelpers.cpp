2b2fccf87dcb23c22f9f1a6d5d07f69f433b92b9
David Martin <david.martin@in.tum.de>
2024-09-07 15:34:12 +0200

    offsets helper function for c18 base step

--- src/autopas/containers/verletListsCellBased/verletListsCells/VerletListsCellsHelpers.cpp
+++ src/autopas/containers/verletListsCellBased/verletListsCells/VerletListsCellsHelpers.cpp
@@ -86,6 +86,56 @@ std::vector<BaseStepOffsets> buildC08BaseStep(const std::array<int, 3> &cellsPer
   return offsets;
 }
 
+std::vector<BaseStepOffsets> buildC18BaseStep(const std::array<int, 3> &cellsPerDim) {
+  // cellOffset1, cellOffset2, list estimation factor
+  std::vector<BaseStepOffsets> offsets{};
+  // In 3D each base cell has 13 neighboring interaction cells + the interaction with it self in the C18 traversal
+  offsets.reserve(14);
+  // This is currently guaranteed by the VerletListsCells constructor
+  constexpr int interactionCellsPerDim = 1;
+  // We estimate factors for fraction of particles needing a neighbor in the base cell.
+  // Four cases: self (cell interacts with itself), and cells which share a face, edge, or corner with the base cell.
+  // The factors are very conservative and express which portion of the cell could possibly have an interaction partner
+  // in the other cell. Considerations for each factor are:
+  // [0] self: Every particle could find a partner within the cell => 1
+  // [1] face: Only case where a particle could not find a partner is if they are on the opposite cell edge,
+  // which is unlikely => 1
+  // [2] edge: Particles further away from the shared edge than r_i can't find a partner in the other cell.
+  // The volume of the possible region is a quarter of a cylinder of height and radius r_i.
+  // [3] corner: Particles further away from the shared corner than r_i can't find a partner in the other cell.
+  // The volume of the possible region is an eighth of a sphere of radius r_i.
+  constexpr std::array<double, 4> estimatorFactors{1., 1., 1. / 4. * M_PI, 1. / 6. * M_PI};
+  // Outgoing from a base cell with coordinates [0, 0, 0] find all interaction cells for a c18 base step
+  for (int z = 0; z <= interactionCellsPerDim; ++z) {
+    for (int y = z > 0 ? -interactionCellsPerDim : 0; y <= interactionCellsPerDim; ++y) {
+      for (int x = (y > 0 or z > 0) ? -interactionCellsPerDim : 0; x <= interactionCellsPerDim; ++x) {
+        int baseCell = 0;
+        int partnerCell = utils::ThreeDimensionalMapping::threeToOneD(x, y, z, cellsPerDim);
+        // Count number of non-aligned dimensions
+        const auto factor = estimatorFactors[std::abs(x) + std::abs(y) + std::abs(z)];
+        const auto &[smallerIndex, biggerIndex] = std::minmax(baseCell, partnerCell);
+        // Check if this offset tuple is already in the offsets list and if not add it.
+        if (auto tuple = BaseStepOffsets{smallerIndex, biggerIndex, factor};
+            std::find(offsets.begin(), offsets.end(), tuple) == offsets.end()) {
+          offsets.emplace_back(tuple);
+        }
+      }
+    }
+  }
+  // Sort offsets to group processing of the same cells (-> slightly better cache re-usage)
+  std::sort(offsets.begin(), offsets.end(), [](const auto &pair1, const auto &pair2) {
+    const auto &[a1, b1, f1] = pair1;
+    const auto &[a2, b2, f2] = pair2;
+
+    if (a1 == a2) {
+      return b1 < b2;
+    } else {
+      return a1 < a2;
+    }
+  });
+  return offsets;
+}
+
 bool BaseStepOffsets::operator==(const BaseStepOffsets &rhs) const {
   return offset1 == rhs.offset1 && offset2 == rhs.offset2 && listSizeEstimateFactor == rhs.listSizeEstimateFactor;
 }
