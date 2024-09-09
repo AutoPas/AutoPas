0fcadc3050e7806baa94c4b42b545170c8274e07
Markus Mühlhäußer <markus.muehlhaeusser@tum.de>
2024-09-09 12:16:58 +0200

    Set triwise lut to linear interpolating values.

--- applicationLibrary/molecularDynamics/molecularDynamicsLibrary/TriwiseLUT.h
+++ applicationLibrary/molecularDynamics/molecularDynamicsLibrary/TriwiseLUT.h
@@ -8,8 +8,8 @@
 
 #include <array>
 #include <vector>
-#include <cmath>
-#include <algorithm>
+
+#include "autopas/utils/ArrayMath.h"
 
 namespace mdLib {
 
@@ -19,7 +19,7 @@ class TriwiseLUT {
 
   template <class Functor>
   [[nodiscard]] std::pair<const std::array<double, 3>, std::array<u_int8_t, 3>> retrieveValues(const Functor &functor, double dist1, double dist2, double dist3) const {
-
+    using namespace autopas::utils::ArrayMath::literals;
     if (dist1 < _lutCutoff or dist2 < _lutCutoff or dist3 < _lutCutoff) {
       return std::make_pair(functor.getLUTValues(dist1, dist2, dist3), std::array<u_int8_t, 3>({0, 1, 2}));
     }
@@ -28,48 +28,52 @@ class TriwiseLUT {
     std::array<u_int8_t,3> order{};  // Array to track the original indices of the distances
 
     if (dist1 >= dist2 && dist2 >= dist3) {
-      index1 = static_cast<size_t>((dist1 - _lutCutoff) / _resolution);
-      index2 = static_cast<size_t>((dist2 - _lutCutoff) / _resolution);
-      index3 = static_cast<size_t>((dist3 - _lutCutoff) / _resolution);
+      index1 = static_cast<size_t>((dist1 - _lutCutoff) * _resolution);
+      index2 = static_cast<size_t>((dist2 - _lutCutoff) * _resolution);
+      index3 = static_cast<size_t>((dist3 - _lutCutoff) * _resolution);
       order[0] = 0; order[1] = 1; order[2] = 2;
     } else if (dist1 >= dist3 && dist3 >= dist2) {
-      index1 = static_cast<size_t>((dist1 - _lutCutoff) / _resolution);
-      index2 = static_cast<size_t>((dist3 - _lutCutoff) / _resolution);
-      index3 = static_cast<size_t>((dist2 - _lutCutoff) / _resolution);
+      index1 = static_cast<size_t>((dist1 - _lutCutoff) * _resolution);
+      index2 = static_cast<size_t>((dist3 - _lutCutoff) * _resolution);
+      index3 = static_cast<size_t>((dist2 - _lutCutoff) * _resolution);
       order[0] = 0; order[1] = 2; order[2] = 1;
     } else if (dist2 >= dist1 && dist1 >= dist3) {
-      index1 = static_cast<size_t>((dist2 - _lutCutoff) / _resolution);
-      index2 = static_cast<size_t>((dist1 - _lutCutoff) / _resolution);
-      index3 = static_cast<size_t>((dist3 - _lutCutoff) / _resolution);
+      index1 = static_cast<size_t>((dist2 - _lutCutoff) * _resolution);
+      index2 = static_cast<size_t>((dist1 - _lutCutoff) * _resolution);
+      index3 = static_cast<size_t>((dist3 - _lutCutoff) * _resolution);
       order[0] = 1; order[1] = 0; order[2] = 2;
     } else if (dist2 >= dist3 && dist3 >= dist1) {
-      index1 = static_cast<size_t>((dist2 - _lutCutoff) / _resolution);
-      index2 = static_cast<size_t>((dist3 - _lutCutoff) / _resolution);
-      index3 = static_cast<size_t>((dist1 - _lutCutoff) / _resolution);
+      index1 = static_cast<size_t>((dist2 - _lutCutoff) * _resolution);
+      index2 = static_cast<size_t>((dist3 - _lutCutoff) * _resolution);
+      index3 = static_cast<size_t>((dist1 - _lutCutoff) * _resolution);
       order[0] = 1; order[1] = 2; order[2] = 0;
     } else if (dist3 >= dist1 && dist1 >= dist2) {
-      index1 = static_cast<size_t>((dist3 - _lutCutoff) / _resolution);
-      index2 = static_cast<size_t>((dist1 - _lutCutoff) / _resolution);
-      index3 = static_cast<size_t>((dist2 - _lutCutoff) / _resolution);
+      index1 = static_cast<size_t>((dist3 - _lutCutoff) * _resolution);
+      index2 = static_cast<size_t>((dist1 - _lutCutoff) * _resolution);
+      index3 = static_cast<size_t>((dist2 - _lutCutoff) * _resolution);
       order[0] = 2; order[1] = 0; order[2] = 1;
     } else {
-      index1 = static_cast<size_t>((dist3 - _lutCutoff) / _resolution);
-      index2 = static_cast<size_t>((dist2 - _lutCutoff) / _resolution);
-      index3 = static_cast<size_t>((dist1 - _lutCutoff) / _resolution);
+      index1 = static_cast<size_t>((dist3 - _lutCutoff) * _resolution);
+      index2 = static_cast<size_t>((dist2 - _lutCutoff) * _resolution);
+      index3 = static_cast<size_t>((dist1 - _lutCutoff) * _resolution);
       order[0] = 2; order[1] = 1; order[2] = 0;
     }
 
-    // Retrieve values from the LUT
-    std::pair<const std::array<double, 3>, std::array<u_int8_t, 3>> result = std::make_pair(_lut[index1][index2][index3], order);
+    // Retrieve values from the LUT by linear interpolation
+    auto resultLeft = _lut[index1][index2][index3];
+    auto resultRight = _lut[index1+1][index2+1][index3+1];
 
-    return result;
+    auto result = (resultRight + resultLeft) * 0.5;
+    auto resultPair = std::make_pair(result, order);
+
+    return resultPair;
   }
 
   template<class Functor>
   void fill(const Functor &functor, double cutoffSquared, bool forceRefill = false) {
     if (not _isFilled or forceRefill) {
       _cutoffSquared = cutoffSquared;
-      _lutCutoff = cutoffSquared / 10.;
+      _lutCutoff = cutoffSquared / 20.;
       double lutDistance = _cutoffSquared - _lutCutoff;
       double pointDistance = lutDistance / static_cast<double>(_resolution);
 
