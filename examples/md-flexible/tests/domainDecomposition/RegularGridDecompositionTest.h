8afb730a2710dea64a3eade9bb1502549a74b327
Markus Mühlhäußer <markus.muehlhaeusser@tum.de>
2024-11-08 15:58:36 +0100

    Update haloExchange test for non-periodic BCs

--- examples/md-flexible/tests/domainDecomposition/RegularGridDecompositionTest.h
+++ examples/md-flexible/tests/domainDecomposition/RegularGridDecompositionTest.h
@@ -8,11 +8,13 @@
 #include <gtest/gtest.h>
 
 #include "AutoPasTestBase.h"
+#include "src/options/BoundaryTypeOption.h"
 
 /**
  * Test class for the RegularGridDecomposition domain decomposition class.
  */
-class RegularGridDecompositionTest : public AutoPasTestBase {
+class RegularGridDecompositionTest : public AutoPasTestBase,
+                                      public ::testing::WithParamInterface<options::BoundaryTypeOption> {
  public:
   /**
    * Constructor.
