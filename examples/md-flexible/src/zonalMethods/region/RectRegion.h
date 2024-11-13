feda542cccaf18b696d1e855fadc918e3d950c7f
noahpy <noschl@proton.me>
2024-11-13 14:53:22 +0900

    add normalization, << and == operator for Region class

--- examples/md-flexible/src/zonalMethods/region/RectRegion.h
+++ examples/md-flexible/src/zonalMethods/region/RectRegion.h
@@ -9,8 +9,7 @@
  * This class represents the geographic regions of a import / export zone of a zonal method.
  * The described zone is a rectangular parallelepiped.
  * */
-class RectRegion : Region {
-
+class RectRegion : public Region {
  public:
   /**
    * Stores the origin of the zone.
@@ -22,15 +21,13 @@ class RectRegion : Region {
    * */
   std::array<double, 3> _size;
 
-
   /**
    * Constructor
    * @param origin
    * @param size
    * @param zoneID
    */
-  RectRegion(std::array<double, 3> origin, std::array<double, 3> size, char zoneID = 0)
-      : _origin(origin), _size(size), Region(zoneID) {}
+  RectRegion(std::array<double, 3> origin, std::array<double, 3> size, char zoneID = 0);
 
   /**
    * Constructor
@@ -43,4 +40,25 @@ class RectRegion : Region {
    * @param buffer
    */
   void collectParticles(AutoPasType &autoPasContainer, std::vector<ParticleType> &buffer) override;
+
+  /**
+   * Equality operator
+   * @param other The other RectRegion to compare with.
+   * @return true if the origins and sizes of both RectRegions are equal.
+   */
+  bool operator==(const RectRegion &other) const;
+
+  /**
+   * Normalizes the origin and size of the region such that all components of
+   * the size vector are positive,
+   * without changing the specified region.
+   * */
+  void normalize();
 };
+
+#include "autopas/utils/ArrayUtils.h"
+using namespace autopas::utils::ArrayUtils;
+
+inline std::ostream &operator<<(std::ostream &strm, const RectRegion &r) {
+  return strm << "Region( origin: " << to_string(r._origin) << " | size: " << to_string(r._size) << " )";
+}
