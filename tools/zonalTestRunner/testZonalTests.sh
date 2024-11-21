32eb7f4ae0d1bd5f048949f7a06037cbc5f3ef00
noahpy <noschl@proton.me>
2024-11-21 23:04:34 +0900

    add testZonalTest tool script

---
+++ tools/zonalTestRunner/testZonalTests.sh
@@ -0,0 +1,11 @@
+
+# Build and run all relevant tests for this thesis
+# Run this from the build folder
+
+cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DMD_FLEXIBLE_USE_MPI=ON
+make mdFlexTests -j 5
+
+ctest -R mdFlexTests/ZonalMethodTest
+ctest -R mdFlexTests/HalfShellTest
+ctest -R mdFlexTests/FullShellTest
+ctest -R mdFlexTests/RegionTest
