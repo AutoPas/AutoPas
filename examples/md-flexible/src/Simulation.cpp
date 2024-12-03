8b8dd893d29e6975a903217dc21ea9f5c7404b0f
joonkim <148557609+rlawnsiiiii@users.noreply.github.com>
2024-12-03 14:25:14 +0100

    rotate clockwise instead of anti-clockwise

--- examples/md-flexible/src/Simulation.cpp
+++ examples/md-flexible/src/Simulation.cpp
@@ -202,10 +202,10 @@ void Simulation::run() {
     }
 
     _timers.computationalLoad.start();
-    const size_t rotationalGlobalForceIterationFrom = 100000;
+    const size_t rotationalGlobalForceIterationFrom = 750000;
     if (_configuration.deltaT.value != 0 and not _simulationIsPaused) {
       const std::array<double, 3> globalForce = calculateRotationalGlobalForce(
-          _configuration.globalForce.value, 9.8, M_PI/4., rotationalGlobalForceIterationFrom);  // TODO: precalculate the global force magnitude
+          _configuration.globalForce.value, -9.8, M_PI/8., rotationalGlobalForceIterationFrom);  // TODO: precalculate the global force magnitude
       updatePositionsAndResetForces(globalForce);  // normal case parameter: _configuration.globalForce.value
 #if MD_FLEXIBLE_MODE == MULTISITE
       updateQuaternions();
