cf9adbf1ca636fa3b40284414615d2d9fac8f2c6
noahpy <noschl@proton.me>
2024-12-19 16:01:12 +0900

    MidpointTest: test Schedule calculation

---
+++ examples/md-flexible/tests/zonalMethods/MidpointTest.h
@@ -0,0 +1,47 @@
+
+
+
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include "AutoPasTestBase.h"
+#include "autopas/AutoPasDecl.h"
+#include "autopas/utils/ArrayMath.h"
+#include "src/TypeDefinitions.h"
+#include "src/zonalMethods/Midpoint.h"
+#include "src/zonalMethods/region/RectRegion.h"
+
+using namespace autopas::utils::ArrayMath::literals;
+
+/**
+ * Test class for the Zone classes
+ */
+class MidpointTest : public AutoPasTestBase, public Midpoint {
+ public:
+  using AutoPasType = autopas::AutoPas<ParticleType>;
+
+  /**
+   * Constructor.
+   */
+  MidpointTest()
+      : AutoPasTestBase(),
+        Midpoint(_cutoff, _verletSkinWidth, 0, _homeBoxRegion, _homeBoxRegion, autopas::AutoPas_MPI_Comm(),
+                  _allNeighbourIndices)
+
+  {}
+
+ protected:
+  // initialize autopas container
+  void initContainer(AutoPasType &autopas, std::vector<ParticleType> particles);
+
+  AutoPasType _autopas;
+
+  constexpr static double _cutoff = 0.9;
+  constexpr static double _verletSkinWidth = 0.1;
+  constexpr static std::array<double, 3> _boxMin = {0, 0, 0};
+  constexpr static std::array<double, 3> _boxMax = {10, 10, 10};
+  constexpr static std::array<int, 26> _allNeighbourIndices = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                                               0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  inline const static RectRegion _homeBoxRegion{_boxMin, _boxMax - _boxMin};
+};
