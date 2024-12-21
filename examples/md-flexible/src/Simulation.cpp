93b80b0800f7265a6b704bb306bcfdc547c7b555
joonkim <148557609+rlawnsiiiii@users.noreply.github.com>
2024-12-21 10:49:05 +0100

    manually set stats-write-frequency

--- examples/md-flexible/src/Simulation.cpp
+++ examples/md-flexible/src/Simulation.cpp
@@ -193,7 +193,7 @@ void Simulation::run() {
   double finalBoxMaxY = _configuration.boxMax.value[1];
   double startBoxMaxX = _configuration.boxMax.value[0];
   double startBoxMaxY = _configuration.boxMax.value[1];
-  const size_t statsWriteFrequency = _configuration.vtkWriteFrequency.value / 100;
+  const size_t statsWriteFrequency = 1000; // _configuration.vtkWriteFrequency.value / 100;
   const size_t strainResizingStartingIteration = 30000;
   const double spring_stiffness = 50;
   const double pressure = 40;
