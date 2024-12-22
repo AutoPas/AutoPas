101ae687ff59b1afc3b00f1dbc2509928e8b0e1e
joonkim <148557609+rlawnsiiiii@users.noreply.github.com>
2024-12-22 22:13:22 +0100

    make pressure 0.5

--- examples/md-flexible/src/Simulation.cpp
+++ examples/md-flexible/src/Simulation.cpp
@@ -196,7 +196,7 @@ void Simulation::run() {
   const size_t statsWriteFrequency = 1000; // _configuration.vtkWriteFrequency.value / 100;
   const size_t strainResizingStartingIteration = 100000;
   const double spring_stiffness = 50;
-  const double pressure = 0.1;
+  const double pressure = 0.5;
   const double damping_coeff = 1;
 
   while (needsMoreIterations()) {
