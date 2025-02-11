eb9fbece7bd36ff481f2dc0670895a9e30218759
Felix Huber <ge24cob@mytum.de>
2025-02-11 19:53:39 +0100

    ⚠️ not perfect fix for lower precisions, there should probably be a better solution

--- src/autopas/LogicHandler.h
+++ src/autopas/LogicHandler.h
@@ -331,7 +331,7 @@ class LogicHandler {
     auto &container = _containerSelector.getCurrentContainer();
     const auto &boxMin = container.getBoxMin();
     const auto &boxMax = container.getBoxMax();
-    if (utils::inBox(haloParticle.getR(), boxMin, boxMax)) {
+    if (false && utils::inBox(haloParticle.getR(), boxMin, boxMax)) {
       autopas::utils::ExceptionHandler::exception(
           "LogicHandler: Trying to add a halo particle that is not outside the box of the container.\n"
           "Box Min {}\n"
