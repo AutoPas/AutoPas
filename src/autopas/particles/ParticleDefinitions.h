fe39d53813e70360fcbcf2a949a40659111cafca
Samuel Newcome <samuel.newcome@tum.de>
2025-03-20 17:33:24 +0100

    Removing `ParticleBaseFP32` alias as it doesn't work anyway.

--- src/autopas/particles/ParticleDefinitions.h
+++ src/autopas/particles/ParticleDefinitions.h
@@ -10,11 +10,6 @@
 #include "autopas/particles/ParticleBase.h"
 
 namespace autopas {
-
-/**
- * Particle with all variables in 32 bit precision
- */
-using ParticleBaseFP32 = ParticleBase<float, unsigned long>;
 /**
  * Particle with all variables in 64 bit precision
  */
