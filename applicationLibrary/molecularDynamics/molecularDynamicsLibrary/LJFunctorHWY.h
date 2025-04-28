e41407660e2a891d4c9137568b46d476ece1dcec
Johannes <johannes.rief@tum.de>
2025-04-28 16:15:47 +0200

    First test commit. Small changes in LJFunctorHWY.h in function SoAFunctorPair.

--- applicationLibrary/molecularDynamics/molecularDynamicsLibrary/LJFunctorHWY.h
+++ applicationLibrary/molecularDynamics/molecularDynamicsLibrary/LJFunctorHWY.h
@@ -206,6 +206,12 @@ class LJFunctorHWY
   // clang-format on
   inline void SoAFunctorPair(autopas::SoAView<SoAArraysType> soa1, autopas::SoAView<SoAArraysType> soa2,
                              bool newton3) final {
+    // first attempt at pattern selection
+    size_t length1 = soa1.getLength();
+    size_t length2 = soa2.getLength();
+
+    setVecPattern(VectorizationPattern::p1xVec);
+
     switch (_vecPattern) {
       case VectorizationPattern::p1xVec: {
         if (newton3) {
