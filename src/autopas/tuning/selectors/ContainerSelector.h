40b4548f55e3c02bf57507a8bf70ff96de0f220b
Markus Muehlhaeusser <markus.muehlhaeusser@tum.de>
2025-05-22 15:40:18 +0200

    Reduce ContainerSelector.h to just be a factory

--- src/autopas/tuning/selectors/ContainerSelector.h
+++ src/autopas/tuning/selectors/ContainerSelector.h
@@ -7,9 +7,7 @@
 #pragma once
 
 #include <array>
-#include <vector>
 
-#include "autopas/containers/CellBasedParticleContainer.h"
 #include "autopas/containers/directSum/DirectSum.h"
 #include "autopas/containers/linkedCells/LinkedCells.h"
 #include "autopas/containers/linkedCells/LinkedCellsReferences.h"
@@ -22,19 +20,13 @@
 #include "autopas/containers/verletListsCellBased/verletListsCells/VerletListsCellsHelpers.h"
 #include "autopas/options/ContainerOption.h"
 #include "autopas/tuning/selectors/ContainerSelectorInfo.h"
-#include "autopas/utils/NumParticlesEstimator.h"
-#include "autopas/utils/StringUtils.h"
 
 namespace autopas {
 
 /**
  * Selector for a particle container.
  *
- * The class is given a list of allowed container and traversal options to choose from.
- * This class selects the optimal container and delegates the choice of the optimal traversal down to this container.
- *
  * @tparam Particle_T
- * @tparam ParticleCell
  */
 template <class Particle_T>
 class ContainerSelector {
@@ -46,59 +38,27 @@ class ContainerSelector {
    * @param cutoff Cutoff radius to be used in this container.
    */
   ContainerSelector(const std::array<double, 3> &boxMin, const std::array<double, 3> &boxMax, double cutoff)
-      : _boxMin(boxMin), _boxMax(boxMax), _cutoff(cutoff), _currentContainer(nullptr), _currentInfo() {}
+      : _boxMin(boxMin), _boxMax(boxMax), _cutoff(cutoff) {}
 
   /**
-   * Sets the container to the given option.
-   * @param containerOption container to generate
-   * @param containerInfo additional parameter for the container
-   */
-  void selectContainer(ContainerOption containerOption, ContainerSelectorInfo containerInfo);
-
-  /**
-   * Set new  boundaries, convert the container and reevaluate particle ownership.
-   * @param boxMin
-   * @param boxMax
-   */
-  void resizeBox(const std::array<double, 3> &boxMin, const std::array<double, 3> &boxMax) {
-    _boxMin = boxMin;
-    _boxMax = boxMax;
-
-    _currentContainer = std::move(generateContainer(_currentContainer->getContainerType(), _currentInfo));
-  }
-
-  /**
-   * Getter for the optimal container. If no container is chosen yet the first allowed is selected.
-   * @return Reference to the optimal container.
-   */
-  inline autopas::ParticleContainerInterface<Particle_T> &getCurrentContainer();
-
-  /**
-   * Getter for the optimal container. If no container is chosen yet the first allowed is selected.
-   * @return Reference to the optimal container.
-   */
-  inline const autopas::ParticleContainerInterface<Particle_T> &getCurrentContainer() const;
-
- private:
-  /**
-   * Container factory that also copies all particles to the new container
+   * Container factory method.
    * @param containerChoice container to generate
    * @param containerInfo additional parameter for the container
-   * @return Smartpointer to new container
+   * @return Smartpointer to the new container
    */
-  std::unique_ptr<autopas::ParticleContainerInterface<Particle_T>> generateContainer(
-      ContainerOption containerChoice, ContainerSelectorInfo containerInfo);
+  std::unique_ptr<ParticleContainerInterface<Particle_T>> generateContainer(ContainerOption containerChoice,
+                                                                            ContainerSelectorInfo containerInfo);
 
+ private:
   std::array<double, 3> _boxMin, _boxMax;
   const double _cutoff;
-  std::unique_ptr<autopas::ParticleContainerInterface<Particle_T>> _currentContainer;
-  ContainerSelectorInfo _currentInfo;
+  ContainerSelectorInfo _currentInfo{};
 };
 
 template <class Particle_T>
-std::unique_ptr<autopas::ParticleContainerInterface<Particle_T>> ContainerSelector<Particle_T>::generateContainer(
+std::unique_ptr<ParticleContainerInterface<Particle_T>> ContainerSelector<Particle_T>::generateContainer(
     ContainerOption containerChoice, ContainerSelectorInfo containerInfo) {
-  std::unique_ptr<autopas::ParticleContainerInterface<Particle_T>> container;
+  std::unique_ptr<ParticleContainerInterface<Particle_T>> container;
   switch (containerChoice) {
     case ContainerOption::directSum: {
       container = std::make_unique<DirectSum<Particle_T>>(_boxMin, _boxMax, _cutoff, containerInfo.verletSkin,
@@ -161,55 +121,6 @@ std::unique_ptr<autopas::ParticleContainerInterface<Particle_T>> ContainerSelect
     }
   }
 
-  // copy particles so they do not get lost when container is switched
-  if (_currentContainer != nullptr) {
-    // with these assumptions slightly more space is reserved as numParticlesTotal already includes halos
-    const auto numParticlesTotal = _currentContainer->size();
-    const auto numParticlesHalo = autopas::utils::NumParticlesEstimator::estimateNumHalosUniform(
-        numParticlesTotal, _currentContainer->getBoxMin(), _currentContainer->getBoxMax(),
-        _currentContainer->getInteractionLength());
-
-    container->reserve(numParticlesTotal, numParticlesHalo);
-    for (auto particleIter = _currentContainer->begin(IteratorBehavior::ownedOrHalo); particleIter.isValid();
-         ++particleIter) {
-      // add particle as inner if it is owned
-      if (particleIter->isOwned()) {
-        container->addParticle(*particleIter);
-      } else {
-        container->addHaloParticle(*particleIter);
-      }
-    }
-  }
-
   return container;
-}
-
-template <class Particle_T>
-autopas::ParticleContainerInterface<Particle_T> &ContainerSelector<Particle_T>::getCurrentContainer() {
-  if (_currentContainer == nullptr) {
-    autopas::utils::ExceptionHandler::exception(
-        "ContainerSelector: getCurrentContainer() called before any container was selected!");
-  }
-  return *_currentContainer;
-}
-
-template <class Particle_T>
-const autopas::ParticleContainerInterface<Particle_T> &ContainerSelector<Particle_T>::getCurrentContainer() const {
-  if (_currentContainer == nullptr) {
-    autopas::utils::ExceptionHandler::exception(
-        "ContainerSelector: getCurrentContainer() called before any container was selected!");
-  }
-  return *_currentContainer;
-}
-
-template <class Particle_T>
-void ContainerSelector<Particle_T>::selectContainer(ContainerOption containerOption,
-                                                    ContainerSelectorInfo containerInfo) {
-  // Only do something if we have no container, a new type is required, or the info changed
-  if (_currentContainer == nullptr or _currentContainer->getContainerType() != containerOption or
-      _currentInfo != containerInfo) {
-    _currentContainer = std::move(generateContainer(containerOption, containerInfo));
-    _currentInfo = containerInfo;
-  }
 }
 }  // namespace autopas
