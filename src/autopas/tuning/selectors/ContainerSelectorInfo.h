43d3a5616820ef60384e8b8f68d40bc3ed81f505
Markus Muehlhaeusser <markus.muehlhaeusser@tum.de>
2025-05-22 15:59:51 +0200

    Add boxMin, boxMax and cutoff to ContainerSelectorInfo

--- src/autopas/tuning/selectors/ContainerSelectorInfo.h
+++ src/autopas/tuning/selectors/ContainerSelectorInfo.h
@@ -20,14 +20,20 @@ class ContainerSelectorInfo {
    * Default Constructor.
    */
   ContainerSelectorInfo()
-      : cellSizeFactor(1.),
+      : boxMin({0., 0., 0.}),
+        boxMax({1., 1., 1.}),
+        cutoff(1.),
+        cellSizeFactor(1.),
         verletSkin(0.),
         verletRebuildFrequency(0),
         verletClusterSize(64),
-        loadEstimator(autopas::LoadEstimatorOption::none) {}
+        loadEstimator(LoadEstimatorOption::none) {}
 
   /**
    * Constructor.
+   * @param boxMin Lower corner of the container.
+   * @param boxMax Upper corner of the container.
+   * @param cutoff Cutoff radius to be used in this container.
    * @param cellSizeFactor Cell size factor to be used in this container (only relevant for LinkedCells, VerletLists and
    * VerletListsCells).
    * @param verletSkin Length added to the cutoff for the verlet lists' skin per timestep inbetween
@@ -36,9 +42,12 @@ class ContainerSelectorInfo {
    * @param verletClusterSize Size of verlet Clusters
    * @param loadEstimator load estimation algorithm for balanced traversals.
    */
-  explicit ContainerSelectorInfo(double cellSizeFactor, double verletSkin, unsigned int verletRebuildFrequency,
-                                 unsigned int verletClusterSize, autopas::LoadEstimatorOption loadEstimator)
-      : cellSizeFactor(cellSizeFactor),
+  explicit ContainerSelectorInfo(const std::array<double, 3> &boxMin, const std::array<double, 3> &boxMax, double cutoff, double cellSizeFactor, double verletSkin, unsigned int verletRebuildFrequency,
+                                 unsigned int verletClusterSize, LoadEstimatorOption loadEstimator)
+      : boxMin(boxMin),
+        boxMax(boxMax),
+        cutoff(cutoff),
+        cellSizeFactor(cellSizeFactor),
         verletSkin(verletSkin),
         verletRebuildFrequency(verletRebuildFrequency),
         verletClusterSize(verletClusterSize),
@@ -47,7 +56,7 @@ class ContainerSelectorInfo {
   /**
    * Equality between ContainerSelectorInfo
    * @param other
-   * @return True iff all member equal
+   * @return True iff all members equal
    */
   bool operator==(const ContainerSelectorInfo &other) const {
     return cellSizeFactor == other.cellSizeFactor and verletSkin == other.verletSkin and
@@ -57,14 +66,14 @@ class ContainerSelectorInfo {
   /**
    * Inequality between ContainerSelectorInfo
    * @param other
-   * @return False iff all member euqal
+   * @return False iff all members equal
    */
   bool operator!=(const ContainerSelectorInfo &other) const { return !(*this == other); }
 
   /**
    * Comparison operator for ContainerSelectorInfo objects.
    * Configurations are compared member wise in the order: _cellSizeFactor, _verletSkin,
-   * _verlerRebuildFrequency, loadEstimator
+   * _verletRebuildFrequency, loadEstimator
    *
    * @param other
    * @return
@@ -74,6 +83,21 @@ class ContainerSelectorInfo {
            std::tie(other.cellSizeFactor, other.verletSkin, other.verletRebuildFrequency, other.verletClusterSize,
                     other.loadEstimator);
   }
+
+  /**
+   * Lower corner of the container.
+   */
+  std::array<double, 3> boxMin;
+
+  /**
+   * Upper corner of the container.
+   */
+  std::array<double, 3> boxMax;
+
+  /**
+   * Cutoff radius to be used in this container.
+   */
+  const double cutoff;
 
   /**
    * cellSizeFactor Cell size factor to be used in this container (only relevant for LinkedCells)
@@ -94,7 +118,7 @@ class ContainerSelectorInfo {
   /**
    * Load estimator for balanced sliced traversals.
    */
-  autopas::LoadEstimatorOption loadEstimator;
+  LoadEstimatorOption loadEstimator;
 };
 
 }  // namespace autopas
