3f6a7ac19d9db90246dace235e4e8734385bac0d
Ash_Ubuntu <ge95pab@tum.de>
2025-07-02 14:41:37 +0200

    feature: changed MPI communication pattern to use non-blocking receives and remove iProbe

--- examples/md-flexible/src/domainDecomposition/RegularGridDecomposition.cpp
+++ examples/md-flexible/src/domainDecomposition/RegularGridDecomposition.cpp
@@ -229,7 +229,7 @@ void RegularGridDecomposition::exchangeHaloParticles(AutoPasType &autoPasContain
     // clear the buffer as it will hold the newly received halo particles.
     _receivedParticlesBuffer.clear();
     sendAndReceiveParticlesLeftAndRight(_particlesForLeftNeighbor, _particlesForRightNeighbor, _receivedParticlesBuffer,
-                                        leftNeighbor, rightNeighbor);
+                                        leftNeighbor, rightNeighbor, dimensionIndex);
     _haloParticles.insert(_haloParticles.end(), _receivedParticlesBuffer.begin(), _receivedParticlesBuffer.end());
   }
   autoPasContainer.addHaloParticles(_haloParticles);
@@ -263,7 +263,7 @@ void RegularGridDecomposition::exchangeMigratingParticles(AutoPasType &autoPasCo
       // clear the buffer as it will hold the newly received immigrants
       _receivedParticlesBuffer.clear();
       sendAndReceiveParticlesLeftAndRight(_particlesForLeftNeighbor, _particlesForRightNeighbor,
-                                          _receivedParticlesBuffer, leftNeighbor, rightNeighbor);
+                                          _receivedParticlesBuffer, leftNeighbor, rightNeighbor, dimensionIndex);
       // custom openmp reduction to concatenate all local vectors to one at the end of a parallel region
       AUTOPAS_OPENMP(declare reduction(vecMergeParticle :                                                 \
                                        std::remove_reference_t<decltype(emigrants)> :                     \
@@ -458,18 +458,21 @@ void RegularGridDecomposition::reflectParticlesAtBoundaries(AutoPasType &autoPas
 void RegularGridDecomposition::sendAndReceiveParticlesLeftAndRight(const std::vector<ParticleType> &particlesToLeft,
                                                                    const std::vector<ParticleType> &particlesToRight,
                                                                    std::vector<ParticleType> &receivedParticlesBuffer,
-                                                                   int leftNeighbor, int rightNeighbor) {
+                                                                   int leftNeighbor, int rightNeighbor, int dimensionIndex) {
   // only actually send / receive if we are not sending / receiving to ourselves
   if (_mpiCommunicationNeeded and leftNeighbor != _domainIndex) {
     ParticleCommunicator particleCommunicator(_communicator);
 
-    particleCommunicator.sendParticles(particlesToLeft, leftNeighbor);
-    particleCommunicator.sendParticles(particlesToRight, rightNeighbor);
+    // Old implementation: keep for reference
+    // particleCommunicator.sendParticles(particlesToLeft, leftNeighbor);
+    // particleCommunicator.sendParticles(particlesToRight, rightNeighbor);
 
-    particleCommunicator.receiveParticles(receivedParticlesBuffer, leftNeighbor);
-    particleCommunicator.receiveParticles(receivedParticlesBuffer, rightNeighbor);
+    // particleCommunicator.receiveParticles(receivedParticlesBuffer, leftNeighbor);
+    // particleCommunicator.receiveParticles(receivedParticlesBuffer, rightNeighbor);
 
-    particleCommunicator.waitForSendRequests();
+    // particleCommunicator.waitForSendRequests();
+    particleCommunicator.sendAndReceiveParticlesLeftAndRight(particlesToLeft, particlesToRight, receivedParticlesBuffer,
+                                                            leftNeighbor, rightNeighbor, dimensionIndex);
   } else {
     receivedParticlesBuffer.insert(receivedParticlesBuffer.end(), particlesToLeft.begin(), particlesToLeft.end());
     receivedParticlesBuffer.insert(receivedParticlesBuffer.end(), particlesToRight.begin(), particlesToRight.end());
