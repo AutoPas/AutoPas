311c0e152b42c28f97a1ee4b603374ad59b965a7
Markus Mühlhäußer <markus.muehlhaeusser@tum.de>
2025-10-28 21:41:49 +0100

    Remove lcc08 3b optimized as option

--- src/autopas/tuning/selectors/TraversalSelector.h
+++ src/autopas/tuning/selectors/TraversalSelector.h
@@ -385,11 +385,6 @@ std::unique_ptr<TraversalInterface> TraversalSelector::generateTriwiseTraversal(
           dataLayout, useNewton3);
       break;
     }
-    // case TraversalOption::lc_c08_opt: {
-    //   return std::make_unique<LCC08Traversal3BOptimized<ParticleCell_T, TriwiseFunctor_T>>(
-    //       traversalInfo.cellsPerDim, &triwiseFunctor, traversalInfo.interactionLength, traversalInfo.cellLength,
-    //       dataLayout, useNewton3);
-    // }
     case TraversalOption::lc_c04: {
       return std::make_unique<LCC04Traversal<ParticleCell_T, TriwiseFunctor_T>>(
           traversalInfo.cellsPerDim, &triwiseFunctor, traversalInfo.interactionLength, traversalInfo.cellLength,
