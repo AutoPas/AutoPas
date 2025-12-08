825696e5013f5f042360b75c92e3053c711370c4
David Martin <david.martin@in.tum.de>
2025-12-08 13:51:37 +0100

    cleanup input yamls

--- examples/md-flexible/input/vectorizationBenchmarking/patternTests/benchmarks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {0.25,0.50,0.75,1.0,1.25,1.50}
-do
-    sed -i "14s/.*/      particle-spacing           :  ${i}/" ./Cubes_hwy.yaml
-    echo "Running Cube experiment with ${i} spacing"
-    OMP_NUM_THREADS=1 ./md-flexible --yaml-filename Cubes_hwy.yaml --no-progress-bar | tee ./output/benchmark_Cube_${i}.txt
-done
