188e573ceb33a0621b788b454d646b9b1d097270
Luis Gall <luis.gall@tum.de>
2025-12-17 13:55:43 +0100

    fixed data layout conversion

--- src/autopas/utils/KokkosDataLayoutConverter.h
+++ src/autopas/utils/KokkosDataLayoutConverter.h
@@ -17,21 +17,23 @@ class KokkosDataLayoutConverter {
   explicit KokkosDataLayoutConverter() {}
 
   template <class Input, class Output, std::size_t... I>
-  void convertToSoA(Input &srcParticles, Output &dstParticles, size_t numParticles, std::index_sequence<I...>) {
+  void convertToSoA(Input &srcParticles, Output &dstParticles, size_t numParticles, std::index_sequence<I...> seq) {
 
     // AoS to SoA
-
     for (size_t i = 0; i < numParticles; ++i) {
-      ((dstParticles. template operator()<I, false>(i) = srcParticles.getParticle(i).template get<static_cast<Particle_T::AttributeNames>(I+1)>()), ...); // I+1 as KokkosSoA does not contain ptr
+      ((dstParticles. template operator()<I, false, true>(i) = srcParticles.getParticle(i).template get<static_cast<Particle_T::AttributeNames>(I+1)>()), ...); // I+1 as KokkosSoA does not contain ptr
     }
+
+    dstParticles.template markModified<Kokkos::HostSpace>(seq);
   }
 
   template <class Input, class Output, std::size_t... I>
-  void convertToAoS(Input &srcParticles, Output &dstParticles, size_t numParticles, std::index_sequence<I...>) {
+  void convertToAoS(Input &srcParticles, Output &dstParticles, size_t numParticles, std::index_sequence<I...> seq) {
 
+    srcParticles.template sync<Kokkos::HostSpace>(seq);
     // SoA to AoS
     for (size_t i = 0; i < numParticles; ++i) {
-      (dstParticles.getParticle(i). template set<static_cast<Particle_T::AttributeNames>(I+1)>(srcParticles. template operator()<I, false>(i)), ...); // I+1 as KokkosSoA does not contain ptr
+      (dstParticles.getParticle(i). template set<static_cast<Particle_T::AttributeNames>(I+1)>(srcParticles. template operator()<I, false, true>(i)), ...); // I+1 as KokkosSoA does not contain ptr
     }
   }
 };
