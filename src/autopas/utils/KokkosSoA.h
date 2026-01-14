9cb74125a5c95a5f1ceb99692fb424f50619db37
Luis Gall <luis.gall@tum.de>
2026-01-14 17:12:33 +0100

    Fix for return type

--- src/autopas/utils/KokkosSoA.h
+++ src/autopas/utils/KokkosSoA.h
@@ -58,7 +58,7 @@ namespace autopas::utils {
 
     template <size_t attribute>
     KOKKOS_INLINE_FUNCTION
-    constexpr std::tuple_element<attribute, std::tuple<Kokkos::View<Types>...>>::type& getView() const {
+    constexpr std::tuple_element<attribute, std::tuple<Kokkos::DualView<Types>...>>::type& getView() const {
       return std::get<attribute>(views);
     }
 
