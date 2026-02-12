606be71a7677395c384bbc9b5a1dc2662f2d059e
Markus Mühlhäußer <markus.muehlhaeusser@tum.de>
2026-02-12 22:30:04 +0100

    Remove logger constructor from mpitest

--- tests/MPIParallelAutoPasTests/AutoPasMPITestBase.h
+++ tests/MPIParallelAutoPasTests/AutoPasMPITestBase.h
@@ -7,14 +7,4 @@
 
 #include <gtest/gtest.h>
 
-#include "autopas/utils/logging/Logger.h"
-
-#pragma once
-
-class AutoPasMPITestBase : public testing::Test {
- public:
-  explicit AutoPasMPITestBase(std::ostream &os = std::cout) : _logger(os) {}
-
- private:
-  autopas::Logger _logger;
-};
+class AutoPasMPITestBase : public testing::Test {};
