3864af9671745716f3f709d9bdf475f5f7afa78d
Stanislaw Grzegórski <stas.grzegorski@gmail.com>
2026-02-17 09:19:52 +0100

    No Early stopping VCL balanced traversal

--- scriptsCluster/spinoidalDecomposition5runs.sh
+++ scriptsCluster/spinoidalDecomposition5runs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J EarlyStoppingVCLBalanced
+#SBATCH -J NoEarlyStoppingVCLBalanced
 #SBATCH -o %x.%j.%N.out
 #SBATCH -e %x.%j.%N.err
 #SBATCH --clusters=cm4
--- src/autopas/containers/verletClusterLists/VerletClusterListsRebuilder.h
+++ src/autopas/containers/verletClusterLists/VerletClusterListsRebuilder.h
@@ -399,9 +399,9 @@ class VerletClusterListsRebuilder {
             const auto [aMin, aMax] = clusterIterA->getBoundingBox();
             const auto [bMin, bMax] = clusterIterB->getBoundingBox();
             // stop condition Early stopping
-            if (bMin[2] > aMax[2] + interactionLength) {
-              break;
-            }
+            //if (bMin[2] > aMax[2] + interactionLength) {
+              //break;
+            //}
             const auto boxDistSquared = boxDistanceSquared(aMin, aMax, bMin, bMax);
             if (boxDistSquared <= _interactionLengthSqr) {
               clusterIterA->addNeighbor(*clusterIterB);
