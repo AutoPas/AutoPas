588b9d8c2f32ad592d680d0ccd7d446d8e7e12b5
Ruben Horn <>
2026-03-11 13:32:12 +0100

    Ignore impossible thread count options (>num cores)

--- src/autopas/AutoPasDecl.h
+++ src/autopas/AutoPasDecl.h
@@ -1031,6 +1031,9 @@ class AutoPas {
    * @param allowedThreadCounts
    */
   void setAllowedThreadCounts(const NumberSet<int> &allowedThreadCounts) {
+    if (!allowedThreadCounts.isFinite()) {
+      utils::ExceptionHandler::exception("Error: thread count options must be finite!", allowedThreadCounts.getMin());
+    }
     if (allowedThreadCounts.getMin() == Configuration::ThreadCountNoTuning and allowedThreadCounts.size() != 1) {
       utils::ExceptionHandler::exception(
           "Error: when thread counts contain {} (no thread tuning), no other values may be provided!",
@@ -1040,12 +1043,16 @@ class AutoPas {
       utils::ExceptionHandler::exception("Error: minimum thread count must be positive {} < 0!",
                                          allowedThreadCounts.getMin());
     }
-    const int maxThreadCounts = autopas_get_max_threads();
-    if (allowedThreadCounts.getMax() > maxThreadCounts) {
-      utils::ExceptionHandler::exception("Error: maximum thread count exceeded {} > {}!", allowedThreadCounts.getMax(),
-                                         maxThreadCounts);
+    const int maxThreadCount = autopas_get_max_threads();
+    if (allowedThreadCounts.getMax() > maxThreadCount) {
+      AutoPasLog(WARN, "Warning: thread count options exceeding {} will be discarded!", maxThreadCount);
     }
-    _allowedThreadCounts = std::move(allowedThreadCounts.clone());
+    std::set<int> values = allowedThreadCounts.getAll();
+    values.erase(values.upper_bound(maxThreadCount), values.end());
+    if (values.size() == 0) {
+      utils::ExceptionHandler::exception("Error: no thread count option remaining!");
+    }
+    _allowedThreadCounts->resetValues(values);
   }
 
   /**
