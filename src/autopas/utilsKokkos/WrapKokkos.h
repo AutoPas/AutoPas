a11b5747add35ad6d1b657b099a82f8ceb17e018
Luis Gall <luis.gall@tum.de>
2026-07-01 13:30:02 +0200

    Added documentation for methods

--- src/autopas/utilsKokkos/WrapKokkos.h
+++ src/autopas/utilsKokkos/WrapKokkos.h
@@ -1,5 +1,5 @@
 /**
-* @file WrapKokkos.h
+ * @file WrapKokkos.h
  * @author Luis Gall
  * @date 04.11.2025
  */
@@ -8,25 +8,28 @@
 
 #ifdef AUTOPAS_ENABLE_KOKKOS
 #include <Kokkos_Core.hpp>
-#define AUTOPAS_KOKKOS_INLINE_FUNCTION KOKKOS_INLINE_FUNCTION
-#else
-#define AUTOPAS_KOKKOS_INLINE_FUNCTION inline
 #endif
 
 namespace autopas {
 
-inline int AutoPas_Kokkos_Init(int& argc, char** argv) {
+/**
+ * Wrapper for Kokkos::initialize()
+ * @param argc: reference to number of arguments
+ * @param argv: argument vector
+ */
+inline void AutoPas_Kokkos_Init(int &argc, char **argv) {
 #ifdef AUTOPAS_ENABLE_KOKKOS
   Kokkos::initialize(argc, argv);
 #endif
-  return 0;
 }
 
-inline int AutoPas_Kokkos_Finalize() {
+/**
+ * Wrapper for Kokkos::finalize()
+ */
+inline void AutoPas_Kokkos_Finalize() {
 #ifdef AUTOPAS_ENABLE_KOKKOS
   Kokkos::finalize();
 #endif
-  return 0;
 }
 
-}
+}  // namespace autopas
