b1999b15533ebd462abe0ec79086992e1e4602a8
Markus Mühlhäußer <markus.muehlhaeusser@tum.de>
2026-07-28 16:10:04 +0200

    Switch highway from zip to git subtree

--- cmake/modules/autopas_highway.cmake
+++ cmake/modules/autopas_highway.cmake
@@ -1,10 +1,4 @@
-include(FetchContent)
-
-FetchContent_Declare(
-    autopas_highway
-    URL ${AUTOPAS_SOURCE_DIR}/libs/highway-1.3.0.zip
-)
-
 # We must force this to ensure that Highway does not create a gtest target in conflict with our own.
 set(HWY_ENABLE_TESTS OFF CACHE BOOL "Disable Highway tests" FORCE)
-FetchContent_MakeAvailable(autopas_highway)
+
+add_subdirectory(${AUTOPAS_SOURCE_DIR}/libs/highway ${CMAKE_CURRENT_BINARY_DIR}/highway EXCLUDE_FROM_ALL)
