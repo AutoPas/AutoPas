3997847d4f938c1665a2f896c80782d2a4f9e46f
Markus Mühlhäußer <markus.muehlhaeusser@tum.de>
2026-07-28 14:57:21 +0200

    Update googletest CMake file changing fetch_content to add_subdirectory

--- cmake/modules/autopas_gtest.cmake
+++ cmake/modules/autopas_gtest.cmake
@@ -1,22 +1,18 @@
 message(STATUS "gtest - using bundled version")
 find_package(Threads REQUIRED)
 
-# Enable FetchContent CMake module
-include(FetchContent)
-
-# Build GoogleTest and make the cmake targets available
-FetchContent_Declare(
-        autopas_gtest
-        SOURCE_DIR ${AUTOPAS_SOURCE_DIR}/libs/googletest
-)
-
 option(INSTALL_GTEST "" OFF)
 
 # hide options from ccmake
-mark_as_advanced(BUILD_GMOCK INSTALL_GTEST)
+mark_as_advanced(
+        BUILD_GMOCK
+        INSTALL_GTEST
+        GTEST_HAS_ABSL  # Only relevant when also using the Abseil library, which we don't.
+)
 
 # Prevent overriding the parent project's compiler/linker settings on Windows.
 # => Compiles gtest with correct mt(d)/md(d)
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
-FetchContent_MakeAvailable(autopas_gtest)
+# EXCLUDE_FROM_ALL, such that gtest is not built by default (target `all`).
+add_subdirectory(${AUTOPAS_SOURCE_DIR}/libs/googletest ${CMAKE_CURRENT_BINARY_DIR}/googletest EXCLUDE_FROM_ALL)
