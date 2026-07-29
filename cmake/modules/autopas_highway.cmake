d0c59cce5ea10bbe1c9a53de6bc64999be5a37e1
Markus Mühlhäußer <markus.muehlhaeusser@tum.de>
2026-07-29 13:31:36 +0200

    mark diabled hwy features internal

--- cmake/modules/autopas_highway.cmake
+++ cmake/modules/autopas_highway.cmake
@@ -1,8 +1,8 @@
 # Disable unnecessary Highway features that bloat the build and cache
-set(HWY_ENABLE_TESTS OFF CACHE BOOL "Disable Highway tests" FORCE) # Force OFF to avoid conflicting test targets
-set(HWY_ENABLE_CONTRIB OFF CACHE BOOL "Disable Highway contrib")
-set(HWY_ENABLE_EXAMPLES OFF CACHE BOOL "Disable Highway examples")
-set(HWY_ENABLE_INSTALL OFF CACHE BOOL "Disable Highway install")
+set(HWY_ENABLE_TESTS OFF CACHE INTERNAL "Disable Highway tests")
+set(HWY_ENABLE_CONTRIB OFF CACHE INTERNAL "Disable Highway contrib")
+set(HWY_ENABLE_EXAMPLES OFF CACHE INTERNAL "Disable Highway examples")
+set(HWY_ENABLE_INSTALL OFF CACHE INTERNAL "Disable Highway install")
 
 # Hide all Highway-specific CMake options from the standard ccmake view
 mark_as_advanced(
@@ -11,10 +11,6 @@ mark_as_advanced(
         HWY_CMAKE_RVV            # Manually enables RISC-V Vector extensions
         HWY_CMAKE_SSE2           # Manually enables SSE2 for x86
         HWY_DISABLE_FUTEX        # Disables fast-mutexes in Highway's thread pool
-        HWY_ENABLE_CONTRIB       # Builds extra Highway algorithms
-        HWY_ENABLE_EXAMPLES      # Builds Highway examples
-        HWY_ENABLE_INSTALL       # Installs Highway globally during `make install`
-        HWY_ENABLE_TESTS         # Builds Highway's own test suite (forced OFF above)
         HWY_TEST_STANDALONE      # Builds Highway tests without Google Test
         HWY_WARNINGS_ARE_ERRORS  # Highway developer flag to treat compiler warnings as errors
 )
