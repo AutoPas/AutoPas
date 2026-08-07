87355f12ad1ceee4436e99b40db0d07504fe98d6
Manish Kumar Mishra <manish6016mishra@gmail.com>
2026-08-07 13:27:21 +0200

    Delta rebuildTime < Delta remainderTravTime

--- src/autopas/utils/RegressionModels.h
+++ src/autopas/utils/RegressionModels.h
@@ -457,7 +457,7 @@ class RebuildDecisionContext {
       // (curr_iter * (curr_iter + 1)), but the denominator is same and can be ignored.
       const double remainderIncline =
           (value_remainder * curr_iter - static_cast<double>(_remainderTraversalTimePredictor.getSumY()));
-      if (value_rebuild <= value_remainder) {
+      if (remainderIncline >= value_rebuild) {
         doDynamicRebuild = true;
       }
     } else if (returnCode_rebuild == RegressionBase::ReturnCode::OVERFLOW_REG ||
